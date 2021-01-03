2727b407bfc03973d2c328b3f147c187f85c9b9b
Rageh ElAzzazy <rageh.az@gmail.com>
2021-01-03 13:57:53 +0200

    - flyer filters are now strings not enums

--- lib/models/enums/enum_filters.dart
+++ lib/models/enums/enum_filters.dart
@@ -1,44 +1,49 @@
 
-enum PropertyType {Penthouse, Chalet, TwinHouse, FullFloor, HalfFloor, Building,
-  Land, Bungalow, HotelRoom, Villa, Office, Store, WareHouse, ExhibitionHall,
-  MeetingRoom, Clinic, }
-enum ContractType {ForSale, ForRent}
-enum PropertyUse {Residential, Commercial, Industrial, Governmental, Administration,
-  Transportation, Educational, Agricultural, Medical, Hotel, Sports, Entertainment}
+List<String> propertyType = ['Penthouse', 'Chalet', 'TwinHouse', 'FullFloor', 'HalfFloor', 'Building',
+  'Land', 'Bungalow', 'HotelRoom', 'Villa', 'Office', 'Store', 'WareHouse', 'ExhibitionHall',
+  'MeetingRoom', 'Clinic', ];
+
+List<String> contractType = ['ForSale', 'ForRent'];
+
+List<String> propertyUse = ['Residential', 'Commercial', 'Industrial', 'Governmental', 'Administration',
+  'Transportation', 'Educational', 'Agricultural', 'Medical', 'Hotel', 'Sports', 'Entertainment'];
+
 // int propertyArea;
 // int numberOfRoom;
 // Coordinates propertyLocation;
 // int propertyPrice;
 
-enum DesignType {ArchitectureDesign, InteriorDesign, UrbanDesign, UrbanPlanning,
-  FurnitureDesign, LightingDesign, LandscapeDesign, StructureDesign, InfrastructureDesign}
+List<String> designType = ['ArchitectureDesign', 'InteriorDesign', 'UrbanDesign', 'UrbanPlanning',
+  'FurnitureDesign', 'LightingDesign', 'LandscapeDesign', 'StructureDesign', 'InfrastructureDesign'];
 
-enum SpaceType {Bedroom, LivingRoom, Kitchen, Bathroom, Reception, Salon, Toilet,
-  Terrace, BackYard, StorageRoom, Spa, Gymnasium, LaundryRoom, Theatre, Court,
-  DiningRoom, Stairs, Attic, MechanicalRoom, ElectricityRoom, store, corridor,
-Lobby, Office, Kitchenette, ElevatorRoom, LectureRoom, MeetingRoom, SeminalHall,
-  ConventionHall, MusicalHall, HomeTheatre, Library, Garage,}
+List<String> spaceType = ['Bedroom', 'LivingRoom', 'Kitchen', 'Bathroom', 'Reception', 'Salon', 'Toilet',
+  'Terrace', 'BackYard', 'StorageRoom', 'Spa', 'Gymnasium', 'LaundryRoom', 'Theatre', 'Court',
+  'DiningRoom', 'Stairs', 'Attic', 'MechanicalRoom', 'ElectricityRoom', 'store', 'corridor',
+'Lobby', 'Office', 'Kitchenette', 'ElevatorRoom', 'LectureRoom', 'MeetingRoom', 'SeminalHall',
+  'ConventionHall', 'MusicalHall', 'HomeTheatre', 'Library', 'Garage',];
 
-enum ArchitecturalStyle {AmericanColonial, Arabian,ArtDeco,ArtNouveau,Asian,BeachStyle,
-  Chinese, Contemporary,Craftsman,Eclectic,EnglishCountry,Farmhouse,French,FrenchCountry,
-Gothic,Indian,Industrial,Japanese,Mediterranean,MidCentury,Medieval,Minimalist,
-  Modern,Moroccan,Rustic,ScandinavianCountry,ScandinavianModern,ShabbyChic,
-  Shaker,Southwestern,Spanish,Traditional,Transitional,Tuscan,Tropical,
-  Victorian,Vintage,Zen,}
+List<String> architecturalStyle = ['AmericanColonial', 'Arabian','ArtDeco','ArtNouveau','Asian','BeachStyle',
+  'Chinese', 'Contemporary','Craftsman','Eclectic','EnglishCountry','Farmhouse','French','FrenchCountry',
+'Gothic','Indian','Industrial','Japanese','Mediterranean','MidCentury','Medieval','Minimalist',
+  'Modern','Moroccan','Rustic','ScandinavianCountry','ScandinavianModern','ShabbyChic',
+  'Shaker','Southwestern','Spanish','Traditional','Transitional','Tuscan','Tropical',
+  'Victorian','Vintage','Zen',];
 
-enum ProductUse {Wall, Floor, Ceiling, Roof, Window, Door, Structure, Signage,
-  ProofingIsolation, FireFighting, Stairs, Landscape,
-  HVAC, Plumbing, Furnishing, Appliance, Safety, Conveying, Lighting, Electricity,
-SmartHome, Security, }
+List<String> productUse = ['Wall', 'Floor', 'Ceiling', 'Roof', 'Window', 'Door', 'Structure', 'Signage',
+  'ProofingIsolation', 'FireFighting', 'Stairs', 'Landscape',
+  'HVAC', 'Plumbing', 'Furnishing', 'Appliance', 'Safety', 'Conveying', 'Lighting', 'Electricity',
+'SmartHome', 'Security', ];
 
-enum ConstructionMaterial {Paint, Wood, Paper, Stone, Marble, Granite, Fabric,
-  Plastic, Glass, Brick, Metal, Concrete, Ceramic, Porcelain, Mosaic, Asphalt,
-  Gypsum, PVC, WPC}
+List<String> constructionMaterial = ['Paint', 'Wood', 'Paper', 'Stone', 'Marble', 'Granite', 'Fabric',
+  'Plastic', 'Glass', 'Brick', 'Metal', 'Concrete', 'Ceramic', 'Porcelain', 'Mosaic', 'Asphalt',
+  'Gypsum', 'PVC', 'WPC'];
 
-//enum ProjectType {Interior, Architecture, LightStructure, }
-enum ConstructionTrade {Carpentry, Electricity, Elevators, Isolation, Masonry,
-  Plumbing,}
+//List<String projectType = ['Interior', 'Architecture', 'LightStructure', ];
+
+List<String> constructionTrade = ['Carpentry', 'Electricity', 'Elevators', 'Isolation', 'Masonry',
+  'Plumbing',];
 
 
-enum EquipmentType {EarthWork, Vehicles, MaterialHandling, HandTools, PowerTools}
-// enum EquipmentUse {EarthWork, SiteWork,}
+List<String> equipmentType = ['EarthWork', 'Vehicles', 'MaterialHandling', 'HandTools', 'PowerTools'];
+
+// List<String equipmentUse = ['EarthWork', 'SiteWork',];
