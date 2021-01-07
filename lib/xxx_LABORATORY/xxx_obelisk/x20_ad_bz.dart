34c35c9e40eb6f7c341eec04b3472ad5d826ceb7
Rageh ElAzzazy <rageh.az@gmail.com>
2021-01-07 22:15:52 +0200

    - fakes the new way which would be hard
    - finishing an easy usual data entry method in a list 3ady geddan zy ennas
    
    - some cleanups as usual

--- lib/xxx_LABORATORY/xxx_obelisk/x20_ad_bz.dart
+++ lib/xxx_LABORATORY/xxx_obelisk/x20_ad_bz.dart
@@ -1,9 +1,12 @@
-import 'package:bldrs/view_brains/drafters/scalers.dart';
-import 'package:bldrs/views/widgets/flyer/header/header.dart';
-import 'package:bldrs/views/widgets/flyer/header/max_header/max_header.dart';
-import 'package:bldrs/views/widgets/flyer/header/mini_header/mini_header.dart';
+import 'package:bldrs/models/enums/enum_bldrs_section.dart';
+import 'package:bldrs/models/enums/enum_bz_type.dart';
+import 'package:bldrs/view_brains/drafters/stringers.dart';
+import 'package:bldrs/view_brains/theme/colorz.dart';
+import 'package:bldrs/view_brains/theme/iconz.dart';
+import 'package:bldrs/views/widgets/buttons/dream_box.dart';
+import 'package:bldrs/views/widgets/in_pyramids/in_pyramids_items/in_pyramids_bubble.dart';
 import 'package:bldrs/views/widgets/layouts/main_layout.dart';
-import 'package:bldrs/views/widgets/pro_flyer/flyer_parts/flyer_zone.dart';
+import 'package:bldrs/views/widgets/textings/super_verse.dart';
 import 'package:flutter/material.dart';
 
 class AddBzScreen2 extends StatefulWidget {
@@ -13,6 +16,8 @@ class AddBzScreen2 extends StatefulWidget {
 
 class _AddBzScreen2State extends State<AddBzScreen2> {
   bool bzPageIsOn;
+  BldrsSection chosenSection;
+  BzType chosenBzType;
   // ----------------------------------------------------------------------
   void initState(){
     super.initState();
@@ -25,35 +30,139 @@ class _AddBzScreen2State extends State<AddBzScreen2> {
     });
   }
   // ----------------------------------------------------------------------
-
+  void selectASection(int index){
+    setState(() {
+      chosenSection = bldrsSectionsList[index];
+    });
+  }
+  // ----------------------------------------------------------------------
+void selectBzType(int index){
+    setState(() {
+      chosenBzType = bzTypesList[index];
+    });
+}
   @override
   Widget build(BuildContext context) {
+
     return MainLayout(
-      layoutWidget: Stack(
-        children: <Widget>[
+      appBarType: AppBarType.Basic,
+      pyramids: Iconz.PyramidsYellow,
+      layoutWidget: SingleChildScrollView(
 
-          FlyerZone(
-            flyerSizeFactor: 0.8,
-            tappingFlyerZone: (){print('fuckyou');},
-            stackWidgets: <Widget>[
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
 
-              Header(
-                  coBz: null,
-                  coAuthor: null,
-                  flyerShowsAuthor: true,
-                  followIsOn: null,
-                  flyerZoneWidth: superFlyerZoneWidth(context, 0.8),
-                  bzPageIsOn: bzPageIsOn,
-                  tappingHeader: triggerMaxHeader,
-                  tappingFollow: null,
-                  tappingUnfollow: null,
-              ),
+            Stratosphere(),
 
-            ],
-          ),
+            MultipleChoiceBubble(
+                title: 'Sections :',
+                buttonsList: sectionsListStrings(context),
+                tappingAButton: selectASection,
+                chosenButton: sectionStringer(context, chosenSection),
+            ),
 
-        ],
+            // --- CHOOSE BZTYPE
+            MultipleChoiceBubble(
+              title: 'Account type :',
+              buttonsList: bzTypesStrings(context),
+              tappingAButton: selectBzType,
+              chosenButton: bzTypeSingleStringer(context, chosenBzType),
+            )
+
+
+
+            // Stack(
+            //   children: <Widget>[
+            //
+            //     // FlyerZone(
+            //     //   flyerSizeFactor: 0.8,
+            //     //   tappingFlyerZone: (){print('fuckyou');},
+            //     //   stackWidgets: <Widget>[
+            //     //
+            //     //     Header(
+            //     //         coBz: null,
+            //     //         coAuthor: null,
+            //     //         flyerShowsAuthor: true,
+            //     //         followIsOn: null,
+            //     //         flyerZoneWidth: superFlyerZoneWidth(context, 0.8),
+            //     //         bzPageIsOn: bzPageIsOn,
+            //     //         tappingHeader: triggerMaxHeader,
+            //     //         tappingFollow: null,
+            //     //         tappingUnfollow: null,
+            //     //     ),
+            //     //
+            //     //   ],
+            //     // ),
+            //
+            //   ],
+            // )
+
+          ],
+        ),
       ),
     );
   }
 }
+
+class MultipleChoiceBubble extends StatelessWidget {
+  final String title;
+  final List<String> buttonsList;
+  final Function tappingAButton;
+  final String chosenButton;
+
+  MultipleChoiceBubble({
+    @required this.title,
+    @required this.buttonsList,
+    @required this.tappingAButton,
+    @required this.chosenButton,
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return InPyramidsBubble(
+        bubbleColor: Colorz.WhiteAir,
+        columnChildren: <Widget>[
+
+          SuperVerse(
+            verse: title,
+            margin: 5,
+          ),
+
+          Wrap(
+              spacing: 0,
+              runSpacing: 0,
+              alignment: WrapAlignment.center,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              direction: Axis.horizontal,
+              runAlignment: WrapAlignment.center,
+
+              children:
+              List<Widget>.generate(
+                  buttonsList.length,
+                      (int index) {
+                    return
+                      DreamBox(
+                        height: 40,
+                        verse: buttonsList[index],
+                        verseItalic: false,
+                        color: chosenButton == buttonsList[index] ? Colorz.Yellow : Colorz.WhiteAir,
+                        verseColor: chosenButton == buttonsList[index] ? Colorz.BlackBlack : Colorz.White,
+                        verseWeight: chosenButton == buttonsList[index] ? VerseWeight.black :  VerseWeight.bold,
+                        verseScaleFactor: 0.6,
+                        boxMargins: EdgeInsets.all(5),
+                        boxFunction: () => tappingAButton(index),
+                      );
+                  }
+              )
+
+
+          ),
+
+
+        ]
+    );
+  }
+}
+
