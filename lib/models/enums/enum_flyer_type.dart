804828fcb6c6834b2b69e8b61b2ff0a19cd9cd84
Rageh ElAzzazy <rageh.az@gmail.com>
2021-01-11 13:28:10 +0200

    - enum cipher & decipher functions are set

--- lib/models/enums/enum_flyer_type.dart
+++ lib/models/enums/enum_flyer_type.dart
@@ -7,3 +7,32 @@ enum FlyerType {
   Equipment, // eq
   General,
 }
+
+// x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+FlyerType decipherFlyerType (int x){
+  switch (x){
+    case 1:   return  FlyerType.Property;     break;
+    case 2:   return  FlyerType.Design;       break;
+    case 3:   return  FlyerType.Product;      break;
+    case 4:   return  FlyerType.Project;      break;
+    case 5:   return  FlyerType.Craft;        break;
+    case 6:   return  FlyerType.Equipment;    break;
+    case 7:   return  FlyerType.General;      break;
+    default : return   null;
+  }
+}
+// x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+int cipherFlyerType (FlyerType x){
+  switch (x){
+    case FlyerType.Property    :    return  1;  break;
+    case FlyerType.Design      :    return  2;  break;
+    case FlyerType.Product     :    return  3;  break;
+    case FlyerType.Project     :    return  4;  break;
+    case FlyerType.Craft       :    return  5;  break;
+    case FlyerType.Equipment   :    return  6;  break;
+    case FlyerType.General     :    return  7;  break;
+    default : return null;
+  }
+}
+// x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+
