9cae3a41008d187794d48a36456b07eef8080a5b
Rageh ElAzzazy <rageh.az@gmail.com>
2021-01-23 14:37:18 +0200

    - shwayet sha2awa, cleanups, translations,, was feeling bored

--- lib/view_brains/drafters/aligners.dart
+++ lib/view_brains/drafters/aligners.dart
@@ -3,25 +3,21 @@ import 'package:flutter/material.dart';
 // === === === === === === === === === === === === === === === === === === ===
 Alignment superTopAlignment(BuildContext context){
   return
-      // Alignment.centerLeft ;
   Wordz.textDirection(context) == 'ltr' ? Alignment.topLeft : Alignment.topRight;
 }
 // === === === === === === === === === === === === === === === === === === ===
 Alignment superCenterAlignment(BuildContext context){
   return
-      // Alignment.centerLeft ;
     Wordz.textDirection(context) == 'ltr' ? Alignment.centerLeft : Alignment.centerRight;
 }
 // === === === === === === === === === === === === === === === === === === ===
 Alignment superInverseCenterAlignment(BuildContext context){
   return
-      // Alignment.centerLeft ;
     Wordz.textDirection(context) == 'ltr' ? Alignment.centerRight : Alignment.centerLeft;
 }
 // === === === === === === === === === === === === === === === === === === ===
 Alignment superInverseTopAlignment(BuildContext context){
   return
-    // Alignment.centerLeft ;
     Wordz.textDirection(context) == 'ltr' ? Alignment.topRight : Alignment.topLeft;
 }
 // === === === === === === === === === === === === === === === === === === ===
