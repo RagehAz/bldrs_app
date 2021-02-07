329bbe151e8114a66048d753abaffbcc808f7a03
Rageh ElAzzazy <rageh.az@gmail.com>
2021-02-07 02:07:54 +0200

    - it barely works now,, 3ala sha3ra w tcrash
    - added too much stuff that I will never go back, so no reason to document ba2a
    
    - some providers, cleaning, 7antafa, models updates, firebase stuff, some in countries and some in users
    
    - cocktail updates
    
    - thankfully I can sleep with no bug now
    
    - good fucking night
    
    - I will keep having fun and tell jokes because I can
    
    - o1o

--- lib/views/widgets/loading/loading.dart
+++ lib/views/widgets/loading/loading.dart
@@ -1,3 +1,4 @@
+import 'package:bldrs/view_brains/drafters/scalers.dart';
 import 'package:bldrs/view_brains/theme/colorz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -23,5 +24,16 @@ class Loading extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+
+class LoadingFullScreenLayer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: superScreenWidth(context),
+        height: superScreenWidth(context),
+        child: Center(child: Loading()));
   }
 }
