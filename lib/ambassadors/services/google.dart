8c0802c0fcde7ff87c84530d22b43ba4bf017891
omarbassam <omar.bassam88@gmail.com>
2021-02-16 22:07:10 +0200

    Signing in and out with Google

---
+++ lib/ambassadors/services/google.dart
@@ -0,0 +1,65 @@
+import 'package:bldrs/models/sub_models/contact_model.dart';
+import 'package:bldrs/models/user_model.dart';
+import 'package:bldrs/providers/users_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+Future<String> signInWithGoogle() async {
+  // await Firebase.initializeApp();
+
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final UserCredential authResult =
+      await _auth.signInWithCredential(credential);
+  final User user = authResult.user;
+
+  if (user != null) {
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+// create a new firestore document for the user with the userID
+    await UserProvider(userID: user.uid).updateUserData(
+      userID: user.uid,
+      joinedAt: DateTime.now(),
+      userStatus: UserStatus.NormalUser,
+      // -------------------------
+      name: user.displayName,
+      pic: user.photoURL,
+      title: '',
+      gender: Gender.any,
+      country: "egy",
+      language: "en",
+      position: GeoPoint(0, 0),
+      contacts: [
+        ContactModel(contact: user.email, contactType: ContactType.Email)
+      ],
+      // -------------------------
+      savedFlyersIDs: [''],
+      followedBzzIDs: [''],
+    );
+    final User currentUser = _auth.currentUser;
+    assert(user.uid == currentUser.uid);
+
+    print('signInWithGoogle succeeded: $user');
+
+    return '$user';
+  }
+
+  return null;
+}
+
+Future<void> signOutGoogle() async {
+  await googleSignIn.signOut();
+
+  print("User Signed Out");
+}
