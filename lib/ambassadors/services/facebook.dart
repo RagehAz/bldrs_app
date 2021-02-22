1234acc9567d1d553899fdfa6b4eeca547bddbe6
Rageh El Azzazy <rageh.az@gmail.com>
2021-02-22 16:00:20 +0200

    - fixed facebook auth routing bug

--- lib/ambassadors/services/facebook.dart
+++ lib/ambassadors/services/facebook.dart
@@ -1,29 +1,32 @@
 import 'package:bldrs/models/sub_models/contact_model.dart';
 import 'package:bldrs/models/user_model.dart';
 import 'package:bldrs/providers/users_provider.dart';
+import 'package:bldrs/view_brains/router/navigators.dart';
+import 'package:bldrs/view_brains/router/route_names.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 // an alert error should be thrown when firebase replies with the following line
 // Firebase Suthentication Error
 // An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.
 // we should consider same scenario with other auth methods that have this conflict with existing signed up e-mail
-
-Future<UserCredential> signInWithFacebook() async {
+// === === === === === === === === === === === === === === === === === === ===
+Future<UserCredential> signInWithFacebook(BuildContext context) async {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
+  // ---------------------------------------------------------------------------
   try {
     final AccessToken accessToken = await FacebookAuth.instance.login();
-
-    // Create a credential from the access token
-    final FacebookAuthCredential credential = FacebookAuthProvider.credential(
-      accessToken.token,
-    );
-    final UserCredential authResult =
-        await _auth.signInWithCredential(credential);
+    // -------------------------
+    /// Create a credential from the access token
+    final FacebookAuthCredential credential = FacebookAuthProvider.credential(accessToken.token,);
+    // -------------------------
+    final UserCredential authResult = await _auth.signInWithCredential(credential);
+    // -------------------------
     final User user = authResult.user;
-// create a new firestore document for the user with the userID
+    // -------------------------
+    /// create a new firestore document for the user with the userID
     await UserProvider(userID: user.uid).updateUserData(
       userID: user.uid,
       joinedAt: DateTime.now(),
@@ -37,21 +40,25 @@ Future<UserCredential> signInWithFacebook() async {
       language: "en",
       position: GeoPoint(0, 0),
       contacts: [
-        ContactModel(contact: user.email, contactType: ContactType.Email)
+        ContactModel(
+          contact: user.email,
+            contactType: ContactType.Email)
       ],
       // -------------------------
       savedFlyersIDs: [''],
       followedBzzIDs: [''],
     );
-    // Once signed in, return the UserCredential
+    // -------------------------
+    /// Once signed in, return the UserCredential
     return await FirebaseAuth.instance.signInWithCredential(credential);
+    // -------------------------
   } on FacebookAuthException catch (e) {
     // handle the FacebookAuthException
     print("Facebook Authentication Error");
     print(e.message);
   } on FirebaseAuthException catch (e) {
     // handle the FirebaseAuthException
-    print("Firebase Suthentication Error");
+    print("Firebase Authentication Error");
     print(e.message);
   } finally {}
   return null;
