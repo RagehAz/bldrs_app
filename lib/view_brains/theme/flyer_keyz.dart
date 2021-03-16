e616b452d859e16fd293a4e3a2e7f96f21097cca
Rageh El Azzazy <rageh.az@gmail.com>
2021-03-16 11:59:28 +0200

    - bug fixes

--- lib/view_brains/theme/flyer_keyz.dart
+++ lib/view_brains/theme/flyer_keyz.dart
@@ -1,13 +1,13 @@
 class Keywordz{
 
 
-  static const List<String> propertyType = const ['Penthouse', 'Chalet', 'TwinHouse', 'FullFloor', 'HalfFloor', 'Building',
+  static const List<String> propertyType = const <String>['Penthouse', 'Chalet', 'TwinHouse', 'FullFloor', 'HalfFloor', 'Building',
     'Land', 'Bungalow', 'HotelRoom', 'Villa', 'Office', 'Store', 'WareHouse', 'ExhibitionHall',
     'MeetingRoom', 'Clinic', ];
 
-  static const List<String> contractType = ['ForSale', 'ForRent'];
+  static const List<String> contractType = const <String>['ForSale', 'ForRent'];
 
-  static const List<String> propertyUse = ['Residential', 'Commercial', 'Industrial', 'Governmental', 'Administration',
+  static const List<String> propertyUse = const <String>['Residential', 'Commercial', 'Industrial', 'Governmental', 'Administration',
     'Transportation', 'Educational', 'Agricultural', 'Medical', 'Hotel', 'Sports', 'Entertainment'];
 
 // int propertyArea;
@@ -15,39 +15,39 @@ class Keywordz{
 // Coordinates propertyLocation;
 // int propertyPrice;
 
-  static const List<String> designType = ['ArchitectureDesign', 'InteriorDesign', 'UrbanDesign', 'UrbanPlanning',
+  static const List<String> designType = const <String>['ArchitectureDesign', 'InteriorDesign', 'UrbanDesign', 'UrbanPlanning',
     'FurnitureDesign', 'LightingDesign', 'LandscapeDesign', 'StructureDesign', 'InfrastructureDesign'];
 
-  static const List<String> spaceType = ['Bedroom', 'LivingRoom', 'Kitchen', 'Bathroom', 'Reception', 'Salon', 'Toilet',
+  static const List<String> spaceType = const <String>['Bedroom', 'LivingRoom', 'Kitchen', 'Bathroom', 'Reception', 'Salon', 'Toilet',
     'Terrace', 'BackYard', 'StorageRoom', 'Spa', 'Gymnasium', 'LaundryRoom', 'Theatre', 'Court',
     'DiningRoom', 'Stairs', 'Attic', 'MechanicalRoom', 'ElectricityRoom', 'store', 'corridor',
     'Lobby', 'Office', 'Kitchenette', 'ElevatorRoom', 'LectureRoom', 'MeetingRoom', 'SeminalHall',
     'ConventionHall', 'MusicalHall', 'HomeTheatre', 'Library', 'Garage',];
 
-  static const List<String> architecturalStyle = ['AmericanColonial', 'Arabian','ArtDeco','ArtNouveau','Asian','BeachStyle',
+  static const List<String> architecturalStyle = const <String>['AmericanColonial', 'Arabian','ArtDeco','ArtNouveau','Asian','BeachStyle',
     'Chinese', 'Contemporary','Craftsman','Eclectic','EnglishCountry','Farmhouse','French','FrenchCountry',
     'Gothic','Indian','Industrial','Japanese','Mediterranean','MidCentury','Medieval','Minimalist',
     'Modern','Moroccan','Rustic','ScandinavianCountry','ScandinavianModern','ShabbyChic',
     'Shaker','Southwestern','Spanish','Traditional','Transitional','Tuscan','Tropical',
     'Victorian','Vintage','Zen',];
 
-  static const List<String> productUse = ['Wall', 'Floor', 'Ceiling', 'Roof', 'Window', 'Door', 'Structure', 'Signage',
+  static const List<String> productUse = const <String>['Wall', 'Floor', 'Ceiling', 'Roof', 'Window', 'Door', 'Structure', 'Signage',
     'ProofingIsolation', 'FireFighting', 'Stairs', 'Landscape',
     'HVAC', 'Plumbing', 'Furnishing', 'Appliance', 'Safety', 'Conveying', 'Lighting', 'Electricity',
     'SmartHome', 'Security', ];
 
-  static const List<String> constructionMaterial = ['Paint', 'Wood', 'Paper', 'Stone', 'Marble', 'Granite', 'Fabric',
+  static const List<String> constructionMaterial = const <String>['Paint', 'Wood', 'Paper', 'Stone', 'Marble', 'Granite', 'Fabric',
     'Plastic', 'Glass', 'Brick', 'Metal', 'Concrete', 'Ceramic', 'Porcelain', 'Mosaic', 'Asphalt',
     'Gypsum', 'PVC', 'WPC'];
 
 //List<String projectType = ['Interior', 'Architecture', 'LightStructure', ];
 
-  static const List<String> constructionTrade = ['Carpentry', 'Electricity', 'Elevators', 'Isolation', 'Masonry',
+  static const List<String> constructionTrade = const <String>['Carpentry', 'Electricity', 'Elevators', 'Isolation', 'Masonry',
     'Plumbing',];
 
 
-  static const List<String> equipmentType = ['EarthWork', 'Vehicles', 'MaterialHandling', 'HandTools', 'PowerTools'];
+  static const List<String> equipmentType = const <String>['EarthWork', 'Vehicles', 'MaterialHandling', 'HandTools', 'PowerTools'];
 
-// List<String equipmentUse = ['EarthWork', 'SiteWork',];
+// List<String equipmentUse = <String>['EarthWork', 'SiteWork',];
 
 }
