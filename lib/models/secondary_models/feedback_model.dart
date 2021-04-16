3e6f35dd11d79c6c8bdef683aa72f0a29e4911e1
Rageh El Azzazy <rageh.az@gmail.com>
2021-04-16 15:02:02 +0200

    - added customer feedback functionality in a minute 2ella minute, front end, back end, middle end w kol end

---
+++ lib/models/secondary_models/feedback_model.dart
@@ -0,0 +1,32 @@
+import 'package:bldrs/controllers/drafters/timerz.dart';
+import 'package:flutter/foundation.dart';
+// ---------------------------------------------------------------------------
+class FeedbackModel{
+  final String userID;
+  final DateTime timeStamp;
+  final String feedback;
+
+  FeedbackModel({
+    @required this.userID,
+    @required this.timeStamp,
+    @required this.feedback,
+});
+// ---------------------------------------------------------------------------
+  Map<String, dynamic> toMap(){
+    return {
+      'userID' : userID,
+      'timeStamp' : cipherDateTimeToString(timeStamp),
+      'feedback' : feedback,
+    };
+  }
+// ---------------------------------------------------------------------------
+static FeedbackModel decipherFeedbackMap(Map<String, dynamic> map){
+    return
+        FeedbackModel(
+          userID: map['userID'],
+          timeStamp: decipherDateTimeString(map['timeStamp']),
+          feedback: map['feedback'],
+        );
+}
+// ---------------------------------------------------------------------------
+}
