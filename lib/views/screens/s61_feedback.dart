b36a6c8ae24b7c69ab3c4558ff3985c7f4652165
Rageh El Azzazy <rageh.az@gmail.com>
2021-04-16 15:02:02 +0200

    - added customer feedback functionality in a minute 2ella minute, front end, back end, middle end w kol end

---
+++ lib/views/screens/s61_feedback.dart
@@ -0,0 +1,129 @@
+import 'package:bldrs/controllers/drafters/scalers.dart';
+import 'package:bldrs/controllers/router/navigators.dart';
+import 'package:bldrs/controllers/theme/colorz.dart';
+import 'package:bldrs/controllers/theme/iconz.dart';
+import 'package:bldrs/firestore/auth/auth.dart';
+import 'package:bldrs/firestore/firestore.dart';
+import 'package:bldrs/models/secondary_models/feedback_model.dart';
+import 'package:bldrs/views/widgets/bubbles/text_field_bubble.dart';
+import 'package:bldrs/views/widgets/buttons/dream_box.dart';
+import 'package:bldrs/views/widgets/dialogs/alert_dialog.dart';
+import 'package:bldrs/views/widgets/layouts/main_layout.dart';
+import 'package:bldrs/views/widgets/textings/super_verse.dart';
+import 'package:flutter/material.dart';
+
+class FeedBack extends StatefulWidget {
+  @override
+  _FeedBackState createState() => _FeedBackState();
+}
+
+class _FeedBackState extends State<FeedBack> {
+  TextEditingController _feedbackController;
+// ---------------------------------------------------------------------------
+  /// --- LOADING BLOCK
+  bool _loading = false;
+  void _triggerLoading(){
+    setState(() {_loading = !_loading;});
+    _loading == true?
+    print('LOADING--------------------------------------') : print('LOADING COMPLETE--------------------------------------');
+  }
+// ---------------------------------------------------------------------------
+  @override
+  void initState() {
+    _feedbackController = new TextEditingController();
+    super.initState();
+  }
+// ---------------------------------------------------------------------------
+  @override
+  void dispose() {
+    _feedbackController.dispose();
+    super.dispose();
+  }
+// ---------------------------------------------------------------------------
+  Future<void> _uploadFeedBack() async {
+
+    _triggerLoading();
+
+    /// upload text to firebase
+    await createFireStoreDocument(
+      context: context,
+      collectionName: FireCollection.feedbacks,
+      input: FeedbackModel(
+        userID: superUserID(),
+        timeStamp: DateTime.now(),
+        feedback: _feedbackController.text,
+      ).toMap(),
+    );
+
+    _triggerLoading();
+
+    await superDialog(context, 'FeedBack sent', 'Thanks');
+
+    Nav.goBack(context);
+
+  }
+// ---------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    return MainLayout(
+      pyramids: Iconz.PyramidsYellow,
+      appBarType: AppBarType.Basic,
+      appBarBackButton: true,
+      pageTitle: 'About Bldrs.net',
+      sky: Sky.Black,
+      loading: _loading,
+      layoutWidget: ListView(
+        children: <Widget>[
+
+          Stratosphere(),
+
+          SuperVerse(
+            verse: 'Your opinion Matters !',
+            margin: 10,
+          ),
+
+          Center(
+            child: Container(
+              width: superScreenWidth(context) * 0.7,
+              margin: EdgeInsets.only(bottom: 10),
+              child: SuperVerse(
+                verse: 'Tell us what you think about Bldrs.net, or what upgrades you might think of',
+                margin: 5,
+                maxLines: 3,
+                size: 1,
+                scaleFactor: 1.25,
+                weight: VerseWeight.thin,
+                italic: true,
+              ),
+            ),
+          ),
+
+          TextFieldBubble(
+            title: 'Feedback',
+            textController: _feedbackController,
+            loading: _loading,
+            maxLines: 5,
+            fieldIsRequired: true,
+            keyboardTextInputAction: TextInputAction.newline,
+            keyboardTextInputType: TextInputType.multiline,
+            maxLength: 1000,
+            counterIsOn: true,
+          ),
+
+          DreamBox(
+            height: 50,
+            width: 200,
+            verse: 'Send',
+            verseColor: Colorz.BlackBlack,
+            color: Colorz.Yellow,
+            verseScaleFactor: 0.6,
+            boxFunction: _uploadFeedBack,
+          ),
+
+          PyramidsHorizon(heightFactor: 10,),
+
+        ],
+      ),
+    );
+  }
+}
