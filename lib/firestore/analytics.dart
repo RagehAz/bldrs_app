10cbd0a7ca91f67654cb10e688007f3ba57b4639
Rageh El Azzazy <rageh.az@gmail.com>
2021-05-03 03:30:27 +0200

    FIX FOR gradle fails with Problems reading data from Binary store error
    EL ZEFT DAH
    
    lutter Could not resolve all dependencies for configuration ':app:releaseCompileClasspath'

--- lib/firestore/analytics.dart
+++ lib/firestore/analytics.dart
@@ -1,10 +1,10 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
-
-class Analytics{
-  final FirebaseAnalytics _analytics = FirebaseAnalytics();
-
-  FirebaseAnalyticsObserver getAnalyticsObserver() => FirebaseAnalyticsObserver(analytics: _analytics);
-
-}
-
+// import 'package:firebase_analytics/firebase_analytics.dart';
+// import 'package:firebase_analytics/observer.dart';
+//
+// class Analytics{
+//   final FirebaseAnalytics _analytics = FirebaseAnalytics();
+//
+//   FirebaseAnalyticsObserver getAnalyticsObserver() => FirebaseAnalyticsObserver(analytics: _analytics);
+//
+// }
+//
