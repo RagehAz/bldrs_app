3f53936ce032d6305238790a510056cf42e861e6
Rageh El Azzazy <rageh.az@gmail.com>
2021-06-24 03:14:30 +0200

    - cleanups

---
+++ lib/views/widgets/appbar/search_bar.dart
@@ -0,0 +1,78 @@
+import 'package:bldrs/controllers/drafters/aligners.dart';
+import 'package:bldrs/controllers/drafters/scalers.dart';
+import 'package:bldrs/controllers/drafters/text_checkers.dart';
+import 'package:bldrs/controllers/theme/colorz.dart';
+import 'package:bldrs/controllers/theme/ratioz.dart';
+import 'package:bldrs/views/widgets/buttons/back_anb_search_button.dart';
+import 'package:bldrs/views/widgets/textings/super_text_field.dart';
+import 'package:flutter/material.dart';
+
+
+class SearchBar extends StatefulWidget {
+  const SearchBar({Key key}) : super(key: key);
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  TextEditingController _searchTextController = TextEditingController();
+
+  @override
+  void initState() {
+    _searchTextController.text = '';
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (TextChecker.textControllerHasNoValue(_searchTextController))_searchTextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    double _appBarClearWidth = Scale.appBarClearWidth(context);
+
+    return Container(
+      width: _appBarClearWidth,
+      height: Ratioz.appBarButtonSize + Ratioz.appBarPadding,
+      // color: Colorz.BloodTest,
+      alignment: Aligners.superTopAlignment(context),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+
+          Container(
+            width: 50,
+            height: 40,
+            // color: Colorz.LinkedIn,
+            alignment: Alignment.topLeft,
+            child: BackAndSearchButton(
+              backAndSearchAction: BackAndSearchAction.EnterSearch,
+            ),
+          ),
+
+          SuperTextField(
+            // fieldIsFormField: true,
+            width: _appBarClearWidth - (Ratioz.appBarButtonSize + Ratioz.appBarPadding * 3) - 3,
+            height: Ratioz.appBarButtonSize * 0.5,
+            textController: _searchTextController,
+            labelColor: Colorz.Yellow,
+            centered: false,
+            italic: true,
+            keyboardTextInputType: TextInputType.text,
+            keyboardTextInputAction: TextInputAction.done,
+            designMode: false,
+            counterIsOn: false,
+            fieldColor: null,
+            corners: Ratioz.appBarButtonCorner,
+          )
+
+        ],
+      ),
+    );
+  }
+}
