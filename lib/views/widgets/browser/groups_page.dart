d5cf801312177046a2efddd25f69b7260dba4768
Rageh El Azzazy <rageh.az@gmail.com>
2021-06-27 15:59:24 +0200

    - progressing

---
+++ lib/views/widgets/browser/groups_page.dart
@@ -0,0 +1,46 @@
+import 'package:bldrs/controllers/theme/colorz.dart';
+import 'package:bldrs/controllers/theme/ratioz.dart';
+import 'package:bldrs/views/widgets/buttons/dream_box.dart';
+import 'package:flutter/material.dart';
+
+class GroupsPage extends StatelessWidget {
+  final List<String> groups;
+  final Function onTap;
+  final String selectedGroup;
+
+  GroupsPage({
+    Key key,
+    @required this.groups,
+    @required this.onTap,
+    @required this.selectedGroup,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // width: 20,
+      // height: 20,
+      // color: _colors[pageIndex],
+      child: ListView.builder(
+          itemCount: groups.length,
+          padding: EdgeInsets.all(Ratioz.appBarMargin),
+          itemBuilder: (context, groupIndex){
+
+            String _group = groups[groupIndex];
+
+            Color _color = selectedGroup == _group ? Colorz.Yellow : Colorz.Nothing;
+
+            return
+              DreamBox(
+                height: 70,
+                // width: 120,
+                color: _color,
+                boxMargins: EdgeInsets.symmetric(vertical: Ratioz.appBarPadding),
+                verse: _group,
+                boxFunction: () => onTap(_group),
+              );
+          }
+      ),
+    );
+  }
+}
