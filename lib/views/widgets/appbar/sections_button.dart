faea1cf219828dd95209d98c9f2ad6a90df8bb43
Rageh El Azzazy <rageh.az@gmail.com>
2021-07-04 18:59:13 +0200

    - 7alawa

--- lib/views/widgets/appbar/sections_button.dart
+++ lib/views/widgets/appbar/sections_button.dart
@@ -1,5 +1,6 @@
 import 'package:bldrs/controllers/drafters/aligners.dart';
 import 'package:bldrs/controllers/drafters/iconizers.dart';
+import 'package:bldrs/controllers/drafters/launchers.dart';
 import 'package:bldrs/controllers/drafters/scalers.dart';
 import 'package:bldrs/controllers/drafters/text_generators.dart';
 import 'package:bldrs/controllers/router/navigators.dart';
@@ -7,28 +8,72 @@ import 'package:bldrs/controllers/theme/colorz.dart';
 import 'package:bldrs/controllers/theme/iconz.dart';
 import 'package:bldrs/controllers/theme/ratioz.dart';
 import 'package:bldrs/controllers/theme/wordz.dart';
+import 'package:bldrs/models/planet/area_model.dart';
+import 'package:bldrs/models/planet/province_model.dart';
+import 'package:bldrs/models/records/share_model.dart';
 import 'package:bldrs/models/section_class.dart';
 import 'package:bldrs/models/bz_model.dart';
+import 'package:bldrs/providers/country_provider.dart';
 import 'package:bldrs/providers/flyers_provider.dart';
 import 'package:bldrs/views/widgets/bubbles/in_pyramids_bubble.dart';
 import 'package:bldrs/views/widgets/bubbles/tile_bubble.dart';
 import 'package:bldrs/views/widgets/buttons/dream_box.dart';
 import 'package:bldrs/views/widgets/dialogs/alert_dialog.dart';
+import 'package:bldrs/views/widgets/dialogs/dialog_button.dart';
 import 'package:bldrs/views/widgets/textings/super_verse.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class SectionsButton extends StatelessWidget {
 // -----------------------------------------------------------------------------
-  Future<void> _onSectionTap({BuildContext context, FlyersProvider pro, Section section}) async {
+  Future<void> _onSectionTap({BuildContext context, FlyersProvider pro, Section section, bool inActiveMode}) async {
 
-    /// if section is not active * if user is author or not
+    CountryProvider _countryPro =  Provider.of<CountryProvider>(context, listen: false);
+    String _currentProvince = _countryPro.currentZone.provinceID;
 
-    /// if section is active
+    /// A - if section is not active * if user is author or not
+    if(inActiveMode == true){
+      print('cant go there ,, Bitch!!');
+
+      await superDialog(
+        context: context,
+        title: 'Section "${TextGenerator.sectionStringer(context, section)}"\nis Temporarily closed',
+        body: 'The Bldrs in $_currentProvince are adding flyers everyday to properly present their markets.\nplease hold for couple of days and come back again.',
+        child: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+
+              DialogButton(
+                verse: 'Inform a friend',
+                width: 133,
+                onTap: () => shareLink(context, LinkModel.bldrsWebSiteLink),
+              ),
+
+              DialogButton(
+                verse: 'Go back',
+                color: Colorz.Yellow,
+                verseColor: Colorz.BlackBlack,
+                onTap: () => Nav.goBack(context),
+              ),
+
+
+            ],
+          ),
+        ),
+      );
+    }
+
+    /// A - if section is active
+    else {
      await pro.changeSection(context, section);
 
-    /// close dialog
-    Nav.goBack(context);
+     /// B - close dialog
+     Nav.goBack(context);
+
+    }
+
 
   }
 // -----------------------------------------------------------------------------
@@ -93,12 +138,18 @@ void _changeSection(BuildContext context, FlyersProvider pro) async {
         // width: _buttonWidth,
         icon: _sectionIcon(section: section, inActiveMode: inActiveMode),
         verse: TextGenerator.sectionStringer(context, section),
-        verseScaleFactor: 0.6,
+        verseScaleFactor: 0.55,
         secondLine: TextGenerator.sectionDescriptionStringer(context, section),
         secondLineColor: Colorz.WhiteLingerie,
         margins: Ratioz.appBarPadding,
         inActiveMode: inActiveMode,
-        boxFunction: () => _onSectionTap(context: context, section: section, pro: pro),
+        boxFunction: () =>
+            _onSectionTap(
+                context: context,
+                section: section,
+                pro: pro,
+                inActiveMode: inActiveMode
+            ),
       );
   }
 
@@ -107,7 +158,7 @@ void _changeSection(BuildContext context, FlyersProvider pro) async {
     body: 'Select a section',
     height: _dialogHeight,
     child: Container(
-      height: _dialogHeight  - 60,
+      // height: _dialogHeight,
       // color: Colorz.BloodTest,
       child: Column(
         children: <Widget>[
