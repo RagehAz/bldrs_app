87df9e3a4812df85157b04edbc03b3523098a513
Rageh El Azzazy <rageh.az@gmail.com>
2021-09-18 07:17:58 +0200

    -----

---
+++ lib/models/target/target_reward.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+
+class TargetReward{
+  final String targetID;
+  final int ankh;
+  final int slides;
+
+  const TargetReward({
+    @required this.targetID,
+    @required this.ankh,
+    @required this.slides,
+  });
+}
