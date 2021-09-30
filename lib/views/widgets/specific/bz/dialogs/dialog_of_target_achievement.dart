3c0c92ae12f0b3491a7f4b8c7d3cb0aff648c441
Rageh El Azzazy <rageh.az@gmail.com>
2021-09-30 05:29:01 +0200

    - 7antafa

--- lib/views/widgets/specific/bz/dialogs/dialog_of_target_achievement.dart
+++ lib/views/widgets/specific/bz/dialogs/dialog_of_target_achievement.dart
@@ -2,7 +2,7 @@ import 'package:bldrs/controllers/theme/colorz.dart';
 import 'package:bldrs/models/bz/target/target_model.dart';
 import 'package:bldrs/views/widgets/general/dialogs/center_dialog/center_dialog.dart';
 import 'package:bldrs/views/widgets/general/textings/super_verse.dart';
-import 'package:bldrs/views/widgets/specific/bz/dialog_of_slides_and_ankhs.dart';
+import 'package:bldrs/views/widgets/specific/bz/dialogs/dialog_of_slides_and_ankhs.dart';
 import 'package:flutter/material.dart';
 
 class DialogOfTargetAchievement extends StatelessWidget {
