bb0ae01ceedbdb310e8bbbd948f8855dc5f5dcff
Rageh El Azzazy <rageh.az@gmail.com>
2021-10-09 11:40:10 +0200

    save bas before the coming disaster

--- lib/xxx_LABORATORY/CLEANING_SPACE.dart
+++ lib/xxx_LABORATORY/CLEANING_SPACE.dart
@@ -21,18 +21,17 @@ enum Activity {
 
 /// shall store customer journey map in firebase
 /// db : userID / {bzID} / {activityID}
-class CustomerJourney {
+class Record {
 
   final String userID; // docName
   final String bzID; // subDocName
+  final String flyerID;
   final Activity activity;
   final DateTime timeStamp;
-  final String flyerID;
   final int slideIndex;
   final double duration;
 
-
-  const CustomerJourney({
+  const Record({
     @required this.userID,
     @required this.bzID,
     @required this.activity,
@@ -41,6 +40,88 @@ class CustomerJourney {
     @required this.slideIndex,
     @required this.duration,
 });
+// -----------------------------------------------------------------------------
+  Map<String, dynamic> cipher(){
+
+    return
+        {
+          'userID' : userID,
+          'bzID' : bzID,
+          'activity' : cipherActivity(activity),
+          'timeStamp' : timeStamp,
+          'flyerID' : flyerID,
+          'slideIndex' : slideIndex,
+          'duration' : duration,
+        };
+
+  }
+// -----------------------------------------------------------------------------
+  static Record decipherRecord(Map<String, dynamic> map){
+
+    Record _record;
+
+    if (map != null){
+
+      _record = Record(
+        userID : map['userID'],
+        bzID : map['bzID'],
+        activity : decipherActivity(map['activity']),
+        timeStamp : map['timeStamp'].toDate(),
+        flyerID : map['flyerID'],
+        slideIndex : map['slideIndex'],
+        duration : map['duration'],
+      );
+
+    }
+
+    return _record;
+  }
+// -----------------------------------------------------------------------------
+  static String cipherActivity(Activity activity){
+    switch (activity){
+      case Activity.follow : return 'follow'; break;
+      case Activity.unfollow : return 'unfollow'; break;
+      case Activity.call : return 'call'; break;
+      case Activity.share : return 'share'; break;
+      case Activity.view : return 'view'; break;
+      case Activity.save : return 'save'; break;
+      case Activity.unSave : return 'unSave'; break;
+      case Activity.review : return 'review'; break;
+      case Activity.editReview : return 'editReview'; break;
+      case Activity.deleteReview : return 'deleteReview'; break;
+      case Activity.question : return 'question'; break;
+      case Activity.editQuestion : return 'editQuestion'; break;
+      case Activity.deleteQuestion : return 'deleteQuestion'; break;
+      case Activity.answer : return 'answer'; break;
+      case Activity.editAnswer : return 'editAnswer'; break;
+      case Activity.deleteAnswer : return 'deleteAnswer'; break;
+      default: return null;
+    }
+  }
+// -----------------------------------------------------------------------------
+  static Activity decipherActivity(String activity){
+    switch (activity){
+      case 'follow' : return Activity.follow; break;
+      case 'unfollow' : return Activity.unfollow; break;
+      case 'call' : return Activity.call; break;
+      case 'share' : return Activity.share; break;
+      case 'view' : return Activity.view; break;
+      case 'save' : return Activity.save; break;
+      case 'unSave' : return Activity.unSave; break;
+      case 'review' : return Activity.review; break;
+      case 'editReview' : return Activity.editReview; break;
+      case 'deleteReview' : return Activity.deleteReview; break;
+      case 'question' : return Activity.question; break;
+      case 'editQuestion' : return Activity.editQuestion; break;
+      case 'deleteQuestion' : return Activity.deleteQuestion; break;
+      case 'answer' : return Activity.answer; break;
+      case 'editAnswer' : return Activity.editAnswer; break;
+      case 'deleteAnswer' : return Activity.deleteAnswer; break;
+      default : return null;
+    }
+
+  }
+// -----------------------------------------------------------------------------
 
 }
 
