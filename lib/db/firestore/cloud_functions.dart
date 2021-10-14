7daea60e9437ce40fc069b171673af2375500d29
Rageh El Azzazy <rageh.az@gmail.com>
2021-10-15 01:44:56 +0200

    -Allah ynawar yasta,, fixed USA and KSA and zones are good for now

--- lib/db/firestore/cloud_functions.dart
+++ lib/db/firestore/cloud_functions.dart
@@ -3,6 +3,30 @@ import 'package:bldrs/views/widgets/general/dialogs/center_dialog/center_dialog.
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 
+/*
+CLOUD FUNCTIONS ARTICLES
+
+check this
+https://jsmobiledev.com/article/firestore-functions/
+
+AND THIS
+https://stackoverflow.com/questions/66194726/how-do-i-query-a-token-and-send-push-notifications-inside-cloud-function
+
+AND THIS
+https://stackoverflow.com/questions/37990140/how-to-send-one-to-one-message-using-firebase-messaging
+
+AND THIS
+https://firebase.google.com/docs/cloud-messaging/android/client
+
+AND THIS
+https://engineering.monstar-lab.com/2021/02/09/Use-Firebase-Cloudfunctions-To-Send-Push-Notifications
+
+// -----------------------------------------------------------------------------
+*/
+
+
+
+
 class CloudFunctionz{
 
   static const String funcName_myFunction = 'myFunction';
