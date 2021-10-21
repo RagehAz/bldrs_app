0c62616e20b5b9f7a5c22f87c41f6417b6ca7991
Rageh El Azzazy <rageh.az@gmail.com>
2021-10-21 18:36:19 +0200

    - no matter how much I work,,, I feel nothing has been done,, why the fuck is that

---
+++ lib/views/widgets/general/buttons/obelisk_button.dart
@@ -0,0 +1,36 @@
+import 'package:bldrs/controllers/router/navigators.dart';
+import 'package:bldrs/controllers/theme/colorz.dart';
+import 'package:bldrs/views/widgets/general/buttons/main_button.dart';
+import 'package:flutter/material.dart';
+
+class ObeliskButton extends StatelessWidget {
+
+  final String title;
+  final String icon;
+  final Widget screen;
+  final Color color;
+
+  const ObeliskButton(
+      this.title,
+      this.icon,
+      this.screen,
+      {this.color = Colorz.black125}
+      );
+
+  @override
+  Widget build(BuildContext context) {
+
+    final Color _color = color == null ? Colorz.black125 : color;
+
+    return MainButton(
+      buttonVerse: title,
+      buttonColor: _color,
+      buttonIcon: icon,
+      buttonVerseShadow: true,
+      splashColor: Colorz.yellow255,
+      function: () => Nav.goToNewScreen(context, screen),
+      stretched: false,
+    );
+
+  }
+}
--- lib/views/widgets/general/nav_bar/nav_bar.dart
+++ lib/views/widgets/general/nav_bar/nav_bar.dart
@@ -15,6 +15,7 @@ import 'package:bldrs/models/user/user_model.dart';
 import 'package:bldrs/providers/bzz_provider.dart';
 import 'package:bldrs/providers/flyers_provider.dart';
 import 'package:bldrs/providers/streamers/user_streamer.dart';
+import 'package:bldrs/providers/user_provider.dart';
 import 'package:bldrs/views/screens/d_more/d_0_more_screen.dart';
 import 'package:bldrs/views/screens/f_bz/f_0_my_bz_screen.dart';
 import 'package:bldrs/views/screens/g_user/g_0_profile_screen.dart';
@@ -154,6 +155,8 @@ class NavBar extends StatelessWidget {
 // -----------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
+    final UsersProvider _usersProvider = Provider.of<UsersProvider>(context, listen: true);
+    final UserModel _myUserModel = _usersProvider.myUserModel;
 // -----------------------------------------------------------------------------
     final double _screenWidth = Scale.superScreenWidth(context);
     final double _buttonCircleCorner = Scale.buttonCircleCorner;
@@ -167,186 +170,178 @@ class NavBar extends StatelessWidget {
 // -----------------------------------------------------------------------------
     final Widget _expander = _deviceIsIOS ? Expanded(child: Container(),) : Container();
 // -----------------------------------------------------------------------------
+
+    final double _spacerWidth = Scale.navBarSpacerWidth(context, _myUserModel);
+    final Widget _spacer = SizedBox(width: _spacerWidth,);
+    final Widget _halfSpacer = SizedBox(width: _spacerWidth * 0.5,);
+
+    final double _boxWidth = Scale.navBarWidth(context: context, userModel: _myUserModel);
+
+    final List<String> _userBzzIDs = BzModel.getBzzIDsFromBzz(myBzz);
+
+    // List<dynamic> _followedBzzIDs = _myUserModel != null ? _myUserModel?.followedBzzIDs : [];
+    // String _bzID = _followedBzzIDs.length > 0 ?  _followedBzzIDs[0] : '';
+    // String _bzLogo = prof.getBzByBzID(_bzID)?.bzLogo;
+
+
     return
 
-       userStreamBuilder(
-           context: context,
-           listen: false,
-           builder: (context, UserModel userModel){
+      Positioned(
+        bottom: _bottomOffset,
+        child: Container(
+          width: _screenWidth,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
 
-             final double _spacerWidth = Scale.navBarSpacerWidth(context, userModel);
-             final Widget _spacer = SizedBox(width: _spacerWidth,);
-             final Widget _halfSpacer = SizedBox(width: _spacerWidth * 0.5,);
+              /// ios back button
+              if (_deviceIsIOS)
+                DreamBox(
+                  height: _circleWidth,
+                  width: _circleWidth,
+                  color: sky == Sky.Black ? Colorz.yellow50 : Colorz.white20,
+                  corners: _buttonCircleCorner,
+                  margins: const EdgeInsets.all(Ratioz.appBarPadding),
+                  icon: Iconizer.superBackIcon(context),
+                  blur : Ratioz.blur1,
+                  onTap: () => Nav.goBack(context),
+                ),
 
-             final double _boxWidth = Scale.navBarWidth(context: context, userModel: userModel);
+              _expander,
 
-             final List<String> _userBzzIDs = BzModel.getBzzIDsFromBzz(myBzz);
+              /// navBar widgets
+              Container(
+                width: _boxWidth,
+                height: _boxHeight,
+                decoration: BoxDecoration(
+                  color: Colorz.black230,
+                  borderRadius: _boxCorners,
+                  boxShadow: Shadowz.appBarShadow,
+                ),
+                child: Stack(
+                  children: <Widget>[
 
-             // List<dynamic> _followedBzzIDs = userModel != null ? userModel?.followedBzzIDs : [];
-             // String _bzID = _followedBzzIDs.length > 0 ?  _followedBzzIDs[0] : '';
-             // String _bzLogo = prof.getBzByBzID(_bzID)?.bzLogo;
+                    /// --- BLUR LAYER
+                    BlurLayer(
+                      width: _boxWidth,
+                      height: _boxHeight,
+                      blur: Ratioz.blur1,
+                      borders: _boxCorners,
+                    ),
 
-                 return
+                    /// BUTTONS
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
 
-               Positioned(
-                 bottom: _bottomOffset,
-                 child: Container(
-                   width: _screenWidth,
-                   child: Row(
-                     mainAxisAlignment: MainAxisAlignment.center,
-                     crossAxisAlignment: CrossAxisAlignment.center,
-                     children: <Widget>[
+                        _halfSpacer,
 
-                       /// ios back button
-                       if (_deviceIsIOS)
-                         DreamBox(
-                           height: _circleWidth,
-                           width: _circleWidth,
-                           color: sky == Sky.Black ? Colorz.yellow50 : Colorz.white20,
-                           corners: _buttonCircleCorner,
-                           margins: const EdgeInsets.all(Ratioz.appBarPadding),
-                           icon: Iconizer.superBackIcon(context),
-                           blur : Ratioz.blur1,
-                           onTap: () => Nav.goBack(context),
-                         ),
-
-                       _expander,
-
-                       /// navBar widgets
-                       Container(
-                         width: _boxWidth,
-                         height: _boxHeight,
-                         decoration: BoxDecoration(
-                           color: Colorz.black230,
-                           borderRadius: _boxCorners,
-                           boxShadow: Shadowz.appBarShadow,
-                         ),
-                         child: Stack(
-                           children: <Widget>[
-
-                             /// --- BLUR LAYER
-                             BlurLayer(
-                               width: _boxWidth,
-                               height: _boxHeight,
-                               blur: Ratioz.blur1,
-                               borders: _boxCorners,
-                             ),
-
-                             /// BUTTONS
-                             Row(
-                               mainAxisAlignment: MainAxisAlignment.center,
-                               crossAxisAlignment: CrossAxisAlignment.center,
-                               children: <Widget>[
-
-                                 _halfSpacer,
-
-                                 /// SAVED FLYERS
-                                 BarButton(
-                                   width: _buttonWidth,
-                                   text: 'Choices',
-                                   icon: Iconz.SaveOn,
-                                   iconSizeFactor: 0.7,
-                                   barType: _barType,
-                                   onTap: () => Nav.goToRoute(context, Routez.SavedFlyers),
-                                 ),
+                        /// SAVED FLYERS
+                        BarButton(
+                          width: _buttonWidth,
+                          text: 'Choices',
+                          icon: Iconz.SaveOn,
+                          iconSizeFactor: 0.7,
+                          barType: _barType,
+                          onTap: () => Nav.goToRoute(context, Routez.SavedFlyers),
+                        ),
 
 
-                                 _spacer,
+                        _spacer,
 
-                                 /// MORE
-                                 BarButton(
-                                   width: _buttonWidth,
-                                   text: Wordz.more(context),
-                                   icon: Iconz.More,
-                                   iconSizeFactor: 0.45,
-                                   barType: _barType,
-                                   onTap: (){
-                                     print('fish');
-                                     Nav.goToNewScreen(context, MoreScreen(userModel: userModel));
-                                     },
-                                 ),
+                        /// MORE
+                        BarButton(
+                          width: _buttonWidth,
+                          text: Wordz.more(context),
+                          icon: Iconz.More,
+                          iconSizeFactor: 0.45,
+                          barType: _barType,
+                          onTap: (){
+                            print('fish');
+                            Nav.goToNewScreen(context, MoreScreen(userModel: _myUserModel));
+                          },
+                        ),
 
-                                 _spacer,
+                        _spacer,
 
-                                 /// BZZ BUTTON
-                                  if (UserModel.userIsAuthor(userModel))
-                                 BzzButton(
-                                   width: _buttonWidth,
-                                   circleWidth: _circleWidth,
-                                   barType: _barType,
-                                   // bzzIDs: _userBzzIDs,
-                                   onTap: (){
-                                     print('fish');
+                        /// BZZ BUTTON
+                        if (UserModel.userIsAuthor(_myUserModel))
+                          BzzButton(
+                            width: _buttonWidth,
+                            circleWidth: _circleWidth,
+                            barType: _barType,
+                            // bzzIDs: _userBzzIDs,
+                            onTap: (){
+                              print('fish');
 
-                                     if (_userBzzIDs.length == 1){
-                                       Nav.goToNewScreen(context, MyBzScreen(
-                                         userModel: userModel,
-                                         bzModel: myBzz[0],
-                                       ));
-                                     } else {
-                                       _multiBzzSlider(context, userModel);
-                                     }
+                              if (_userBzzIDs.length == 1){
+                                Nav.goToNewScreen(context, MyBzScreen(
+                                  userModel: _myUserModel,
+                                  bzModel: myBzz[0],
+                                ));
+                              } else {
+                                _multiBzzSlider(context, _myUserModel);
+                              }
 
 
-                                   },
-                                 ),
+                            },
+                          ),
 
-                                 _spacer,
+                        _spacer,
 
-                                 /// NEWS
-                                 BarButton(
-                                   width: _buttonWidth,
-                                   text: Wordz.news(context),
-                                   icon: Iconz.News,
-                                   iconSizeFactor: 0.45,
-                                   barType: _barType,
-                                   onTap: () {
-                                     Nav.goToNewScreen(context, NotificationsScreen());
-                                   },
-                                 ),
+                        /// NEWS
+                        BarButton(
+                          width: _buttonWidth,
+                          text: Wordz.news(context),
+                          icon: Iconz.News,
+                          iconSizeFactor: 0.45,
+                          barType: _barType,
+                          onTap: () {
+                            Nav.goToNewScreen(context, NotificationsScreen());
+                          },
+                        ),
 
-                                 _spacer,
+                        _spacer,
 
-                                 /// PROFILE
-                                 BarButton(
-                                     width: _buttonWidth,
-                                     text: Wordz.profile(context),
-                                     icon: Iconz.NormalUser,
-                                     iconSizeFactor: 0.7,
-                                     barType: _barType,
-                                     onTap: () => Nav.goToNewScreen(context, UserProfileScreen(userModel: userModel,)),
-                                     clipperWidget : UserBalloon(
-                                       balloonWidth: _circleWidth,
-                                       loading: false,
-                                       userModel: userModel,
-                                     )
-                                 ),
+                        /// PROFILE
+                        BarButton(
+                            width: _buttonWidth,
+                            text: Wordz.profile(context),
+                            icon: Iconz.NormalUser,
+                            iconSizeFactor: 0.7,
+                            barType: _barType,
+                            onTap: () => Nav.goToNewScreen(context, UserProfileScreen(userModel: _myUserModel,)),
+                            clipperWidget : UserBalloon(
+                              balloonWidth: _circleWidth,
+                              loading: false,
+                              userModel: _myUserModel,
+                            )
+                        ),
 
-                                 _halfSpacer,
+                        _halfSpacer,
 
-                               ],
-                             ),
-                           ],
-                         ),
-                       ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
 
-                       _expander,
+              _expander,
 
-                       /// IOS balance container
-                       if (_deviceIsIOS)
-                         Container(
-                           width: _circleWidth,
-                           height: _circleWidth,
-                           margin: const EdgeInsets.all(Ratioz.appBarPadding),
-                         ),
+              /// IOS balance container
+              if (_deviceIsIOS)
+                Container(
+                  width: _circleWidth,
+                  height: _circleWidth,
+                  margin: const EdgeInsets.all(Ratioz.appBarPadding),
+                ),
 
-                     ],
-                   ),
-                 ),
-               );
-
-               }
-            );
+            ],
+          ),
+        ),
+      );
 
   }
 }
