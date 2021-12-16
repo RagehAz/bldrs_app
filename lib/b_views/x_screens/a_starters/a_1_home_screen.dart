9e8a85b141b139c286c578ed1e16e334656e0002
RagehAz <rageh.az@gmail.com>
2021-12-17 01:55:31 +0200

    - optimizing select city screen and its views

--- lib/b_views/x_screens/a_starters/a_1_home_screen.dart
+++ lib/b_views/x_screens/a_starters/a_1_home_screen.dart
@@ -40,7 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
       _triggerLoading().then((_) async {
 
-        await controlHomeScreen(context);
+        await initializeHomeScreen(context);
 
         await _triggerLoading();
       });
--- lib/b_views/x_screens/b_auth/b_0_auth_screen.dart
+++ lib/b_views/x_screens/b_auth/b_0_auth_screen.dart
@@ -1,17 +1,10 @@
 import 'dart:async';
 
 import 'package:bldrs/a_models/user/user_model.dart';
-import 'package:bldrs/b_views/widgets/general/artworks/bldrs_name_logo_slogan.dart';
-import 'package:bldrs/b_views/widgets/general/buttons/main_button.dart';
 import 'package:bldrs/b_views/widgets/general/layouts/main_layout/main_layout.dart';
 import 'package:bldrs/b_views/y_views/b_auth/b_0_auth_screen_view.dart';
 import 'package:bldrs/c_controllers/b_0_auth_controller.dart';
-import 'package:bldrs/f_helpers/drafters/device_checkers.dart' as DeviceChecker;
-import 'package:bldrs/f_helpers/router/route_names.dart';
-import 'package:bldrs/f_helpers/theme/colorz.dart';
 import 'package:bldrs/f_helpers/theme/iconz.dart' as Iconz;
-import 'package:bldrs/f_helpers/theme/ratioz.dart';
-import 'package:bldrs/f_helpers/theme/wordz.dart' as Wordz;
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -32,7 +25,7 @@ class _AuthScreenState extends State<AuthScreen> {
 // -----------------------------------------------------------------------------
   Future<void> _auth(BuildContext context, AuthBy authBy) async {
 
-    await controlAuth(context, authBy);
+    await controlOnAuth(context, authBy);
 
   }
 // -----------------------------------------------------------------------------
