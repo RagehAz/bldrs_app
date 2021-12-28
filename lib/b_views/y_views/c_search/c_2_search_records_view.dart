88156077b2e6c8389fe5b57ff8ba4b3ebfa5401a
RagehAz <rageh.az@gmail.com>
2021-12-28 21:48:22 +0200

    - this work like a charm

--- lib/b_views/y_views/c_search/c_2_search_records_view.dart
+++ lib/b_views/y_views/c_search/c_2_search_records_view.dart
@@ -2,19 +2,22 @@ import 'package:bldrs/a_models/flyer/records/record_model.dart';
 import 'package:bldrs/b_views/widgets/general/buttons/dream_box/dream_box.dart';
 import 'package:bldrs/c_controllers/c_0_search_controller.dart';
 import 'package:bldrs/d_providers/general_provider.dart';
+import 'package:bldrs/e_db/fire/methods/firestore.dart';
+import 'package:bldrs/e_db/fire/methods/paths.dart';
 import 'package:bldrs/f_helpers/drafters/scalers.dart';
-import 'package:bldrs/f_helpers/drafters/timerz.dart';
 import 'package:bldrs/f_helpers/theme/iconz.dart' as Iconz;
 import 'package:bldrs/f_helpers/theme/ratioz.dart';
-import 'package:bldrs/xxx_dashboard/widgets/wide_button.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class SearchRecordsView extends StatelessWidget {
 // -----------------------------------------------------------------------------
   const SearchRecordsView({
+    @required this.scrollController,
     Key key
   }) : super(key: key);
+// -----------------------------------------------------------------------------
+  final ScrollController scrollController;
 // -----------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
@@ -22,29 +25,69 @@ class SearchRecordsView extends StatelessWidget {
     final GeneralProvider _generalProvider = Provider.of<GeneralProvider>(context, listen: true);
     final List<RecordModel> _searchRecords = _generalProvider.searchRecords;
 
+    _searchRecords.sort((RecordModel a, RecordModel b){
+      final DateTime _timeA = a.timeStamp;
+      final DateTime _timeB = b.timeStamp;
+      return _timeB.compareTo(_timeA);
+    });
+
+    // _searchRecords.reversed;
+
     return ListView.builder(
         itemCount: _searchRecords.length,
-        reverse: true,
+        shrinkWrap: true,
+        controller: scrollController,
         physics: const BouncingScrollPhysics(),
         padding: const EdgeInsets.only(top: Ratioz.appBarBigHeight + Ratioz.appBarMargin * 2, bottom: Ratioz.horizon),
         itemBuilder: (BuildContext context, int index) {
 
           final RecordModel _searchRecord = _searchRecords[index];
 
-          return DreamBox(
-            height: 50,
-            width: superScreenWidth(context) - (2 * Ratioz.appBarMargin),
-            verse: _searchRecord?.recordDetails.toString(),
-            icon: Iconz.clock,
-            iconSizeFactor: 0.4,
-            verseCentered: false,
-            verseScaleFactor: 1.6,
-            margins: const EdgeInsets.only(bottom: Ratioz.appBarMargin),
-            onTap: () => onSearchRecordTap(
-              context: context,
-              record: _searchRecord,
-            ),
-          );
+          return
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+
+                DreamBox(
+                  height: 50,
+                  width: superScreenWidth(context) - (2 * Ratioz.appBarMargin) - 60,
+                  verse: _searchRecord?.recordDetails.toString(),
+                  icon: Iconz.clock,
+                  iconSizeFactor: 0.4,
+                  verseCentered: false,
+                  verseScaleFactor: 1.6,
+                  margins: const EdgeInsets.only(bottom: Ratioz.appBarMargin),
+                  onTap: () => onSearchRecordTap(
+                    context: context,
+                    record: _searchRecord,
+                  ),
+                ),
+
+                DreamBox(
+                  width: 50,
+                  height: 50,
+                  margins: const EdgeInsets.only(left: 5, right: 5, bottom: Ratioz.appBarMargin),
+                  icon: Iconz.xLarge,
+                  iconSizeFactor: 0.4,
+                  onTap: () async {
+
+                    await deleteDoc(
+                        context: context,
+                        collName: FireColl.records,
+                        docName: _searchRecord.recordID,
+                    );
+
+                    _generalProvider.deleteSearchRecord(_searchRecord);
+
+                  },
+                ),
+
+
+              ],
+
+            )
+
+            ;
 
         });
   }
