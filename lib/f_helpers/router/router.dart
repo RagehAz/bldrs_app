87afbe8f9b0196d4d350cd1ca10010b2efe8367c
RagehAz <rageh.az@gmail.com>
2022-01-06 23:29:02 +0200

    - TAMAM KEDA SAVED FLYERS SCREEN VIEW CONTROLLER BABY EL7

--- lib/f_helpers/router/router.dart
+++ lib/f_helpers/router/router.dart
@@ -1,7 +1,7 @@
 import 'package:bldrs/b_views/x_screens/a_starters/a_0_logo_screen.dart';
 import 'package:bldrs/b_views/x_screens/b_auth/b_0_auth_screen.dart';
 import 'package:bldrs/b_views/x_screens/c_search/c_0_search_screen.dart';
-import 'package:bldrs/b_views/x_screens/e_saves/e_0_old_saved_flyers_screen.dart';
+import 'package:bldrs/b_views/x_screens/e_saves/e_0_saved_flyers_screen.dart';
 import 'package:bldrs/b_views/x_screens/h_notifications/g_1_notifications_screen.dart';
 import 'package:bldrs/b_views/x_screens/i_flyer/h_0_flyer_screen.dart';
 import 'package:bldrs/f_helpers/router/navigators.dart' as Nav;
@@ -33,7 +33,7 @@ Route<dynamic> allRoutes(RouteSettings settings) {
 // -----------------------------------------------------------------------------
     /// s12
     case Routez.savedFlyers:
-      return Nav.slideToScreen(const OldSavedFlyersScreen(), settings);
+      return Nav.slideToScreen(const SavedFlyersScreen(), settings);
       break;
 // -----------------------------------------------------------------------------
     /// s13
