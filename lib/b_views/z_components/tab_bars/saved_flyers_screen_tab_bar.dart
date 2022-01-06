c59abdaa8944cff42f9e8c07bf553976bf224554
RagehAz <rageh.az@gmail.com>
2022-01-06 23:29:02 +0200

    - TAMAM KEDA SAVED FLYERS SCREEN VIEW CONTROLLER BABY EL7

---
+++ lib/b_views/z_components/tab_bars/saved_flyers_screen_tab_bar.dart
@@ -0,0 +1,63 @@
+import 'package:bldrs/a_models/flyer/sub/flyer_type_class.dart';
+import 'package:bldrs/b_views/widgets/general/buttons/tab_button.dart';
+import 'package:bldrs/b_views/z_components/tab_bars/bldrs_sliver_tab_bar.dart';
+import 'package:bldrs/c_controllers/e_saves_controller.dart';
+import 'package:bldrs/f_helpers/drafters/iconizers.dart' as Iconizer;
+import 'package:bldrs/f_helpers/drafters/text_generators.dart' as TextGen;
+import 'package:flutter/material.dart';
+
+class SavedFlyersTabBar extends StatelessWidget {
+  /// --------------------------------------------------------------------------
+  const SavedFlyersTabBar({
+    @required this.tabController,
+    @required this.currentFlyerTypeTab,
+    Key key
+  }) : super(key: key);
+  /// --------------------------------------------------------------------------
+  final FlyerType currentFlyerTypeTab;
+  final TabController tabController;
+  /// --------------------------------------------------------------------------
+  bool _isSelected(FlyerType flyerType){
+
+    bool _isSelected = false;
+
+    if (currentFlyerTypeTab == flyerType){
+      _isSelected = true;
+    }
+
+    return _isSelected;
+  }
+// -----------------------------------------------------------------------------
+
+  @override
+  Widget build(BuildContext context) {
+
+    return BldrsSliverTabBar(
+      tabController: tabController,
+      tabs: <Widget>[
+
+        /// IT HAS TO BE LIST.GENERATE ma3lesh
+        ...List.generate(sectionsList.length, (index){
+
+          final FlyerType _flyerType = sectionsList[index];
+          final String _flyerTypeString = cipherFlyerType(_flyerType);
+
+          return
+            TabButton(
+              key: ValueKey<String>('saved_flyer_tab_button_$_flyerTypeString'),
+              verse: TextGen.flyerTypePluralStringer(context, _flyerType),
+              icon: Iconizer.flyerTypeIconOff(_flyerType),
+              isSelected: _isSelected(_flyerType),
+              onTap: () => onChangeTabIndex(
+                context: context,
+                tabController: tabController,
+                index: index,
+              ),
+            );
+
+        }),
+
+      ],
+    );
+  }
+}
