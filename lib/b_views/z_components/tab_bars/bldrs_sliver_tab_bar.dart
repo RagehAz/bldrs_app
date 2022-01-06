8ab6146ff395b23dd0acff5a43e13aef7cb36e2a
RagehAz <rageh.az@gmail.com>
2022-01-06 23:29:02 +0200

    - TAMAM KEDA SAVED FLYERS SCREEN VIEW CONTROLLER BABY EL7

---
+++ lib/b_views/z_components/tab_bars/bldrs_sliver_tab_bar.dart
@@ -0,0 +1,54 @@
+import 'package:bldrs/f_helpers/drafters/borderers.dart' as Borderers;
+import 'package:bldrs/f_helpers/drafters/tracers.dart';
+import 'package:bldrs/f_helpers/theme/colorz.dart';
+import 'package:bldrs/f_helpers/theme/ratioz.dart';
+import 'package:flutter/material.dart';
+
+class BldrsSliverTabBar extends StatelessWidget {
+  /// --------------------------------------------------------------------------
+  const BldrsSliverTabBar({
+    @required this.tabs,
+    @required this.tabController,
+    Key key
+  }) : super(key: key);
+  /// --------------------------------------------------------------------------
+  final List<Widget> tabs;
+  final TabController tabController;
+  /// --------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabs.length,
+      child: SliverAppBar(
+        collapsedHeight: Ratioz.stratosphere,
+        backgroundColor: Colorz.blackSemi230,
+        leadingWidth: 0,
+        leading: Container(),
+        floating: true,
+        bottom: TabBar(
+          controller: tabController,
+          physics: const BouncingScrollPhysics(),
+          // labelColor: Colorz.BloodTest,
+
+          // indicatorColor: Colorz.BloodTest,
+          indicatorSize: TabBarIndicatorSize.tab,
+          indicatorWeight: 0,
+          indicator: BoxDecoration(
+            color: Colorz.yellow255,
+            borderRadius: Borderers.superBorderAll(context, Ratioz.boxCorner12 + 2.5),
+          ),
+
+          isScrollable: true,
+          padding: const EdgeInsets.symmetric(horizontal: Ratioz.appBarMargin),
+          labelPadding: const EdgeInsets.symmetric(horizontal: Ratioz.appBarPadding),
+
+          onTap: (int x){
+            blog('x is : $x');
+          },
+
+          tabs: tabs,
+        ),
+      ),
+    );
+  }
+}
--- lib/c_controllers/e_saves_controller.dart
+++ lib/c_controllers/e_saves_controller.dart
@@ -1,147 +1,10 @@
 import 'package:bldrs/a_models/flyer/flyer_model.dart';
 import 'package:bldrs/a_models/flyer/sub/flyer_type_class.dart';
-import 'package:bldrs/b_views/widgets/general/buttons/tab_button.dart';
-import 'package:bldrs/b_views/widgets/specific/flyer/stacks/saved_flyers_grid.dart';
-import 'package:bldrs/b_views/z_components/layouts/tab_layout_model.dart';
 import 'package:bldrs/d_providers/flyers_provider.dart';
 import 'package:bldrs/d_providers/ui_provider.dart';
-import 'package:bldrs/f_helpers/drafters/iconizers.dart' as Iconizer;
-import 'package:bldrs/f_helpers/drafters/text_generators.dart' as TextGen;
 import 'package:bldrs/f_helpers/theme/ratioz.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-// -----------------------------------------------------------------------------
-const List<FlyerType> sectionsTabs = sectionsList;
-// -----------------------------------------------------------------------------
-void createSavedFlyersTabModels({
-  @required BuildContext context,
-  @required TabController tabController,
-  @required bool selectionMode,
-  @required List<FlyerModel> allFlyers,
-}){
-
-  final List<TabModel> _models = <TabModel>[];
-
-
-  for (int i = 0; i < sectionsTabs.length; i++) {
-
-    _models.add(
-
-        TabModel(
-
-          tabButton: _createSectionTabButton(
-              context: context,
-              tabIndex: i,
-              tabController: tabController
-          ),
-          page: _createSavedFlyersGridPage(
-            context: context,
-            selectionMode: selectionMode,
-            allFlyers: allFlyers,
-            section: sectionsTabs[i],
-          ),
-        )
-
-    );
-  }
-
-  // return _models;
-
-  final UiProvider _uiProvider = Provider.of<UiProvider>(context, listen: false);
-  _uiProvider.setSavedFlyersTabModels(_models);
-
-}
-// -----------------------------------------------------------------------------
-Widget _createSectionTabButton({
-  @required BuildContext context,
-  @required int tabIndex,
-  @required TabController tabController,
-}){
-
-  final UiProvider _uiProvider = Provider.of<UiProvider>(context, listen: false);
-  final int _currentTabIndex = _uiProvider.savedFlyersCurrentTabIndex;
-
-  const List<FlyerType> _sectionsTabs = sectionsList;
-  final FlyerType _section = _sectionsTabs[tabIndex];
-
-  return
-    TabButton(
-      key: ValueKey<String>('saved_flyer_tab_button_$tabIndex'),
-      verse: TextGen.flyerTypePluralStringer(context, _section),
-      icon: Iconizer.flyerTypeIconOff(_section),
-      isSelected: _sectionsTabs[_currentTabIndex] == _section,
-      onTap: () {
-
-        onSetCurrentTab(
-          context: context,
-          tabIndex: tabIndex,
-          tabController: tabController,
-        );
-
-      },
-    );
-}
-// -----------------------------------------------------------------------------
-Widget _createSavedFlyersGridPage({
-  @required BuildContext context,
-  @required bool selectionMode,
-  @required List<FlyerModel> allFlyers,
-  @required FlyerType section,
-}){
-
-  final FlyersProvider _flyersProvider = Provider.of<FlyersProvider>(context, listen: false);
-  final List<FlyerModel> _selectedFlyers = _flyersProvider.selectedFlyers;
-
-  return
-    SavedFlyersGrid(
-      key: ValueKey<String>('saved_flyers_grid_page_$section'),
-      selectionMode: selectionMode,
-      onSelectFlyer: (FlyerModel flyer) => onSelectFlyerFromSavedFlyers(
-        context: context,
-        flyer: flyer,
-      ),
-      selectedFlyers: _selectedFlyers,
-      flyers: FlyerModel.filterFlyersByFlyerType(
-        flyers: allFlyers,
-        flyerType: section,
-      ),
-    );
-
-}
-// -----------------------------------------------------------------------------
-void onSetCurrentTab({
-  @required BuildContext context,
-  @required int tabIndex,
-  @required TabController tabController,
-}){
-
-  /// set saved flyers current tab index
-  final UiProvider _uiProvider = Provider.of<UiProvider>(context, listen: false);
-  _uiProvider.setSavedFlyersCurrentTabIndex(tabIndex);
-
-  /// REBUILD TAB MODELS
-  _uiProvider.setSavedFlyersCurrentTabIndex(tabIndex);
-  // setState(() {
-  //   _tabModels = createTabModels();
-  // });
-
-
-  /// ANIMATE
-  tabController.animateTo(tabIndex,
-      curve: Curves.easeIn,
-      duration: Ratioz.duration150ms
-  );
-
-}
-// -----------------------------------------------------------------------------
-/// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-/// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-/// -----------------------------------------------------------------------------
-// -----------------------------------------------------------------------------
-/// THE NEW CORRECT SEXY STUFF BABY
 // -----------------------------------------------------------------------------
 int getInitialTabIndex(BuildContext context){
   final UiProvider _uiProvider = Provider.of<UiProvider>(context, listen: false);
@@ -176,7 +39,7 @@ void onChangeTabIndex({
 
   final UiProvider _uiProvider = Provider.of<UiProvider>(context, listen: false);
 
-  final FlyerType _newFlyerType = sectionsTabs[index];
+  final FlyerType _newFlyerType = sectionsList[index];
   final FlyerType _previousTab = _uiProvider.currentSavedFlyerTypeTab;
 
   /// ONLY WHEN THE TAB CHANGES FOR REAL IN THE EXACT MIDDLE BETWEEN BUTTONS
