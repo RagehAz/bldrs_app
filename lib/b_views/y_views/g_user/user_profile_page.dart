d63455163925bbdef1490205aff56748cb34c937
RagehAz <rageh.az@gmail.com>
2022-01-09 01:11:21 +0200

    - konna ben2ool ehh,, ana gamed wenta la22a

--- lib/b_views/y_views/g_user/user_profile_page.dart
+++ lib/b_views/y_views/g_user/user_profile_page.dart
@@ -1,21 +1,18 @@
 import 'package:bldrs/a_models/user/user_model.dart';
 import 'package:bldrs/b_views/widgets/general/textings/super_verse.dart';
-import 'package:bldrs/b_views/z_components/sizing/expander.dart';
 import 'package:flutter/material.dart';
 
 class UserProfilePage extends StatelessWidget {
-
+  /// --------------------------------------------------------------------------
   const UserProfilePage({
     @required this.userModel,
     Key key
   }) : super(key: key);
-
+  /// --------------------------------------------------------------------------
   final UserModel userModel;
-
+  /// --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-
-    blog('BUILDING UserProfilePage');
 
     return Center(
       child: SuperVerse(
