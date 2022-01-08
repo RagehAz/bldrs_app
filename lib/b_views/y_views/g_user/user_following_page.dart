a93ef4eb7fc5922a46d4f977091c832630301c05
RagehAz <rageh.az@gmail.com>
2022-01-09 01:11:21 +0200

    - konna ben2ool ehh,, ana gamed wenta la22a

--- lib/b_views/y_views/g_user/user_following_page.dart
+++ lib/b_views/y_views/g_user/user_following_page.dart
@@ -1,21 +1,18 @@
 import 'package:bldrs/a_models/user/user_model.dart';
 import 'package:bldrs/b_views/widgets/general/textings/super_verse.dart';
-import 'package:bldrs/b_views/z_components/sizing/expander.dart';
 import 'package:flutter/material.dart';
 
 class UserFollowingPage extends StatelessWidget {
-
+  /// --------------------------------------------------------------------------
   const UserFollowingPage({
     @required this.userModel,
     Key key
   }) : super(key: key);
-
+  /// --------------------------------------------------------------------------
   final UserModel userModel;
-
+  /// --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-
-    blog('BUILDING UserFollowingPage');
 
     return Center(
       child: SuperVerse(
--- lib/b_views/y_views/g_user/user_states_page.dart
+++ lib/b_views/y_views/g_user/user_states_page.dart
@@ -1,21 +1,18 @@
 import 'package:bldrs/a_models/user/user_model.dart';
 import 'package:bldrs/b_views/widgets/general/textings/super_verse.dart';
-import 'package:bldrs/b_views/z_components/sizing/expander.dart';
 import 'package:flutter/material.dart';
 
 class UserStatsPage extends StatelessWidget {
-
+  /// --------------------------------------------------------------------------
   const UserStatsPage({
     @required this.userModel,
     Key key
   }) : super(key: key);
-
+  /// --------------------------------------------------------------------------
   final UserModel userModel;
-
+  /// --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-
-    blog('BUILDING UserStatsPage');
 
     return Center(
       child: SuperVerse(
@@ -24,5 +21,6 @@ class UserStatsPage extends StatelessWidget {
         maxLines: 5,
       ),
     );
+
   }
 }
