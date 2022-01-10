c5dc79eff5c4a09c9ab5f1300933a48f49c9a9af
RagehAz <rageh.az@gmail.com>
2022-01-10 03:12:34 +0200

    - snail progress

--- lib/b_views/y_views/g_user/user_states_page.dart
+++ lib/b_views/y_views/g_user/user_states_page.dart
@@ -1,5 +1,7 @@
 import 'package:bldrs/a_models/user/user_model.dart';
+import 'package:bldrs/b_views/widgets/general/bubbles/status_bubble.dart';
 import 'package:bldrs/b_views/widgets/general/textings/super_verse.dart';
+import 'package:bldrs/b_views/z_components/sizing/expander.dart';
 import 'package:flutter/material.dart';
 
 class UserStatsPage extends StatelessWidget {
@@ -11,15 +13,59 @@ class UserStatsPage extends StatelessWidget {
   /// --------------------------------------------------------------------------
   final UserModel userModel;
   /// --------------------------------------------------------------------------
+  ///
+// -----------------------------------------------------------------------------
+  void _switchUserStatus(UserStatus type) {
+    // setState(() {
+    //   _currentUserStatus = type;
+    // });
+
+    blog('type is $type');
+
+  }
+// -----------------------------------------------------------------------------
+  static const List<Map<String, dynamic>> _status = <Map<String, dynamic>>[
+    <String, dynamic>{
+      'title': 'Property Status',
+      'buttons': <Map<String, dynamic>>[
+        <String, dynamic>{
+          'state': 'Looking for a\nnew property',
+          'userStatus': UserStatus.searching
+        },
+        <String, dynamic>{
+          'state': 'Constructing\nan existing\nproperty',
+          'userStatus': UserStatus.finishing
+        },
+        <String, dynamic>{
+          'state': 'Want to\nSell / Rent\nmy property',
+          'userStatus': UserStatus.selling
+        }
+      ],
+    },
+    <String, dynamic>{
+      'title': 'Construction Status',
+      'buttons': <Map<String, dynamic>>[
+        <String, dynamic>{
+          'state': 'Planning Construction',
+          'userStatus': UserStatus.planning
+        },
+        <String, dynamic>{
+          'state': 'Under Construction',
+          'userStatus': UserStatus.building
+        }
+      ],
+    },
+  ];
+// -----------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
 
-    return Center(
-      child: SuperVerse(
-        verse: 'User states : ${userModel.status.toString()}',
-        size: 5,
-        maxLines: 5,
-      ),
+    return StatusBubble(
+      status: _status,
+      switchUserStatus: (UserStatus type) => _switchUserStatus(type),
+      userStatus: userModel?.status,
+      currentUserStatus: null,
+      // openEnumLister: widget.openEnumLister,
     );
 
   }
