ac2693f7e5972b5af1dfe990d20115ddb4defec8
RagehAz <rageh.az@gmail.com>
2022-01-10 03:20:44 +0200

    - snail progress attempt 2

--- lib/b_views/y_views/g_user/user_following_page.dart
+++ lib/b_views/y_views/g_user/user_following_page.dart
@@ -1,7 +1,6 @@
 import 'package:bldrs/a_models/bz/bz_model.dart';
 import 'package:bldrs/a_models/user/user_model.dart';
 import 'package:bldrs/b_views/widgets/general/bubbles/following_bzz_bubble.dart';
-import 'package:bldrs/b_views/widgets/general/textings/super_verse.dart';
 import 'package:bldrs/d_providers/bzz_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
