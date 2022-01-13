012527d44d0a9337453c578068ca4e48b7206ca8
RagehAz <rageh.az@gmail.com>
2022-01-13 10:40:26 +0200

    - bz grid adjusted in followed bzz page el7amdolellah
    - Essala 3annaby
    - 3leih afdal assalat wessalm
    - Allah Ynawwar Yasta

--- lib/b_views/widgets/general/bubbles/following_bzz_bubble.dart
+++ lib/b_views/widgets/general/bubbles/following_bzz_bubble.dart
@@ -1,26 +1,29 @@
 import 'package:bldrs/a_models/bz/bz_model.dart';
-import 'package:bldrs/b_views/widgets/general/bubbles/bubble.dart';
-import 'package:bldrs/b_views/widgets/specific/bz/bz_grid.dart';
+import 'package:bldrs/b_views/widgets/general/buttons/dream_box/dream_box.dart';
+import 'package:bldrs/b_views/widgets/specific/bz/bz_static_grid.dart';
 import 'package:bldrs/b_views/x_screens/i_flyer/h_1_bz_card_screen.dart';
-import 'package:bldrs/d_providers/user_provider.dart';
+import 'package:bldrs/f_helpers/drafters/aligners.dart';
 import 'package:bldrs/f_helpers/drafters/scalers.dart' as Scale;
 import 'package:bldrs/f_helpers/drafters/tracers.dart';
 import 'package:bldrs/f_helpers/router/navigators.dart' as Nav;
 import 'package:bldrs/f_helpers/theme/colorz.dart';
 import 'package:bldrs/f_helpers/theme/ratioz.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-class FollowingBzzBubble extends StatelessWidget {
+class FollowingBzzGrid extends StatelessWidget {
   /// --------------------------------------------------------------------------
-  const FollowingBzzBubble({
+  const FollowingBzzGrid({
     @required this.bzzModels,
+    this.title,
+    this.icon,
     this.onBzTap,
     Key key,
   }) : super(key: key);
   /// --------------------------------------------------------------------------
   final List<BzModel> bzzModels;
   final Function onBzTap;
+  final String title;
+  final String icon;
   /// --------------------------------------------------------------------------
   Future<void> _onBzTap({
     @required BuildContext context,
@@ -49,20 +52,34 @@ class FollowingBzzBubble extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Container(
-      width: Scale.superScreenWidth(context),
-      // height: Scale.superScreenHeight(context),
+      color: Colorz.white10,
+      margin: const EdgeInsets.only(bottom: 5),
       child: ListView(
         shrinkWrap: true,
+        physics: const BouncingScrollPhysics(),
         children: <Widget>[
 
-          /// FOLLOWING BZZ GRID
-          BzGrid(
-            gridZoneWidth: Scale.appBarWidth(context),
+          /// TITLE
+          if (title != null)
+          Align(
+            alignment: superCenterAlignment(context),
+            child: DreamBox(
+              height: 40,
+              verse: title,
+              verseScaleFactor: 0.7,
+              verseCentered: false,
+              margins: 10,
+              verseItalic: true,
+              bubble: false,
+              icon: icon,
+            ),
+          ),
+
+          /// GRID
+          BzStaticGrid(
+            gridBoxWidth: Scale.superScreenWidth(context),
             bzzModels: bzzModels ?? [],
             numberOfColumns: 4,
-            scrollDirection: Axis.horizontal,
-
-            // numberOfRows: 1000,
             itemOnTap: (String bzID) => _onBzTap(context: context, bzID: bzID),
           ),
 
--- lib/b_views/z_components/bz/bz_box.dart
+++ lib/b_views/z_components/bz/bz_box.dart
@@ -1,7 +1,6 @@
 import 'package:bldrs/a_models/bz/bz_model.dart';
 import 'package:bldrs/b_views/widgets/general/textings/super_verse.dart';
 import 'package:bldrs/b_views/widgets/specific/flyer/parts/header_parts/bz_logo.dart';
-import 'package:bldrs/f_helpers/drafters/borderers.dart' as Borderers;
 import 'package:bldrs/f_helpers/theme/colorz.dart';
 import 'package:flutter/material.dart';
 
