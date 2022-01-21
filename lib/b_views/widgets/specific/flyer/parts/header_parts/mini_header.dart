fb7260c2d1d24297232f30d0cccb0f9565698176
RagehAz <rageh.az@gmail.com>
2022-01-21 09:56:03 +0200

    - 7alaweyyat

--- lib/b_views/widgets/specific/flyer/parts/header_parts/mini_header.dart
+++ lib/b_views/widgets/specific/flyer/parts/header_parts/mini_header.dart
@@ -1,8 +1,8 @@
 import 'package:bldrs/a_models/flyer/mutables/super_flyer.dart';
-import 'package:bldrs/b_views/widgets/specific/flyer/parts/old_flyer_zone_box.dart';
 import 'package:bldrs/b_views/widgets/specific/flyer/parts/header_parts/bz_pg_headline.dart';
 import 'package:bldrs/b_views/widgets/specific/flyer/parts/header_parts/header_shadow.dart';
 import 'package:bldrs/b_views/widgets/specific/flyer/parts/header_parts/mini_header_strip.dart';
+import 'package:bldrs/b_views/widgets/specific/flyer/parts/old_flyer_zone_box.dart';
 import 'package:flutter/material.dart';
 
 class MiniHeader extends StatelessWidget {
