234f6cee109fab91ac00a8b8c457ccc85fcf0215
RagehAz <rageh.az@gmail.com>
2022-02-27 11:45:45 +0200

    - CLEAN UPS

--- lib/b_views/z_components/flyer/b_flyer_parts/b_footer/review_button/b_expanded_review_page_contents/b_submitted_reviews.dart
+++ lib/b_views/z_components/flyer/b_flyer_parts/b_footer/review_button/b_expanded_review_page_contents/b_submitted_reviews.dart
@@ -1,7 +1,7 @@
 import 'package:bldrs/a_models/user/user_model.dart';
 import 'package:bldrs/b_views/z_components/flyer/b_flyer_parts/b_footer/review_button/b_expanded_review_page_contents/c_review_bubble.dart';
-import 'package:flutter/material.dart';
 import 'package:bldrs/f_helpers/drafters/timerz.dart' as Timerz;
+import 'package:flutter/material.dart';
 
 class SubmittedReviews extends StatelessWidget {
   /// --------------------------------------------------------------------------
