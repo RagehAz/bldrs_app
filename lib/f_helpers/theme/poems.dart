3f1e31b9eec9d466d9d896029655e9d98778eac1
RagehAz <rageh.az@gmail.com>
2022-03-23 05:25:16 +0200

    - added pitch dialog

--- lib/f_helpers/theme/poems.dart
+++ lib/f_helpers/theme/poems.dart
@@ -45,3 +45,65 @@ opinion - size - age - shape - colour - origin - material - purpose - noun
 have a lovely little old rectangular green french silver whittling knife
 
  */
+
+
+/// PITCH DIALOG
+
+/// mixed irrelevant posts
+// Now -- is the time -to transform industries -into their own specialized digital platforms,
+// We have all seen the expansion of social media platforms -and their efforts to aggregate
+// all what they can collect - into one digital platform - or another.
+
+/// after removal of irrelevant content
+// While human psychology -- requires focus -- to get things done properly.
+
+/// Bldrs.net without the categories
+// I'm Architect Rage7 Azzazi, and I've been working in the construction industry
+// for the past 10 years -- I've managed more than 500 customers projects.
+
+/// Bldrs.net with the categories
+// and please allow me to tell you -- you don't only need someone you know in
+// your most expensive life journey -of building and construction
+//  - without losing the upto 50% budget safe guard
+//  - without losing the upto 200% delay
+//  - without losing the upto 30% flawed quality reworks
+
+// what you need -- Ladies and Gentlemen -- is the right - TEAM - of professional BUILDERS.
+
+/// Bldrs.net relations diagram
+// Bldrs.net is the open BUILDERS NETWORK,
+// for Designers, Contractors, Craftsmen and CUSTOMERS to MATCH - according
+// to their COMMON NEEDS,
+
+// A B2B -- B2C -- and C2B Platform
+// where CUSTOMERS seek & filter BUSINESSES - for their projects.
+// And BUSINESSES seek & filter OTHER BUSINESSES - for those projects.
+// meanwhile -- BUSINESSES can still seek & filter - LABELLED - POTENTIAL - CUSTOMERS
+// who manually chose to be notified about construction services - they need RIGHT NOW
+
+// and while we are not the only ones - in this digital market.
+// The majority of platforms are specialized in one field or another - all aiming
+// to get more traffic - in which the Bldrs.net provides.
+
+// and as we only monetize - through providing business accounts - tailored
+// subscription packages which can help them obtain bigger market exposure.
+
+// Bldrs.net incentives - businesses contribution - in growing the network -
+// by its rewarding referral and loyalty systems
+
+// Egypt has got an annual spending on construction of - 450 Billion pounds,
+// in Which gives us a market size - of annual - 11 Billion pounds those
+// businesses spend on customer acquisition.
+
+// and where are about to hit the market starting Q3 2022
+
+// so will approach the market starting with our 200+ business accounts - in our
+// collected data base - and personal networks.
+// as well as pitching 5000+ Egyptian businesses - already existent on the internet.
+// just before - we get on ground - to pitch those - who are not - yet online.
+
+// meanwhile -- seeking who are going to be -- the construction market leaders -
+// of this act of -- digital transcendence
+
+
+
