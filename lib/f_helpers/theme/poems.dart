d179043052463616cc2f036677502f230d6c8884
RagehAz <rageh.az@gmail.com>
2022-03-24 23:18:31 +0200

    - added icons and working on pitch deck

--- lib/f_helpers/theme/poems.dart
+++ lib/f_helpers/theme/poems.dart
@@ -57,17 +57,18 @@ have a lovely little old rectangular green french silver whittling knife
 /// after removal of irrelevant content
 // While human psychology -- requires focus -- to get things done properly.
 
-/// Bldrs.net without the categories
+/// PROJECT SCOPES SLIDES
 // I'm Architect Rage7 Azzazi, and I've been working in the construction industry
 // for the past 10 years -- I've managed more than 500 customers projects.
 
-/// Bldrs.net with the categories
+/// CONSTRUCTION PAINS SLIDES
 // and please allow me to tell you -- you don't only need someone you know in
 // your most expensive life journey -of building and construction
 //  - without losing the upto 50% budget safe guard
 //  - without losing the upto 200% delay
 //  - without losing the upto 30% flawed quality reworks
 
+/// Bldrs.net without the categories
 // what you need -- Ladies and Gentlemen -- is the right - TEAM - of professional BUILDERS.
 
 /// Bldrs.net relations diagram
@@ -75,33 +76,41 @@ have a lovely little old rectangular green french silver whittling knife
 // for Designers, Contractors, Craftsmen and CUSTOMERS to MATCH - according
 // to their COMMON NEEDS,
 
+/// CUSTOMER JOURNEY SLIDE
 // A B2B -- B2C -- and C2B Platform
 // where CUSTOMERS seek & filter BUSINESSES - for their projects.
 // And BUSINESSES seek & filter OTHER BUSINESSES - for those projects.
 // meanwhile -- BUSINESSES can still seek & filter - LABELLED - POTENTIAL - CUSTOMERS
 // who manually chose to be notified about construction services - they need RIGHT NOW
 
+/// COMPETITION DIAGRAM
 // and while we are not the only ones - in this digital market.
 // The majority of platforms are specialized in one field or another - all aiming
 // to get more traffic - in which the Bldrs.net provides.
 
+/// MONETIZATION DIAGRAM
 // and as we only monetize - through providing business accounts - tailored
 // subscription packages which can help them obtain bigger market exposure.
 
+/// REFERRAL AND LOYALTY DIAGRAMS
 // Bldrs.net incentives - businesses contribution - in growing the network -
 // by its rewarding referral and loyalty systems
 
+/// MARKET SIZE DIAGRAM
 // Egypt has got an annual spending on construction of - 450 Billion pounds,
 // in Which gives us a market size - of annual - 11 Billion pounds those
 // businesses spend on customer acquisition.
 
+/// TIME LINE DIAGRAM
 // and where are about to hit the market starting Q3 2022
 
+/// GO TO MARKET SLIDE
 // so will approach the market starting with our 200+ business accounts - in our
 // collected data base - and personal networks.
 // as well as pitching 5000+ Egyptian businesses - already existent on the internet.
 // just before - we get on ground - to pitch those - who are not - yet online.
 
+/// PYRAMID NETWORK
 // meanwhile -- seeking who are going to be -- the construction market leaders -
 // of this act of -- digital transcendence
 
