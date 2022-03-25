33439b5b98be63a78219027b8b70809256f07ed6
RagehAz <rageh.az@gmail.com>
2022-03-25 18:04:11 +0200

    - isa kheir

--- lib/xxx_dashboard/a_modules/translations_manager/translations_manager.dart
+++ lib/xxx_dashboard/a_modules/translations_manager/translations_manager.dart
@@ -1,3 +1,4 @@
+import 'package:bldrs/a_models/kw/kw.dart';
 import 'package:bldrs/a_models/secondary_models/phrase_model.dart';
 import 'package:bldrs/a_models/secondary_models/translation_model.dart';
 import 'package:bldrs/b_views/z_components/artworks/bldrs_name.dart';
@@ -7,7 +8,10 @@ import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart'
 import 'package:bldrs/b_views/z_components/layouts/unfinished_night_sky.dart';
 import 'package:bldrs/b_views/z_components/sizing/expander.dart';
 import 'package:bldrs/b_views/z_components/streamers/trans_mdel_streamer.dart';
+import 'package:bldrs/e_db/fire/methods/firestore.dart';
+import 'package:bldrs/e_db/fire/methods/paths.dart';
 import 'package:bldrs/e_db/fire/ops/trans_ops.dart';
+import 'package:bldrs/f_helpers/drafters/mappers.dart' as Mapper;
 import 'package:bldrs/f_helpers/drafters/scalers.dart' as Scale;
 import 'package:bldrs/f_helpers/drafters/text_checkers.dart' as TextChecker;
 import 'package:bldrs/f_helpers/theme/colorz.dart';
@@ -16,17 +20,17 @@ import 'package:bldrs/xxx_dashboard/a_modules/translations_manager/pages/transla
 import 'package:bldrs/xxx_dashboard/a_modules/translations_manager/translations_controller.dart';
 import 'package:flutter/material.dart';
 
-class TranslationsLab extends StatefulWidget {
+class TranslationsManager extends StatefulWidget {
 
-  const TranslationsLab({
+  const TranslationsManager({
     Key key
   }) : super(key: key);
 
   @override
-  _TranslationsLabState createState() => _TranslationsLabState();
+  _TranslationsManagerState createState() => _TranslationsManagerState();
 }
 
-class _TranslationsLabState extends State<TranslationsLab> {
+class _TranslationsManagerState extends State<TranslationsManager> {
   // ---------------------------------------------------------------------------
   final TextEditingController _idController = TextEditingController();
   final TextEditingController _englishController = TextEditingController();
@@ -68,12 +72,85 @@ class _TranslationsLabState extends State<TranslationsLab> {
 
   // -----------------------------
   Future<void> _onBldrsTap() async {
+    blog('Blogging BLDRS.net ahooo');
+  }
+  // -----------------------------
+  Future<void> _onUploadGroup({
+    @required List<Phrase> originalEnPhrases,
+    @required List<Phrase> originalArPhrases,
+}) async {
 
-      blog('Blogging BLDRS.net ahooo');
+    const String _docName = FireDoc.keys_propertiesKeywords;
+
+    final Map<String, dynamic> _docMap = await readDoc(
+        context: context,
+        collName: FireColl.keys,
+        docName: _docName, // <----------------------
+    );
+
+
+    final List<String> _keys = _docMap.keys.toList();
+
+    final List<Phrase> _enPhrasesToAdd = <Phrase>[];
+    final List<Phrase> _arPhrasesToAdd = <Phrase>[];
+    final List<String> _keywordsIDs = <String>[];
+
+    for (final String key in _keys){
+
+      final Map<String, dynamic> _keywordMap = _docMap[key];
+      final String _keywordID = 'phid_k_${_keywordMap['id']}';
+
+      final Phrase _enPhrase = Phrase(
+        id: _keywordID,
+        value: _keywordMap['names']['en']['value'],
+      );
+
+      final Phrase _arPhrase = Phrase(
+        id: _keywordID,
+        value: _keywordMap['names']['ar']['value'],
+      );
+
+      _enPhrasesToAdd.add(_enPhrase);
+      _arPhrasesToAdd.add(_arPhrase);
+      _keywordsIDs.add(_keywordID);
+    }
+
+    final List<Phrase> _enResult = <Phrase>[...originalEnPhrases, ..._enPhrasesToAdd];
+    final List<Phrase> _arResult = <Phrase>[...originalArPhrases, ..._arPhrasesToAdd];
+
+    /// update english translations
+    await updateDocField(
+        context: context,
+        collName: FireColl.translations,
+        docName: 'en',
+        field: 'phrases',
+        input: Phrase.cipherPhrases(phrases: _enResult),
+    );
+
+    /// update arabic translations
+    await updateDocField(
+      context: context,
+      collName: FireColl.translations,
+      docName: 'ar',
+      field: 'phrases',
+      input: Phrase.cipherPhrases(phrases: _arResult),
+    );
+
+    await updateDocField(
+        context: context,
+        collName: FireColl.keys,
+        docName: 'keywords',
+        field: _docName,
+        input: _keywordsIDs,
+    );
+
+    Phrase.blogPhrases(_enResult);
+    Phrase.blogPhrases(_arResult);
+    blog('number of keywords = ${_keys.length} + ${originalEnPhrases.length} existing = ${_keys.length + originalEnPhrases.length} TOTAL');
+    blog('final list is ${_enResult.length} english : ${_arResult.length} arabic');
 
   }
   // -----------------------------
-
   @override
   Widget build(BuildContext context) {
 
@@ -138,7 +215,10 @@ class _TranslationsLabState extends State<TranslationsLab> {
                       verse: 'Upload',
                       secondLine: 'group',
                       margins: const EdgeInsets.symmetric(horizontal: 5),
-                      onTap: () async {},
+                      onTap: () => _onUploadGroup(
+                        originalArPhrases: _arPhrases,
+                        originalEnPhrases: _enPhrases,
+                      ),
                     ),
 
                     /// UPLOAD BUTTON
