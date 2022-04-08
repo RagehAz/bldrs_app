eb2c36e87d1fcfd648cc8623dcee5d4d59147c50
RagehAz <rageh.az@gmail.com>
2022-04-08 20:11:28 +0200

    - fixed flyers search engine

--- lib/b_views/x_screens/c_search/c_0_search_screen.dart
+++ lib/b_views/x_screens/c_search/c_0_search_screen.dart
@@ -18,14 +18,18 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class SearchScreen extends StatefulWidget {
-  const SearchScreen({Key key}) : super(key: key);
-
+  /// --------------------------------------------------------------------------
+  const SearchScreen({
+    Key key
+  }) : super(key: key);
+  /// --------------------------------------------------------------------------
   @override
   _SearchScreenState createState() => _SearchScreenState();
+/// --------------------------------------------------------------------------
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-
+// -----------------------------------------------------------------------------
   final TextEditingController _searchController = TextEditingController();
   final ScrollController _scrollController = ScrollController();
 // -----------------------------------------------------------------------------
