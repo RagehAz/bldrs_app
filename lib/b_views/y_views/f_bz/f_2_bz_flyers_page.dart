2fc32179b888883ab4265f653fd5d2776e22cdd4
RagehAz <rageh.az@gmail.com>
2022-04-25 00:34:13 +0200

    - before I refactor shelf slides part list view from builder to normal list

--- lib/b_views/y_views/f_bz/f_2_bz_flyers_page.dart
+++ lib/b_views/y_views/f_bz/f_2_bz_flyers_page.dart
@@ -2,9 +2,11 @@ import 'package:bldrs/a_models/bz/bz_model.dart';
 import 'package:bldrs/a_models/flyer/flyer_model.dart';
 import 'package:bldrs/a_models/zone/city_model.dart';
 import 'package:bldrs/a_models/zone/country_model.dart';
+import 'package:bldrs/b_views/x_screens/i_flyer/flyer_maker_screen.dart/flyer_maker_screen.dart';
 import 'package:bldrs/b_views/z_components/flyer/c_flyer_groups/flyers_grid.dart';
 import 'package:bldrs/f_helpers/drafters/mappers.dart';
 import 'package:bldrs/f_helpers/drafters/scalers.dart';
+import 'package:bldrs/f_helpers/router/navigators.dart' as Nav;
 import 'package:flutter/material.dart';
 
 class BzFlyersPage extends StatelessWidget {
@@ -22,9 +24,20 @@ class BzFlyersPage extends StatelessWidget {
   final CountryModel bzCountry;
   final CityModel bzCity;
   /// --------------------------------------------------------------------------
-  // void _showOldFlyersOnTap(BuildContext context, BzModel bzModel) {
-  //   Nav.goToNewScreen(context, DeactivatedFlyerScreen(bz: bzModel));
-  // }
+  Future<void> _onEditFlyer({
+    @required FlyerModel flyer,
+    @required BuildContext context,
+  }) async {
+
+    await Nav.goToNewScreen(
+        context,
+        FlyerPublisherScreen(
+          bzModel: bzModel,
+          flyerModel: flyer,
+        ),
+    );
+
+  }
 // -----------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
@@ -40,7 +53,11 @@ class BzFlyersPage extends StatelessWidget {
         scrollController: null,
         numberOfColumns: 2,
         topPadding: 5,
-        addFlyerButtonIsOn: true,
+        authorMode: true,
+        onEditFlyer: (FlyerModel flyerModel) => _onEditFlyer(
+          context: context,
+          flyer: flyerModel,
+        ),
       );
     }
     else {
