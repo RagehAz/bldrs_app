efc6f57b0aa2523a7a775803b9814e0284f79675
RagehAz <rageh.az@gmail.com>
2022-05-02 00:24:46 +0200

    - upgraded widget fader

--- lib/b_views/z_components/animators/fade_widget_out.dart
+++ lib/b_views/z_components/animators/fade_widget_out.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 
-class FadeWidgetOut extends StatefulWidget {
-  const FadeWidgetOut({
+enum FadeType{
+  fadeIn,
+  fadeOut,
+  repeatAndReverse,
+  repeatForwards,
+  stillAtMin,
+  stillAtMax,
+}
+
+class WidgetFader extends StatefulWidget {
+  const WidgetFader({
     @required this.child,
+    @required this.fadeType,
+    this.max = 1,
+    this.min = 0,
+    this.duration,
+    this.curve = Curves.easeInOut,
     Key key
   }) : super(key: key);
 
   final Widget child;
+  final FadeType fadeType;
+  final double max;
+  final double min;
+  final Duration duration;
+  final Curve curve;
 
   @override
-  _FadeWidgetOutState createState() => _FadeWidgetOutState();
+  _WidgetFaderState createState() => _WidgetFaderState();
 }
 
-class _FadeWidgetOutState extends State<FadeWidgetOut> with SingleTickerProviderStateMixin {
+class _WidgetFaderState extends State<WidgetFader> with SingleTickerProviderStateMixin {
 
   AnimationController _animationController;
   CurvedAnimation _animation;
@@ -21,16 +40,18 @@ class _FadeWidgetOutState extends State<FadeWidgetOut> with SingleTickerProvider
   @override
   void initState() {
 
+    final Duration _duration = widget.duration ?? const Duration(seconds: 2);
+
     _animationController = AnimationController(
-        reverseDuration: const Duration(seconds: 2),
-        duration: const Duration(seconds: 2),
+        reverseDuration: _duration,
+        duration: _duration,
         vsync: this
     );
 
     _animation = CurvedAnimation(
       parent: _animationController,
-      curve: Curves.easeInOut,
-      reverseCurve: Curves.easeInOut,
+      curve: widget.curve,
+      reverseCurve: widget.curve,
     );
 
     super.initState();
@@ -43,14 +64,51 @@ class _FadeWidgetOutState extends State<FadeWidgetOut> with SingleTickerProvider
     super.dispose();
   }
 
+  Future<void> _animate() async {
+
+    if (widget.fadeType == FadeType.fadeIn){
+      await _animationController.forward(from: widget.min,);
+    }
+    else if (widget.fadeType == FadeType.fadeOut){
+      await _animationController.reverse(from: widget.max);
+    }
+    else if (widget.fadeType == FadeType.repeatAndReverse){
+      await _animationController.repeat(
+        min: widget.min,
+        max: widget.max,
+        reverse: true,
+        period: widget.duration,
+      );
+    }
+    else if (widget.fadeType == FadeType.repeatForwards){
+      await _animationController.repeat(
+        min: widget.min,
+        max: widget.max,
+        reverse: false,
+        period: widget.duration,
+      );
+    }
+    else if (widget.fadeType == FadeType.stillAtMin){
+      _animationController.value = widget.min;
+    }
+    else if (widget.fadeType == FadeType.stillAtMax){
+      _animationController.value = widget.max;
+    }
+    else {
+      await _animationController.forward(from: widget.min,);
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    _animationController.reverse(from: 1);
+    _animate();
 
     return FadeTransition(
       opacity: _animation,
       child: widget.child,
     );
   }
+
 }
