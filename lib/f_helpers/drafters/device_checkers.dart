12e54f45c830c505e5c3eb251930ad5b896f9922
RagehAz <rageh.az@gmail.com>
2022-05-02 05:40:03 +0200

    - can check internet connection through a stream bitch naw

--- lib/f_helpers/drafters/device_checkers.dart
+++ lib/f_helpers/drafters/device_checkers.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-
+import 'package:connectivity_plus/connectivity_plus.dart';
 // -----------------------------------------------------------------------------
   bool deviceIsIOS(){
     bool _isIOS;
@@ -46,3 +46,42 @@ import 'package:flutter/material.dart';
     return _isLandscape;
   }
 // -----------------------------------------------------------------------------
+  Future<bool> deviceIsConnected({
+    ConnectivityResult streamResult,
+}) async {
+
+    bool _connected = false;
+
+    final ConnectivityResult _result = streamResult ?? await Connectivity().checkConnectivity();
+
+    /// THROUGH MOBILE NETWORK
+    if (_result == ConnectivityResult.mobile) {
+      _connected = true;
+    }
+
+    /// THROUGH WIFI
+    else if (_result == ConnectivityResult.wifi) {
+      _connected = true;
+    }
+
+    /// THROUGH BLUETOOTH
+    else if (_result == ConnectivityResult.bluetooth){
+      _connected = true;
+    }
+
+    /// THROUGH ETHERNET
+    else if (_result == ConnectivityResult.ethernet){
+      _connected = true;
+    }
+
+    /// NOT CONNECTED
+    else if (_result == ConnectivityResult.none){
+      _connected = false;
+    }
+
+    else {
+      _connected = false;
+    }
+
+    return _connected;
+  }
