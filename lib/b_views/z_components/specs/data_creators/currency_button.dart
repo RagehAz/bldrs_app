3feb3bb7c26b26b453f8ab8ab71c5aeec4daa0fa
RagehAz <rageh.az@gmail.com>
2022-05-12 08:31:07 +0200

    - fixed currencies and added their translations via google translate api

--- lib/b_views/z_components/specs/data_creators/currency_button.dart
+++ lib/b_views/z_components/specs/data_creators/currency_button.dart
@@ -1,11 +1,8 @@
 import 'package:bldrs/a_models/zone/currency_model.dart';
 import 'package:bldrs/a_models/zone/flag_model.dart';
 import 'package:bldrs/b_views/z_components/buttons/dream_box/dream_box.dart';
-import 'package:bldrs/b_views/z_components/texting/super_verse.dart';
 import 'package:bldrs/d_providers/phrase_provider.dart';
-import 'package:bldrs/f_helpers/drafters/borderers.dart' as Borderers;
 import 'package:bldrs/f_helpers/theme/colorz.dart';
-import 'package:bldrs/f_helpers/theme/ratioz.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyButton extends StatelessWidget {
@@ -18,7 +15,6 @@ class CurrencyButton extends StatelessWidget {
     this.height = 60,
     Key key
   }) : super(key: key);
-
   /// --------------------------------------------------------------------------
   final double width;
   final double height;
@@ -28,42 +24,20 @@ class CurrencyButton extends StatelessWidget {
   /// --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
+
+    return DreamBox(
       height: height,
-      decoration: BoxDecoration(
-        // color: Colorz.blackSemi255,
-        borderRadius: Borderers.superBorderAll(context, Ratioz.appBarCorner),
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 2.5),
-      child: Row(
-        children: <Widget>[
-
-          DreamBox(
-            height: height,
-            width: width - height,
-            color: Colorz.blackSemi255,
-            icon: Flag.getFlagIconByCountryID(countryID.toLowerCase()),
-            verse: currency.symbol,
-            verseScaleFactor: 0.9,
-            verseCentered: false,
-            verseMaxLines: 2,
-            secondLine: superPhrase(context, currency.code),
-            bubble: false,
-            onTap: onTap,
-          ),
-
-          Container(
-            width: height,
-            height: height,
-            alignment: Alignment.center,
-            child: SuperVerse(
-              verse: currency.nativeSymbol,
-            ),
-          ),
-
-        ],
-      ),
+      width: width - height,
+      color: Colorz.blackSemi255,
+      icon: Flag.getFlagIconByCountryID(countryID.toLowerCase()),
+      verse: superPhrase(context, currency.id),
+      secondLine: currency.symbol,
+      iconSizeFactor: 0.7,
+      verseCentered: false,
+      bubble: false,
+      onTap: onTap,
+      margins: 5,
     );
+
   }
 }
--- lib/x_dashboard/a_modules/b_phrases_editor/widgets/translations_bubble.dart
+++ lib/x_dashboard/a_modules/b_phrases_editor/widgets/translations_bubble.dart
@@ -4,7 +4,6 @@ import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart'
 import 'package:bldrs/b_views/z_components/layouts/navigation/scroller.dart';
 import 'package:bldrs/b_views/z_components/layouts/page_bubble.dart';
 import 'package:bldrs/f_helpers/drafters/mappers.dart';
-import 'package:bldrs/f_helpers/drafters/scalers.dart' as Scale;
 import 'package:bldrs/f_helpers/theme/ratioz.dart';
 import 'package:bldrs/x_dashboard/a_modules/b_phrases_editor/widgets/translation_strip.dart';
 import 'package:flutter/material.dart';
