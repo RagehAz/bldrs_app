165060aa15e0899cdada65b8e35dc96e77cac37f
RagehAz <rageh.az@gmail.com>
2022-05-15 16:16:42 +0200

    -nono push

--- lib/x_dashboard/a_modules/g_users_manager/users_manager_screen.dart
+++ lib/x_dashboard/a_modules/g_users_manager/users_manager_screen.dart
@@ -1,6 +1,7 @@
 import 'package:bldrs/a_models/user/user_model.dart';
 import 'package:bldrs/b_views/z_components/buttons/dream_box/dream_box.dart';
 import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
+import 'package:bldrs/b_views/z_components/layouts/page_bubble.dart';
 import 'package:bldrs/b_views/z_components/layouts/unfinished_night_sky.dart';
 import 'package:bldrs/b_views/z_components/sizing/expander.dart';
 import 'package:bldrs/e_db/fire/methods/cloud_functions.dart' as CloudFunctionz;
@@ -8,145 +9,158 @@ import 'package:bldrs/e_db/fire/methods/firestore.dart' as Fire;
 import 'package:bldrs/e_db/fire/methods/paths.dart';
 import 'package:bldrs/f_helpers/drafters/scalers.dart' as Scale;
 import 'package:bldrs/f_helpers/drafters/scrollers.dart' as Scrollers;
+import 'package:bldrs/f_helpers/theme/colorz.dart';
 import 'package:bldrs/f_helpers/theme/ratioz.dart';
+import 'package:bldrs/x_dashboard/a_modules/g_users_manager/users_manager_controller.dart';
 import 'package:bldrs/x_dashboard/b_widgets/user_button.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class UsersManagerScreen extends StatefulWidget {
-  const UsersManagerScreen({Key key}) : super(key: key);
-
+  /// --------------------------------------------------------------------------
+  const UsersManagerScreen({
+    Key key
+  }) : super(key: key);
+  /// --------------------------------------------------------------------------
   @override
   _UsersManagerScreenState createState() => _UsersManagerScreenState();
+/// --------------------------------------------------------------------------
 }
 
 class _UsersManagerScreenState extends State<UsersManagerScreen> {
 // -----------------------------------------------------------------------------
-  /// --- FUTURE LOADING BLOCK
-  bool _loading = false;
-  Future<void> _triggerLoading({Function function}) async {
-    if (function == null) {
-      setState(() {
-        _loading = !_loading;
-      });
-    } else {
-      setState(() {
-        _loading = !_loading;
-        function();
-      });
-    }
-
-    _loading == true
-        ? blog('LOADING--------------------------------------')
-        : blog('LOADING COMPLETE--------------------------------------');
+  /// --- LOCAL LOADING BLOCK
+  final ValueNotifier<bool> _loading = ValueNotifier(false); /// tamam disposed
+// -----------------------------------
+  Future<void> _triggerLoading() async {
+    _loading.value = !_loading.value;
+    blogLoading(
+      loading: _loading.value,
+      callerName: 'HomeScreen',
+    );
   }
-
 // -----------------------------------------------------------------------------
   @override
   void initState() {
     super.initState();
   }
-
   // -----------------------------------------------------------------------------
   bool _isInit = true;
   @override
   void didChangeDependencies() {
-    if (_isInit) {
-      _triggerLoading(function: () {}).then((_) async {
-        /// ---------------------------------------------------------0
+    if (_isInit && mounted) {
 
-        await _readMoreUsers();
+      _triggerLoading().then((_) async {
 
-        /// ---------------------------------------------------------0
+        await readMoreUsers(
+          usersModels: _usersModels,
+          scrollController: _scrollController,
+          lastSnapshot: _lastSnapshot,
+        );
+
       });
+
+      _isInit = false;
     }
-    _isInit = false;
     super.didChangeDependencies();
   }
-
+  // -----------------------------------------------------------------------------
+  @override
+  void dispose() {
+    super.dispose();
+    _loading.dispose();
+  }
 // -----------------------------------------------------------------------------
   final ScrollController _scrollController = ScrollController();
 // -----------------------------------------------------------------------------
-  QueryDocumentSnapshot<Object> _lastSnapshot;
-  final List<UserModel> _usersModels = <UserModel>[];
-  Future<void> _readMoreUsers() async {
-    final List<dynamic> _maps = await Fire.readCollectionDocs(
-      collName: FireColl.users,
-      orderBy: 'id',
-      limit: 5,
-      startAfter: _lastSnapshot,
-      addDocSnapshotToEachMap: true,
-    );
-
-    final List<UserModel> _fetchedModel = UserModel.decipherUsersMaps(
-      maps: _maps,
-      fromJSON: false,
-    );
-
-    setState(() {
-      _lastSnapshot = _maps[_maps.length - 1]['docSnapshot'];
-      _usersModels.addAll(_fetchedModel);
-    });
-
-    await Future<void>.delayed(const Duration(milliseconds: 400), () async {
-      await Scrollers.scrollToEnd(controller: _scrollController);
-    });
-  }
-
-// -----------------------------------------------------------------------------
-  Future<void> _onDeleteUser(UserModel userModel) async {
-    final String _result =
-        await CloudFunctionz.deleteFirebaseUser(userID: userModel.id);
-
-    if (_result == 'stop') {
-      blog('operation stopped');
-    } else if (_result == 'deleted') {
-      final int _userIndex =
-          _usersModels.indexWhere((UserModel user) => user.id == userModel.id);
-      setState(() {
-        _usersModels.removeAt(_userIndex);
-      });
-    }
-  }
-
+  final ValueNotifier<QueryDocumentSnapshot<Object>> _lastSnapshot = ValueNotifier(null);
+  final ValueNotifier<List<UserModel>> _usersModels = ValueNotifier<List<UserModel>>(<UserModel>[]);
 // -----------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
+
+    final double _screenHeight = Scale.superScreenHeightWithoutSafeArea(context);
+
     return MainLayout(
       // loading: _loading,
       pageTitle: 'Users Manager',
-      appBarType: AppBarType.basic,
+      sectionButtonIsOn: false,
+      appBarType: AppBarType.search,
+      zoneButtonIsOn: false,
       skyType: SkyType.black,
       appBarRowWidgets: <Widget>[
+
         const Expander(),
-        DreamBox(
-          width: 150,
-          height: 40,
-          verse: 'Load More',
-          secondLine: 'showing ${_usersModels.length} users',
-          verseScaleFactor: 0.7,
-          secondLineScaleFactor: 0.9,
-          margins: const EdgeInsets.symmetric(horizontal: 5),
-          onTap: () async {
-            await _readMoreUsers();
-          },
-        ),
+
+        ValueListenableBuilder(
+            valueListenable: _usersModels,
+            builder: (_, List<UserModel> users, Widget child){
+
+              return DreamBox(
+                height: 40,
+                verse: 'Load More',
+                secondLine: 'showing ${users.length} users',
+                verseScaleFactor: 0.7,
+                secondLineScaleFactor: 0.9,
+                margins: const EdgeInsets.symmetric(horizontal: 5),
+                onTap: () => readMoreUsers(
+                  usersModels: _usersModels,
+                  lastSnapshot: _lastSnapshot,
+                  scrollController: _scrollController,
+                ),
+              );
+
+            }
+            ),
+
       ],
-      layoutWidget: ListView.builder(
-        controller: _scrollController,
+
+      layoutWidget: PageView(
         physics: const BouncingScrollPhysics(),
-        itemCount: _usersModels.length,
-        padding: const EdgeInsets.only(
-            bottom: Ratioz.grandHorizon, top: Ratioz.stratosphere),
-        itemExtent: 70,
-        itemBuilder: (BuildContext context, int index) {
-          return DashboardUserButton(
-            width: Scale.superScreenWidth(context) - 20,
-            userModel: _usersModels[index],
-            index: index,
-            onDeleteUser: (UserModel userModel) => _onDeleteUser(userModel),
-          );
-        },
+        children: <Widget>[
+
+          PageBubble(
+              screenHeightWithoutSafeArea: _screenHeight,
+              appBarType: AppBarType.search,
+              child: ValueListenableBuilder(
+                valueListenable: _usersModels,
+                builder: (_, List<UserModel> users, Widget child){
+
+                  return ListView.builder(
+                    controller: _scrollController,
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: users.length,
+                    padding: const EdgeInsets.only(bottom: Ratioz.grandHorizon),
+                    itemExtent: 70,
+                    itemBuilder: (BuildContext context, int index) {
+
+                      return DashboardUserButton(
+                        width: PageBubble.clearWidth(context),
+                        userModel: users[index],
+                        index: index,
+                        onDeleteUser: (UserModel userModel) => onDeleteUser(
+                          userID: userModel.id,
+                          usersModels: _usersModels,
+                        ),
+                      );
+
+                    },
+                  );
+
+                },
+              ),
+          ),
+
+          PageBubble(
+            appBarType: AppBarType.search,
+            screenHeightWithoutSafeArea: _screenHeight,
+            child: Container(
+              width: 100,
+              height: 100,
+              color: Colorz.bloodTest,
+            ),
+          ),
+        ],
       ),
     );
   }
