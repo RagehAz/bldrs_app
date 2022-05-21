5cbde7e49b9bf0d7a7baff11a91dea0b5bd6b11c
RagehAz <rageh.az@gmail.com>
2022-05-21 11:57:10 +0200

    - some fixes

---
+++ lib/x_dashboard/a_modules/a_test_labs/test_widgets/is_connected_button.dart
@@ -0,0 +1,46 @@
+import 'package:bldrs/b_views/z_components/buttons/dream_box/dream_box.dart';
+import 'package:bldrs/b_views/z_components/dialogs/nav_dialog/nav_dialog.dart';
+import 'package:bldrs/b_views/z_components/layouts/main_layout/connectivity_sensor.dart';
+import 'package:bldrs/f_helpers/theme/colorz.dart';
+import 'package:bldrs/f_helpers/theme/iconz.dart' as Iconz;
+import 'package:bldrs/f_helpers/theme/ratioz.dart';
+import 'package:flutter/material.dart';
+
+class IsConnectedButton extends StatelessWidget {
+  /// --------------------------------------------------------------------------
+  const IsConnectedButton({
+    Key key
+  }) : super(key: key);
+  /// --------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+
+    return ConnectivitySensor(
+        builder: (bool connected, Widget child){
+
+          return DreamBox(
+              width: Ratioz.appBarButtonSize,
+              height: Ratioz.appBarButtonSize,
+              icon: connected ? Iconz.check : Iconz.xSmall,
+              color: connected ? Colorz.green255 : Colorz.bloodTest,
+              verseScaleFactor: 0.6,
+              bubble: false,
+              onTap: () async {
+                // final bool _connected = await checkConnectivity();
+                //
+                // await _onConnectivityChanged(_connected);
+
+                NavDialog.showNavDialog(
+                  context: context,
+                  firstLine: 'Hello There',
+                  secondLine: "Welcome to Bldrs.net the Builders' network",
+                  // color: Colorz.red50,
+                );
+
+              }
+          );
+        }
+    );
+
+  }
+}
---
+++ lib/x_dashboard/a_modules/a_test_labs/test_widgets/is_signed_in_button.dart
@@ -0,0 +1,65 @@
+import 'package:bldrs/b_views/z_components/buttons/dream_box/dream_box.dart';
+import 'package:bldrs/b_views/z_components/dialogs/center_dialog/center_dialog.dart';
+import 'package:bldrs/f_helpers/theme/colorz.dart';
+import 'package:bldrs/f_helpers/theme/ratioz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:bldrs/e_db/fire/ops/auth_ops.dart' as FireAuthOps;
+
+class IsSignedInButton extends StatelessWidget {
+  /// --------------------------------------------------------------------------
+  const IsSignedInButton({
+    Key key
+  }) : super(key: key);
+  /// --------------------------------------------------------------------------
+  bool _isSignedInCheck() {
+    bool _isSignedIn;
+
+    final User _firebaseUser = FireAuthOps.superFirebaseUser();
+
+    if (_firebaseUser == null) {
+      _isSignedIn = false;
+    } else {
+      _isSignedIn = true;
+    }
+
+    return _isSignedIn;
+  }
+// -----------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+
+    final bool _isSignedIn = _isSignedInCheck();
+
+    return DreamBox(
+      height: Ratioz.appBarButtonSize,
+      verse: _isSignedIn ? 'Signed in' : 'Signed out',
+      color: _isSignedIn ? Colorz.green255 : Colorz.grey80,
+      verseScaleFactor: 0.6,
+      verseColor: _isSignedIn ? Colorz.white255 : Colorz.darkGrey255,
+      bubble: false,
+      onTap: () async {
+
+        final bool _result = await CenterDialog.showCenterDialog(
+          context: context,
+          title: 'Sign out ?',
+          boolDialog: true,
+          confirmButtonText: 'Yes!\nSign out',
+        );
+
+        if (_result == true){
+
+          await FireAuthOps.signOut(
+              context: context,
+              routeToUserChecker: true
+          );
+
+        }
+
+
+      },
+    );
+
+  }
+
+}
