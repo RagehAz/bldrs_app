7897d86baa871e7287e967cfa7ddbf4c751dd1ce
RagehAz <rageh.az@gmail.com>
2022-05-22 11:36:48 +0200

    - some progress hena w hnak - updates in zone manager,, should get all the code of country screen w ne5las bel search bel 5ara

--- lib/x_dashboard/a_modules/e_zones_editor/zones_manager_screen.dart
+++ lib/x_dashboard/a_modules/e_zones_editor/zones_manager_screen.dart
@@ -2,84 +2,90 @@ import 'dart:async';
 
 import 'package:bldrs/a_models/zone/country_model.dart';
 import 'package:bldrs/a_models/zone/flag_model.dart';
+import 'package:bldrs/b_views/y_views/d_zoning/d_1a_all_countries_buttons.dart';
 import 'package:bldrs/b_views/z_components/buttons/dream_box/dream_box.dart';
 import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
+import 'package:bldrs/b_views/z_components/layouts/page_bubble.dart';
 import 'package:bldrs/b_views/z_components/layouts/unfinished_night_sky.dart';
-import 'package:bldrs/d_providers/phrase_provider.dart';
 import 'package:bldrs/e_db/fire/methods/firestore.dart' as Fire;
 import 'package:bldrs/e_db/fire/methods/paths.dart';
 import 'package:bldrs/f_helpers/drafters/mappers.dart' as Mapper;
 import 'package:bldrs/f_helpers/drafters/scalers.dart' as Scale;
 import 'package:bldrs/f_helpers/drafters/tracers.dart';
-import 'package:bldrs/f_helpers/router/navigators.dart' as Nav;
 import 'package:bldrs/f_helpers/theme/colorz.dart';
 import 'package:bldrs/f_helpers/theme/ratioz.dart';
-import 'package:bldrs/x_dashboard/a_modules/e_zones_editor/country_screen.dart';
+import 'package:bldrs/x_dashboard/a_modules/e_zones_editor/zones_manager_controller.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ZonesEditorScreen extends StatefulWidget {
-  const ZonesEditorScreen({Key key}) : super(key: key);
-
+  /// --------------------------------------------------------------------------
+  const ZonesEditorScreen({
+    Key key
+  }) : super(key: key);
+  /// --------------------------------------------------------------------------
   @override
   _ZonesEditorScreenState createState() => _ZonesEditorScreenState();
+/// --------------------------------------------------------------------------
 }
 
 class _ZonesEditorScreenState extends State<ZonesEditorScreen> {
-  List<CountryModel> _countries;
-  ScrollController _scrollController;
+// -----------------------------------------------------------------------------
+  ValueNotifier<List<CountryModel>> _countries;
+  ScrollController _scrollController; /// tamam disposed
+  PageController _pageController; /// tamam disposed
   QueryDocumentSnapshot<Object> _lastSnap;
 // -----------------------------------------------------------------------------
-  /// --- FUTURE LOADING BLOCK
-  bool _loading = false;
-  Future<void> _triggerLoading({Function function}) async {
-    if (mounted) {
-      if (function == null) {
-        setState(() {
-          _loading = !_loading;
-        });
-      } else {
-        setState(() {
-          _loading = !_loading;
-          function();
-        });
-      }
-    }
-
-    _loading == true
-        ? blog('LOADING--------------------------------------')
-        : blog('LOADING COMPLETE--------------------------------------');
+  /// --- LOCAL LOADING BLOCK
+  final ValueNotifier<bool> _loading = ValueNotifier(false); /// tamam disposed
+// -----------------------------------
+  Future<void> _triggerLoading() async {
+    _loading.value = !_loading.value;
+    blogLoading(
+      loading: _loading.value,
+      callerName: 'ZonesEditorScreen',
+    );
   }
-
 // -----------------------------------------------------------------------------
   @override
   void initState() {
-    _scrollController = ScrollController();
     super.initState();
+    _scrollController = ScrollController();
+    _pageController = PageController();
+    _countries = ValueNotifier<List<CountryModel>>(<CountryModel>[]);
   }
-
 // -----------------------------------------------------------------------------
   bool _isInit = true;
   @override
   void didChangeDependencies() {
     if (_isInit) {
-      _triggerLoading().then((_) async {
-        await _readMoreCountries();
 
-        unawaited(_triggerLoading(function: () {
-          /// set new values here
-        }));
+      _triggerLoading().then((_) async {
+        // -------------------------------
+        await _readMoreCountries();
+        // -------------------------------
+        await _triggerLoading();
+
       });
+
     }
     _isInit = false;
     super.didChangeDependencies();
   }
 // -----------------------------------------------------------------------------
+  @override
+  void dispose() {
+    super.dispose();
+    _loading.dispose();
+    _countries.dispose();
+    _pageController.dispose();
+    _scrollController.dispose();
+  }
+// -----------------------------------------------------------------------------
   Future<void> _readMoreCountries() async {
-    if (_loading == false) {
-      setState(() {
-        _loading = true;
-      });
+
+    if (_loading.value == false) {
+      _loading.value = true;
     }
 
     final List<Map<String, dynamic>> _maps = await Fire.readSubCollectionDocs(
@@ -100,52 +106,103 @@ class _ZonesEditorScreenState extends State<ZonesEditorScreen> {
 
     final QueryDocumentSnapshot<Object> _snap = _maps[_maps.length - 1]['docSnapshot'];
 
-    setState(() {
-      _countries = _countriesModels;
+      _countries.value = _countriesModels;
       _lastSnap = _snap;
-      _loading = false;
-    });
-  }
+      _loading.value = false;
 
+  }
+// -----------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-    final double _screenWidth = Scale.superScreenWidth(context);
+
+    final double _screenHeight = Scale.superScreenHeightWithoutSafeArea(context);
 
     return MainLayout(
         pyramidsAreOn: true,
-        appBarType: AppBarType.basic,
+        sectionButtonIsOn: false,
+        zoneButtonIsOn: false,
+        appBarType: AppBarType.search,
         pageTitle: 'Zones Manager',
-        // appBarBackButton: true,
         skyType: SkyType.black,
-        layoutWidget: Mapper.canLoopList(_countries) == false ?
-        Container()
-            :
-        ListView.builder(
+        layoutWidget: PageView(
           physics: const BouncingScrollPhysics(),
-          controller: _scrollController,
-          padding: const EdgeInsets.only(top: Ratioz.stratosphere),
-          itemCount: _countries?.length,
-          itemBuilder: (BuildContext context, int index) {
+          controller: _pageController,
+          children: <Widget>[
 
-            final CountryModel _countryModel = _countries[index];
-            final String _countryName = superPhrase(context, _countryModel.id);
+            /// COUNTRIES PAGE
+            PageBubble(
+                screenHeightWithoutSafeArea: _screenHeight,
+                appBarType: AppBarType.search,
+                color: Colorz.white10,
+                child: ValueListenableBuilder(
+                  valueListenable: _countries,
+                  builder: (_, List<CountryModel> _countries, Widget child){
 
-            return DreamBox(
-              height: 100,
-              width: _screenWidth - (Ratioz.appBarMargin * 2),
-              icon: Flag.getFlagIconByCountryID(_countries[index].id),
-              verse: _countryName,
-              bubble: false,
-              color: Colorz.white20,
-              verseMaxLines: 2,
-              verseScaleFactor: 0.6,
-              margins: const EdgeInsets.all(7.5),
-              onTap: () => Nav.goToNewScreen(
-                  context: context,
-                  screen: CountryEditorScreen(country: _countries[index])
-              ),
-            );
-            },
-        ));
+                    if (Mapper.canLoopList(_countries) == false){
+                      return const SizedBox();
+                    }
+
+                    else {
+
+                      return AllCountriesButtons(
+                        padding: const EdgeInsets.only(
+                            top: Ratioz.appBarMargin,
+                            bottom: Ratioz.horizon
+                        ),
+                        onCountryTap: (String countryID) => onCountryEditorTap(
+                          context: context,
+                          countryID: countryID,
+                        ),
+                      );
+
+                      // return ListView.builder(
+                      //   physics: const BouncingScrollPhysics(),
+                      //   controller: _scrollController,
+                      //   padding: const EdgeInsets.only(top: Ratioz.stratosphere),
+                      //   itemCount: _countries?.length,
+                      //   itemBuilder: (BuildContext context, int index) {
+                      //
+                      //     final CountryModel _countryModel = _countries[index];
+                      //     final String _countryName = CountryModel.getTranslatedCountryName(
+                      //         context: context,
+                      //         countryID: _countryModel.id,
+                      //     );
+                      //
+                      //     return DreamBox(
+                      //       height: 50,
+                      //       width: PageBubble.clearWidth(context),
+                      //       icon: Flag.getFlagIconByCountryID(_countryModel.id),
+                      //       verse: _countryName,
+                      //       bubble: false,
+                      //       color: Colorz.white20,
+                      //       verseMaxLines: 2,
+                      //       verseScaleFactor: 0.8,
+                      //       verseCentered: false,
+                      //       margins: const EdgeInsets.only(bottom: 10),
+                      //       onTap: () => onCountryEditorTap(
+                      //         context: context,
+                      //         countryModel: _countryModel,
+                      //       ),
+                      //     );
+                      //
+                      //   },
+                      // );
+
+                    }
+                  },
+                ),
+            ),
+
+            /// CITIES PAGE
+            PageBubble(
+              screenHeightWithoutSafeArea: _screenHeight,
+              appBarType: AppBarType.search,
+              color: Colorz.bloodTest,
+              child: Container(),
+            ),
+
+          ],
+        ),
+    );
   }
 }
