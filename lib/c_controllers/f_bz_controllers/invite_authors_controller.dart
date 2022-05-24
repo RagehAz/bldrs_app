7fda0fa0e4a737eddcf09712ca6d86844637d335
RagehAz <rageh.az@gmail.com>
2022-05-24 18:10:12 +0200

    - updated security rules and progressing in notes

--- lib/c_controllers/f_bz_controllers/invite_authors_controller.dart
+++ lib/c_controllers/f_bz_controllers/invite_authors_controller.dart
@@ -1,14 +1,20 @@
 import 'package:bldrs/a_models/bz/bz_model.dart';
+import 'package:bldrs/a_models/secondary_models/note_model.dart';
 import 'package:bldrs/a_models/user/user_model.dart';
 import 'package:bldrs/b_views/x_screens/f_bz/f_3_add_author_screen.dart';
 import 'package:bldrs/b_views/y_views/g_user/b_1_user_profile_page.dart';
 import 'package:bldrs/b_views/z_components/dialogs/bottom_dialog/bottom_dialog.dart';
 import 'package:bldrs/b_views/z_components/dialogs/center_dialog/center_dialog.dart';
+import 'package:bldrs/b_views/z_components/dialogs/top_dialog/top_dialog.dart';
 import 'package:bldrs/b_views/z_components/sizing/expander.dart';
+import 'package:bldrs/d_providers/user_provider.dart';
+import 'package:bldrs/e_db/fire/ops/auth_ops.dart';
+import 'package:bldrs/e_db/fire/ops/note_ops.dart' as NoteFireOps;
 import 'package:bldrs/e_db/fire/search/user_fire_search.dart';
 import 'package:bldrs/f_helpers/drafters/text_checkers.dart';
 import 'package:bldrs/f_helpers/drafters/text_mod.dart';
 import 'package:bldrs/f_helpers/router/navigators.dart' as Nav;
+import 'package:bldrs/f_helpers/theme/colorz.dart';
 import 'package:flutter/material.dart';
 // -----------------------------------------------------------------------------
 
@@ -96,25 +102,53 @@ Future<void> onShowUserDialog({
 // -------------------------------
 Future<void> onInviteUserButtonTap({
   @required BuildContext context,
-  @required UserModel userModel,
+  @required UserModel selectedUser,
   @required BzModel bzModel,
 }) async {
 
   final bool _result = await CenterDialog.showCenterDialog(
     context: context,
     title: 'Send Invitation ?',
-    body: 'confirm sending invitation to ${userModel.name} to become an author of ${bzModel.name} account',
+    body: 'confirm sending invitation to ${selectedUser.name} to become an author of ${bzModel.name} account',
     boolDialog: true,
     height: 500,
     child: UserProfilePage(
-      userModel: userModel,
+      userModel: selectedUser,
       // showContacts: false,
     ),
   );
 
   if (_result == true){
 
-    blog('invite the user naaaaaaaw');
+    final UserModel _myUserModel = UsersProvider.proGetMyUserModel(context);
+
+    final NoteModel _note = NoteModel(
+      id: null, // will be defined in note create fire ops
+      senderID: superUserID(),
+      receiverID: selectedUser.id,
+      title: 'Business Account Invitation',
+      body: '${_myUserModel.name} sent you an invitation to become an Author for ${bzModel.name} business page',
+      metaData: NoteModel.defaultMetaData,
+      sentTime: DateTime.now(),
+      attachment: <String>['Accept', 'Decline'],
+      attachmentType: NoteAttachmentType.buttons,
+      seen: false,
+      seenTime: null,
+      sendFCM: true,
+    );
+
+    await NoteFireOps.createNote(
+        context: context,
+        noteModel: _note,
+    );
+
+    await TopDialog.showTopDialog(
+      context: context,
+      firstLine: 'Invitation Sent',
+      secondLine: 'Account authorship invitation has been sent to ${selectedUser.name} successfully',
+      color: Colorz.green255,
+      textColor: Colorz.white255,
+    );
 
   }
 
