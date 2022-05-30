9d089089a50d6ec244c55813afa0332a6be19c9c
RagehAz <rageh.az@gmail.com>
2022-05-30 09:08:18 +0200

    - currency manager updates

--- lib/x_dashboard/a_modules/k_currency_manager/currency_manager.dart
+++ lib/x_dashboard/a_modules/k_currency_manager/currency_manager.dart
@@ -1,4 +1,5 @@
 import 'package:bldrs/a_models/zone/currency_model.dart';
+import 'package:bldrs/b_views/z_components/buttons/flagbox_button.dart';
 import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
 import 'package:bldrs/b_views/z_components/layouts/page_bubble.dart';
 import 'package:bldrs/b_views/z_components/layouts/unfinished_night_sky.dart';
@@ -6,6 +7,7 @@ import 'package:bldrs/b_views/z_components/sizing/expander.dart';
 import 'package:bldrs/b_views/z_components/specs/data_creators/currency_button.dart';
 import 'package:bldrs/b_views/z_components/texting/super_verse.dart';
 import 'package:bldrs/d_providers/zone_provider.dart';
+import 'package:bldrs/f_helpers/drafters/borderers.dart';
 import 'package:bldrs/f_helpers/drafters/numeric.dart' as Numeric;
 import 'package:bldrs/f_helpers/drafters/scalers.dart';
 import 'package:bldrs/f_helpers/theme/colorz.dart';
@@ -63,15 +65,55 @@ class CurrencyManagerScreen extends StatelessWidget {
                   SuperVerse(
                     verse: _number,
                     labelColor: Colorz.black125,
+                    margin: 5,
                   ),
 
-                  CurrencyButton(
+                  Container(
                     width: 300,
-                    height: 50,
-                    countryID: _currency.countriesIDs.first,
-                    currency: _currency,
-                    onTap: null,
+                    decoration: BoxDecoration(
+                        color: Colorz.bloodTest,
+                        borderRadius: superBorderAll(context, 50 * 0.15)
+                    ),
+                    margin: superInsets(context: context, enBottom: 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+
+                        CurrencyButton(
+                          width: 300,
+                          height: 50,
+                          countryID: _currency.countriesIDs.last,
+                          currency: _currency,
+                          onTap: null,
+                        ),
+
+                        if (_currency.countriesIDs.length > 1)
+                        SizedBox(
+                          width: 300,
+                          height: 60,
+                          child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              physics: const BouncingScrollPhysics(),
+                              itemCount: _currency.countriesIDs.length,
+                              padding: superMargins(margins: 10),
+                              itemBuilder: (_, index){
+
+                                final String _countryID = _currency.countriesIDs[index];
+
+                                return FlagBox(
+                                  size: 35,
+                                  countryID: _countryID,
+                                );
+
+                              }
+                          ),
+                        ),
+
+                      ],
+
+                    ),
                   ),
+
 
                 ],
               );
