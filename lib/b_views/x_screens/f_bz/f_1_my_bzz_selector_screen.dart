da5b6a7b94bc446f2fc3fe3b1ea972e7d171d3ec
RagehAz <rageh.az@gmail.com>
2022-06-11 02:43:21 +0200

    - GREAT REFACTOR BABY, GOOD JOB,, YOU ARE GOOD,, AND YOU WORK HARD, KEEP UP, AND RISE

--- lib/b_views/x_screens/f_bz/f_1_my_bzz_selector_screen.dart
+++ lib/b_views/x_screens/f_bz/f_1_my_bzz_selector_screen.dart
@@ -67,7 +67,6 @@ class MyBzzSelectorScreen extends StatelessWidget {
                 icon: _bzModel.logo,
                 onTap: () => goToMyBzScreen(
                   context: context,
-                  myBzModel: _bzModel,
                 ),
 
               ),
