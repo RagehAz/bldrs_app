0ccbfbc2b1fdcc324df7b8c6508ccb8929525a8c
RagehAz <rageh.az@gmail.com>
2022-06-11 12:55:52 +0200

    - good job

--- lib/b_views/x_screens/f_bz/f_0_my_bz_screen.dart
+++ lib/b_views/x_screens/f_bz/f_0_my_bz_screen.dart
@@ -1,134 +1,28 @@
 import 'package:bldrs/a_models/bz/bz_model.dart';
 import 'package:bldrs/b_views/z_components/bz_profile/appbar/bz_credits_counter.dart';
 import 'package:bldrs/b_views/z_components/flyer/b_flyer_parts/a_header/bz_logo.dart';
-import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
 import 'package:bldrs/b_views/z_components/layouts/obelisk_layout/my_bz_screen_pages.dart';
-import 'package:bldrs/b_views/z_components/layouts/obelisk_layout/o_pyramids.dart';
-import 'package:bldrs/b_views/z_components/layouts/unfinished_night_sky.dart';
 import 'package:bldrs/b_views/z_components/sizing/expander.dart';
-import 'package:bldrs/c_controllers/f_bz_controllers/f_my_bz_screen_controller.dart';
-import 'package:bldrs/c_controllers/i_flyer_controllers/slides_controller.dart';
 import 'package:bldrs/d_providers/bzz_provider.dart';
 import 'package:bldrs/f_helpers/drafters/borderers.dart';
 import 'package:bldrs/f_helpers/drafters/numeric.dart' as Numeric;
-import 'package:bldrs/f_helpers/drafters/scalers.dart';
-import 'package:bldrs/f_helpers/drafters/sliders.dart' as Sliders;
-import 'package:bldrs/f_helpers/router/navigators.dart' as Nav;
 import 'package:bldrs/f_helpers/theme/ratioz.dart';
 import 'package:bldrs/x_dashboard/a_modules/a_test_labs/specialized_labs/new_navigators/nav_model.dart';
+import 'package:bldrs/x_dashboard/a_modules/a_test_labs/specialized_labs/new_navigators/obelisk_layout.dart';
 import 'package:flutter/material.dart';
 
-class MyBzScreen extends StatefulWidget {
+class MyBzScreen extends StatelessWidget {
   /// --------------------------------------------------------------------------
   const MyBzScreen({
     Key key
   }) : super(key: key);
   /// --------------------------------------------------------------------------
   @override
-  State<MyBzScreen> createState() => _MyBzScreenState();
-/// --------------------------------------------------------------------------
-}
-
-class _MyBzScreenState extends State<MyBzScreen> with SingleTickerProviderStateMixin {
-// -----------------------------------------------------------------------------
-  TabController _tabController;
-// -----------------------------------------------------------------------------
-  @override
-  void initState() {
-
-    _tabController = TabController(
-      vsync: this,
-      length: BzModel.bzPagesTabsTitlesInEnglishOnly.length,
-    );
-
-    _pageTitle = ValueNotifier(BzModel.getTabTitle(0));
-
-    _tabController.animation.addListener((){
-
-      onChangeMyBzScreenTabIndexWhileAnimation(
-        context: context,
-        tabController: _tabController,
-      );
-
-      onHorizontalSlideSwipe(
-        context: context,
-        newIndex: _tabController.index,
-        currentSlideIndex: _tabIndex,
-        swipeDirection: _swipeDirection,
-      );
-
-      _pageTitle.value = BzModel.getTabTitle(_tabIndex.value);
-
-    });
-
-    super.initState();
-  }
-// -----------------------------------------------------------------------------
-  @override
-  void dispose() {
-    _isExpanded.dispose();
-    super.dispose();
-  }
-// -----------------------------------------------------------------------------
-  final ValueNotifier<bool> _isExpanded = ValueNotifier(false);
-// -----------------------------------------------------------------------------
-  void onTriggerExpansion(){
-    _isExpanded.value = !_isExpanded.value;
-  }
-// -----------------------------------------------------------------------------
-  /// TAB INDEX
-  final ValueNotifier<int> _tabIndex = ValueNotifier(0);
-  /// PAGE TITLE
-  ValueNotifier<String> _pageTitle;
-  /// SWIPE DIRECTION
-  final ValueNotifier<Sliders.SwipeDirection> _swipeDirection = ValueNotifier(Sliders.SwipeDirection.next); /// tamam disposed
-// -------------------------------------------
-  Future<void> onRowTap(int index) async {
-
-    onHorizontalSlideSwipe(
-      context: context,
-      newIndex: index,
-      currentSlideIndex: _tabIndex,
-      swipeDirection: _swipeDirection,
-    );
-
-    _tabController.animateTo(_tabIndex.value,
-      duration: const Duration(milliseconds: 500),
-      curve: Curves.easeOutExpo,
-    );
-
-    _pageTitle.value = BzModel.getTabTitle(index);
-
-  }
-// -----------------------------------------------------------------------------
-
-  @override
   Widget build(BuildContext context) {
 
     final BzModel _bzModel = BzzProvider.proGetActiveBzModel(context: context, listen: true);
-    final double _screenHeight = superScreenHeightWithoutSafeArea(context);
 
-    return MainLayout(
-      zoneButtonIsOn: false,
-      sectionButtonIsOn: false,
-      skyType: SkyType.black,
-      appBarType: AppBarType.basic,
-      pageTitle: _pageTitle,
-      loading: ValueNotifier(false),
-      index: _tabIndex,
-      swipeDirection: _swipeDirection,
-      numberOfStrips: BzModel.bzTabsList.length,
-      canGoBack: false,
-      onBack: (){
-
-        if (_isExpanded.value == false){
-          Nav.goBack(context);
-        }
-        else {
-          _isExpanded.value = false;
-        }
-
-      },
+    return ObeliskLayout(
       appBarRowWidgets: <Widget>[
 
         const Expander(),
@@ -139,7 +33,6 @@ class _MyBzScreenState extends State<MyBzScreen> with SingleTickerProviderStateM
           ankhsCredit: Numeric.formatNumToCounterCaliber(context, 123),
         ),
 
-
         BzLogo(
           width: 40,
           image: _bzModel.logo,
@@ -148,42 +41,22 @@ class _MyBzScreenState extends State<MyBzScreen> with SingleTickerProviderStateM
         ),
 
       ],
-      layoutWidget: Stack(
-        children: <Widget>[
+      navModels: <NavModel>[
 
-          /// PAGES
-          MyBzScreenPages(
-            screenHeight: _screenHeight,
-            tabController: _tabController,
-          ),
+        ...List.generate(BzModel.bzTabsList.length, (index){
 
-          /// PYRAMIDS NAVIGATOR
-          SuperPyramids(
-            isExpanded: _isExpanded,
-            onExpansion: onTriggerExpansion,
-            onRowTap: onRowTap,
-            tabIndex: _tabIndex,
-            navModels: <NavModel>[
+          final BzTab _bzTab = BzModel.bzTabsList[index];
 
-              ...List.generate(BzModel.bzTabsList.length, (index){
+          return NavModel(
+            title: BzModel.translateBzTab(context: context, bzTab: _bzTab),
+            icon: BzModel.getBzTabIcon(_bzTab),
+            screen: MyBzScreenPages.pages[index],
+          );
 
-                final BzTab _bzTab = BzModel.bzTabsList[index];
+        }),
 
-                return NavModel(
-                  title: BzModel.translateBzTab(_bzTab),
-                  icon: BzModel.getBzTabIcon(_bzTab),
-                  screen: MyBzScreenPages.pages[index],
-                );
+      ],
 
-              }),
-
-            ],
-          ),
-
-        ],
-      ),
     );
-
   }
-
 }
--- lib/b_views/x_screens/g_user/g_0_user_profile_screen.dart
+++ lib/b_views/x_screens/g_user/g_0_user_profile_screen.dart
@@ -1,82 +1,34 @@
 import 'package:bldrs/a_models/user/user_model.dart';
-import 'package:bldrs/b_views/y_views/g_user/a_user_screen_view.dart';
-import 'package:bldrs/b_views/z_components/buttons/dream_box/dream_box.dart';
-import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
-import 'package:bldrs/b_views/z_components/layouts/unfinished_night_sky.dart';
-import 'package:bldrs/b_views/z_components/sizing/expander.dart';
-import 'package:bldrs/c_controllers/g_user_controllers/user_screen_controller.dart';
-import 'package:bldrs/d_providers/phrase_provider.dart';
-import 'package:bldrs/f_helpers/theme/iconz.dart' as Iconz;
-import 'package:bldrs/f_helpers/theme/ratioz.dart';
+import 'package:bldrs/b_views/y_views/g_user/b_0_user_screen_view_pages.dart';
+import 'package:bldrs/x_dashboard/a_modules/a_test_labs/specialized_labs/new_navigators/nav_model.dart';
+import 'package:bldrs/x_dashboard/a_modules/a_test_labs/specialized_labs/new_navigators/obelisk_layout.dart';
 import 'package:flutter/material.dart';
 
-class UserProfileScreen extends StatefulWidget {
+class UserProfileScreen extends StatelessWidget {
   /// --------------------------------------------------------------------------
   const UserProfileScreen({
     Key key
   }) : super(key: key);
   /// --------------------------------------------------------------------------
   @override
-  _UserProfileScreenState createState() => _UserProfileScreenState();
-/// --------------------------------------------------------------------------
-}
-
-class _UserProfileScreenState extends State<UserProfileScreen> with SingleTickerProviderStateMixin {
-  TabController _tabController;
-// -----------------------------------------------------------------------------
-  @override
-  void initState() {
-
-    _tabController = TabController(
-      vsync: this,
-      length: UserModel.userProfileTabsList.length,
-      initialIndex: getInitialUserScreenTabIndex(context),
-    );
-
-    _tabController.animation.addListener(
-            () => onChangeUserScreenTabIndexWhileAnimation(
-              context: context,
-              tabController: _tabController,
-            )
-    );
-
-    super.initState();
-  }
-// -----------------------------------------------------------------------------
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose(); /// tamam
-  }
-// -----------------------------------------------------------------------------
-  @override
   Widget build(BuildContext context) {
 
-    return MainLayout(
-      appBarType: AppBarType.basic,
-      skyType: SkyType.black,
-      pyramidsAreOn: true,
-      sectionButtonIsOn: false,
-      zoneButtonIsOn: false,
-      pageTitle: superPhrase(context, 'phid_my_account'),
-      appBarRowWidgets: <Widget>[
+    return ObeliskLayout(
+      navModels: <NavModel>[
 
-        const Expander(),
+        ...List.generate(UserModel.userProfileTabsList.length, (index){
 
-        DreamBox(
-          height: Ratioz.appBarButtonSize,
-          width: Ratioz.appBarButtonSize,
-          icon: Iconz.more,
-          iconSizeFactor: 0.6,
-          margins: const EdgeInsets.symmetric(horizontal: Ratioz.appBarPadding),
-          onTap: () => onMoreOptionsTap(context),
-        ),
+          final UserTab _userTab = UserModel.userProfileTabsList[index];
+
+          return NavModel(
+            title: UserModel.translateUserTab(context: context, userTab: _userTab),
+            icon: UserModel.getUserTabIcon(_userTab),
+            screen: UserScreenViewPages.pages[index],
+          );
+
+        }),
 
       ],
-      layoutWidget: UserScreenView(
-        tabController: _tabController,
-      ),
-
     );
 
   }
