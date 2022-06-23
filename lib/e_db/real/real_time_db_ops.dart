f5c79e98029dcdbff1220025b44fff0171f0d0c8
RagehAz <rageh.az@gmail.com>
2022-06-23 15:31:20 +0200

    - re-added real time db ops - records stuff

---
+++ lib/e_db/real/real_time_db_ops.dart
@@ -0,0 +1,224 @@
+import 'dart:convert';
+
+import 'package:bldrs/a_models/secondary_models/error_helpers.dart';
+import 'package:bldrs/b_views/z_components/dialogs/center_dialog/center_dialog.dart';
+import 'package:bldrs/f_helpers/drafters/tracers.dart';
+import 'package:bldrs/f_helpers/theme/colorz.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+abstract class RealTimeOps{
+
+  RealTimeOps();
+
+// -----------------------------------------------------------------------------
+
+  /// REFERENCES
+
+// ----------------------------------------
+  static Uri collectionLink(String collName){
+    return Uri.parse('https://bldrsnet.firebaseio.com/$collName.json');
+  }
+// ----------------------------------------
+  static Uri docLink({
+    @required String collName,
+    @required String docName,
+  }){
+    return Uri.parse('https://bldrsnet.firebaseio.com/$collName/$docName.json');
+  }
+// -----------------------------------------------------------------------------
+
+  /// ERROR HANDLING
+
+// ----------------------------------------
+  static Future<void> onHttpError({
+    @required BuildContext context,
+    @required String error,
+  }) async {
+
+    await CenterDialog.showCenterDialog(
+      context: context,
+      title: 'Something went wrong',
+      body: error,
+      color: Colorz.red255,
+    );
+
+  }
+// -----------------------------------------------------------------------------
+
+  /// CREATE
+
+// ----------------------------------------
+  /// TESTED : WORKS PERFECT
+  static Future<String> createDoc({
+    @required BuildContext context,
+    @required String collName,
+    @required Map<String, Object> input,
+  }) async {
+
+    String _docID;
+
+    await tryAndCatch(
+        context: context,
+        functions: () async {
+
+          /// post map to realtime database
+          final http.Response _response = await http.post(
+            collectionLink(collName),
+            body: json.encode(input),
+          );
+
+          blog('response is : $_response');
+
+          /// --- get doc ID;
+          _docID = json.decode(_response.body)['name'];
+
+          },
+        onError: (String error) async {
+
+          await onHttpError(
+              context: context,
+              error: error,
+          );
+
+        }
+
+        );
+
+    return _docID;
+  }
+// -----------------------------------------------------------------------------
+  /// TESTED : WORKS PERFECT
+  static Future<void> createNamedDoc({
+    @required BuildContext context,
+    @required String collName,
+    @required String docName,
+    @required Map<String, Object> input,
+  }) async {
+
+    await updateDoc(
+        context: context,
+        collName: collName,
+        docName: docName,
+        input: input,
+    );
+
+  }
+// -----------------------------------------------------------------------------
+
+  /// READ
+
+// ----------------------------------------
+  /// TESTED : WORKS PERFECT
+  static Future<Map<String, Object>> readDoc({
+    @required BuildContext context,
+    @required String collName,
+    @required String docName,
+  }) async {
+
+    Map<String, Object> _map;
+
+    await tryAndCatch(
+        context: context,
+        functions: () async {
+
+          /// READ data
+          final http.Response response = await http.get(
+              docLink(
+                collName: collName,
+                docName: docName,
+              )
+          );
+
+          _map = json.decode(response.body);
+
+        },
+
+        onError: (error) async {
+
+          await onHttpError(
+            context: context,
+            error: error,
+          );
+
+        }
+    );
+
+    return _map;
+  }
+// -----------------------------------------------------------------------------
+
+  /// UPDATE
+
+// ----------------------------------------
+  /// TESTED : WORKS PERFECT
+  static Future<void> updateDoc({
+    @required BuildContext context,
+    @required String collName,
+    @required String docName,
+    @required Map<String, Object> input,
+  }) async {
+
+    await tryAndCatch(
+        context: context,
+        functions: () async {
+
+          /// post map to realtime database
+          final http.Response _response = await http.patch(
+            docLink(
+              collName: collName,
+              docName: docName,
+            ),
+            body: json.encode(input),
+          );
+
+          blog('response is : $_response');
+
+          /// --- get things
+          // dynamic things = json.decode(_response.body)['name'];
+
+        },
+        onError: (String error) async {
+
+          await onHttpError(
+            context: context,
+            error: error,
+          );
+
+        }
+
+    );
+
+  }
+// -----------------------------------------------------------------------------
+
+  /// DELETE
+
+// ----------------------------------------
+  /// TESTED : WORKS PERFECT
+  static Future<void> deleteDoc({
+    @required BuildContext context,
+    @required String collName,
+    @required String docName,
+  }) async {
+
+    final http.Response _response = await http.delete(docLink(collName: collName, docName: docName),);
+
+    if(_response.statusCode >= 400){
+
+      // can assign 'failed' value to a variable and return it from here
+      // to be able to handle failed scenarios to see wether to keep or delete
+      // the doc locally on providers or ldbs
+
+      await onHttpError(
+        context: context,
+        error: _response.body,
+      );
+
+      // throw HttpException('Could not delete Business');
+    }
+
+  }
+// -----------------------------------------------------------------------------
+
+}
--- lib/x_dashboard/a_modules/a_test_labs/specialized_labs/records_test.dart
+++ lib/x_dashboard/a_modules/a_test_labs/specialized_labs/records_test.dart
@@ -1,18 +1,20 @@
+import 'package:bldrs/a_models/secondary_models/flyer_counter_model.dart';
 import 'package:bldrs/a_models/secondary_models/record_model.dart';
 import 'package:bldrs/b_views/z_components/buttons/settings_wide_button.dart';
 import 'package:bldrs/b_views/z_components/layouts/custom_layouts/centered_list_layout.dart';
-import 'package:bldrs/b_views/z_components/sizing/expander.dart';
 import 'package:bldrs/b_views/z_components/streamers/fire_coll_streamer.dart';
 import 'package:bldrs/b_views/z_components/texting/data_strip.dart';
-import 'package:bldrs/b_views/z_components/texting/super_verse.dart';
 import 'package:bldrs/e_db/fire/fire_models/query_parameters.dart';
+import 'package:bldrs/e_db/fire/foundation/paths.dart';
 import 'package:bldrs/e_db/fire/ops/auth_ops.dart';
+import 'package:bldrs/e_db/real/real_time_colls.dart';
+import 'package:bldrs/e_db/real/real_time_db_ops.dart';
+import 'package:bldrs/f_helpers/drafters/mappers.dart';
+import 'package:bldrs/f_helpers/drafters/numeric.dart';
 import 'package:bldrs/f_helpers/drafters/scalers.dart';
 import 'package:bldrs/f_helpers/theme/colorz.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
 import 'package:bldrs/f_helpers/theme/iconz.dart' as Iconz;
-import 'package:bldrs/e_db/fire/foundation/firestore.dart' as Fire;
+import 'package:flutter/material.dart';
 
 class RecordsTestScreen extends StatelessWidget {
 
@@ -23,40 +25,125 @@ class RecordsTestScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    // RealTimeOps
+
     return CenteredListLayout(
       columnChildren: <Widget>[
 
+        /// RECORD CREATION
         SettingsWideButton(
-          verse: 'create save flyer record',
+          verse: 'CREATE RECORD',
           icon: Iconz.addFlyer,
           onTap: () async {
-            
+
+            final String _randomID = createUniqueID().toString();
+
             final RecordModel _recordModel = RecordModel.createSaveRecord(
-                userID: superUserID(), 
-                flyerID: 'flyerID',
-                slideIndex: 0,
-            );
-            
-            final DocumentReference<Object> _docRef = await Fire.createDoc(
-                context: context,
-                collName: 'nour',
-                input: _recordModel.toMap(toJSON: false),
+              userID: superUserID(),
+              flyerID: _randomID,
+              slideIndex: 0,
             );
 
-            blog('doc ref id is : ${_docRef.id}');
-            
+            await RealTimeOps.createDoc(
+              context: context,
+              collName: RealTimeColl.records,
+              input: _recordModel.toMap(toJSON: true),
+            );
+
+            // blog('doc ref id is : ${_docRef.id}');
+
           },
         ),
 
+        /// CREATE NEW FLYER COUNTER
+        SettingsWideButton(
+          verse: 'CREATE NEW FLYER COUNTER ( 1656001361010677 )',
+          icon: Iconz.addFlyer,
+          onTap: () async {
+
+            const String _randomID = '1656001361010677';//createUniqueID().toString();
+
+            final FlyerCounterModel _flyerStats = FlyerCounterModel.createInitialModel(_randomID);
+
+            await RealTimeOps.createNamedDoc(
+              context: context,
+              collName: RealTimeColl.flyersCounters,
+              docName: _flyerStats.flyerID,
+              input: _flyerStats.copyWith(saves: 15402).toMap(),
+            );
+
+            // blog('doc ref id is : ${_docRef.id}');
+
+          },
+        ),
+
+        /// UPDATE EXISTING FLYER COUNTER ( 1656001361010677 )
+        SettingsWideButton(
+          verse: 'UPDATE FLYER COUNTER 1656001361010677',
+          icon: Iconz.addFlyer,
+          onTap: () async {
+
+            final String _randomID = createUniqueID().toString();
+
+            final FlyerCounterModel _flyerStats = FlyerCounterModel.createInitialModel(_randomID);
+
+            await RealTimeOps.updateDoc(
+              context: context,
+              collName: RealTimeColl.flyersCounters,
+              docName: '1656001361010677',
+              input: _flyerStats.copyWith(saves: 65464564054054).toMap(),
+            );
+
+            // blog('doc ref id is : ${_docRef.id}');
+
+          },
+        ),
+
+        /// READ FLYER COUNTER
+        SettingsWideButton(
+          verse: 'READ FLYER SCOUNTER',
+          icon: Iconz.addFlyer,
+          onTap: () async {
+
+            final Map<String, dynamic> _map = await RealTimeOps.readDoc(
+                context: context,
+                collName: RealTimeColl.flyersCounters,
+                docName: '1656001361010677',
+            );
+
+            blogMap(_map, methodName: 'REAL FUCKING TIME MAP IS :');
+
+          },
+        ),
+
+        /// DELETE FLYER COUNTER
+        SettingsWideButton(
+          verse: 'delete FLYER SCOUNTER ( 1656001361010677 )',
+          icon: Iconz.addFlyer,
+          onTap: () async {
+
+            await RealTimeOps.deleteDoc(
+              context: context,
+              collName: RealTimeColl.flyersCounters,
+              docName: '1656001361010677',
+            );
+
+            // blogMap(_map, methodName: 'REAL FUCKING TIME MAP IS :');
+
+          },
+        ),
 
         Container(
           width: superScreenWidth(context),
           height: superScreenHeight(context),
           color: Colorz.bloodTest,
           child: FireCollStreamer(
-            queryParameters: const QueryModel(
-              collName: 'nour',
+            queryParameters: QueryModel(
+              collName: FireColl.records,
               limit: 100,
+              onDataChanged: (List<Map<String, dynamic>> newMaps){
+
+              }
             ),
             builder: (_, List<Map<String, dynamic>> maps){
 
