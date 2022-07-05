db045ad316e8eb95dfd184ed781a145e8e0f315a
RagehAz <rageh.az@gmail.com>
2022-07-05 16:02:20 +0200

    - created super rage7

---
+++ lib/x_dashboard/a_modules/a_test_labs/specialized_labs/animations_lab/super_rage7.dart
@@ -0,0 +1,229 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:bldrs/b_views/z_components/animators/widget_fader.dart';
+import 'package:bldrs/b_views/z_components/artworks/pyramids.dart';
+import 'package:bldrs/b_views/z_components/balloons/balloons.dart';
+import 'package:bldrs/b_views/z_components/balloons/user_balloon_structure/b_balloona.dart';
+import 'package:bldrs/b_views/z_components/images/super_image.dart';
+import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
+import 'package:bldrs/b_views/z_components/sizing/expander.dart';
+import 'package:bldrs/b_views/z_components/texting/super_verse.dart';
+import 'package:bldrs/f_helpers/drafters/mappers.dart';
+import 'package:bldrs/f_helpers/drafters/scalers.dart' as Scale;
+import 'package:bldrs/f_helpers/theme/colorz.dart';
+import 'package:bldrs/f_helpers/theme/iconz.dart' as Iconz;
+import 'package:flutter/material.dart';
+
+class SuperRage7Screen extends StatelessWidget {
+
+  const SuperRage7Screen({
+    Key key
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final double _screenWidth = Scale.superScreenWidth(context);
+    final double _screenHeight = Scale.superScreenHeight(context);
+
+    return MainLayout(
+      sectionButtonIsOn: false,
+      pyramidsAreOn: true,
+      pyramidType: PyramidType.crystalYellow,
+      loading: ValueNotifier(true),
+      zoneButtonIsOn: false,
+      appBarType: AppBarType.basic,
+      pageTitle: 'Rageh Azzazi',
+      layoutWidget: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+
+          Center(
+            child: WidgetFader(
+              fadeType: FadeType.repeatAndReverse,
+              duration: const Duration(milliseconds: 100),
+              builder: (double value, Widget child){
+
+                // blog('building : value : $value');
+
+                if (value < 0.5){
+                  return  const SuperImage(
+                    width: 250,
+                    height: 250,
+                    pic: Iconz.dvRageh2,
+                    corners: 50,
+                  );
+                }
+                else {
+                  return  const SuperImage(
+                    width: 250,
+                    height: 250,
+                    pic: Iconz.dvRageh,
+                    corners: 50,
+                  );
+                }
+
+              },
+            ),
+          ),
+
+          Positioned(
+            top: _screenHeight * 0.5 - 125,
+            left: _screenWidth * 0.5 + 60,
+            child: SizedBox(
+              width: 150,
+              height: 150,
+              child: Stack(
+                alignment: Alignment.center,
+                children: const <Widget>[
+
+                  Balloona(
+                    loading: false,
+                    balloonWidth: 150,
+                    balloonType: BalloonType.speaking,
+                    balloonColor: Colorz.white255,
+                    shadowIsOn: true,
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(25),
+                    child: TalkingBalloon(
+                      lines: <String>[
+
+                        'صباح الخير يا جماعة',
+                        'كل سنة و انتم طيبين',
+                        'كل سنة و انتم طيبين',
+                        'كل سنة و انتم طيبين',
+                        'كل سنة و انتم طيبين',
+                        'كل سنة و انتم طيبين',
+                        'كل سنة و انتم طيبين',
+                        'كل سنة و انتم طيبين',
+                        'كل سنة و انتم طيبين',
+
+
+                        // 'مساء الخير يا أحمد بيه',
+                        // 'حابب أرحب بيك معانا',
+                        // 'قولي عايز ايه',
+                        // 'ده مش استاجرام و لا أولكس',
+                        // 'و لا الكلام الفارغ ده',
+                        // 'هنا مكان شغل',
+                        // 'أشوف منك تعليق ميعجبنيش',
+                        // 'صورة ماتكيفنيش',
+                        // 'هازعل منك',
+                        //
+                        // 'بس انت شكلك مستخدم ذوق',
+                        // 'و بتفهم في الأصول',
+                        // 'خش خدلك لفة و لو عزت حاجة تعالى قولي',
+                        // 'نورت التطبيق يا حبيب قلبي',
+                        // 'اتفضضض اتفضل',
+                        // 'دوس عالزرار أهو',
+                        // 'أهو',
+                      ],
+                    ),
+
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+
+
+        ],
+      ),
+    );
+
+  }
+}
+
+class TalkingBalloon extends StatelessWidget {
+
+  const TalkingBalloon({
+    @required this.lines,
+    Key key
+  }) : super(key: key);
+
+  final List<String> lines;
+// -----------------------------------------------------------------------------
+
+  static TextStyle getStyle({
+    @required BuildContext context,
+  }){
+    return SuperVerse.createStyle(
+      context: context,
+      color: Colorz.black255,
+      size: 2,
+      weight: VerseWeight.bold,
+    );
+  }
+// -----------------------------------------------------------------------------
+  static TypewriterAnimatedText generateLine({
+    @required BuildContext context,
+    @required String text,
+  }){
+    return TypewriterAnimatedText(
+      text,
+      speed: const Duration(milliseconds: 100),
+      textAlign: TextAlign.center,
+      curve: Curves.easeOut,
+      textStyle: getStyle(context: context),
+      cursor: '',
+    );
+  }
+// -----------------------------------------------------------------------------
+  static List<TypewriterAnimatedText> generateLines({
+    @required BuildContext context,
+    @required List<String> lines,
+}){
+    final List<TypewriterAnimatedText> _lines = <TypewriterAnimatedText>[];
+
+    if (checkCanLoopList(lines) == true){
+
+      for (final String line in lines){
+
+        final TypewriterAnimatedText _line = generateLine(
+            context: context,
+            text: line,
+        );
+
+        _lines.add(_line);
+
+      }
+
+    }
+
+    return _lines;
+  }
+// -----------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+
+    return AnimatedTextKit(
+      displayFullTextOnTap: true,
+      // isRepeatingAnimation: true,
+      // onFinished: (){
+      //   blog('finished type writer');
+      // },
+      onNext: (int x, bool what){
+        blog('onNext : x : $x : what : $what');
+      },
+      onNextBeforePause: (int x, bool what){
+        blog('onNextBeforePause : x : $x : what : $what');
+      },
+      // pause: const Duration(seconds: 2),
+      repeatForever: true,
+      stopPauseOnTap: true,
+      totalRepeatCount: 10,
+      onTap: (){
+        blog('animated text is tapped');
+      },
+      animatedTexts: <AnimatedText>[
+
+        ...generateLines(
+            context: context,
+            lines: lines
+        ),
+
+      ],
+    );
+  }
+}
