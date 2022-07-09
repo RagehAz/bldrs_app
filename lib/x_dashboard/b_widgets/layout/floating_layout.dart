4127d783c283d50fed0151b765f9186ea3303334
RagehAz <rageh.az@gmail.com>
2022-07-10 00:30:02 +0200

    - refactoring zone selection

--- lib/x_dashboard/b_widgets/layout/floating_layout.dart
+++ lib/x_dashboard/b_widgets/layout/floating_layout.dart
@@ -1,24 +1,24 @@
-import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
-import 'package:bldrs/f_helpers/drafters/scalers.dart' as Scale;
-import 'package:flutter/material.dart';
-
-class FloatingLayout extends StatelessWidget {
-  /// --------------------------------------------------------------------------
-  const FloatingLayout({
-    @required this.child,
-    Key key,
-  }) : super(key: key);
-  /// --------------------------------------------------------------------------
-  final Widget child;
-  /// --------------------------------------------------------------------------
-  @override
-  Widget build(BuildContext context) {
-
-    return SizedBox(
-      width: Scale.superScreenWidth(context),
-      height: MainLayout.clearHeight(context),
-      child: child,
-    );
-
-  }
-}
+// import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
+// import 'package:bldrs/f_helpers/drafters/scalers.dart' as Scale;
+// import 'package:flutter/material.dart';
+//
+// class FloatingLayout extends StatelessWidget {
+//   /// --------------------------------------------------------------------------
+//   const FloatingLayout({
+//     @required this.child,
+//     Key key,
+//   }) : super(key: key);
+//   /// --------------------------------------------------------------------------
+//   final Widget child;
+//   /// --------------------------------------------------------------------------
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return SizedBox(
+//       width: Scale.superScreenWidth(context),
+//       height: MainLayout.clearHeight(context),
+//       child: child,
+//     );
+//
+//   }
+// }
