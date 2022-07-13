3ad3a0f0c61316c710f56851558675ac79f3727b
RagehAz <rageh.az@gmail.com>
2022-07-13 18:21:19 +0200

    - WE NEED TO MASTER CLOUD FUNCTIONS BA2A, SWITCH TO LEARNING MODE PLEASE

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,33 @@
+// --------------------------------------------------------------------------
+
+/// IMPORTS
+
+// -------------------------------------
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const myImport = require("./helperes");
+// --------------------------------------------------------------------------
 
+/// INITIALIZATION
+
+// -------------------------------------
 admin.initializeApp();
-
-
 const fireFunction = functions.firestore;
 const fbm = admin.messaging();
 const fireAdmin = admin.firestore();
+// --------------------------------------------------------------------------
 
-// COLLECTION PATHS
+/// DOCS / PATHS
+
+// -------------------------------------
 // const flyerDoc = "flyers/{flyerID}";
 // const userDoc = "users/{userID}";
 const docStatistics = fireAdmin.collection("admin").doc("statistics");
+// --------------------------------------------------------------------------
 
+/// FUNCTIONS
+
+// -------------------------------------
 // T001 : DB / admin / statistics.numberOfUsers increment 1
 exports.t001_onCreateNewFirebaseUser = functions.auth.user().onCreate(
     (user) => {
@@ -22,7 +36,7 @@ exports.t001_onCreateNewFirebaseUser = functions.auth.user().onCreate(
         numberOfUsers: admin.firestore.FieldValue.increment(1),
       });
     });
-
+// -------------------------------------
 // T002 : DB / admin / statistics.numberOfUsers increment -1
 exports.t002_onDeleteFirebaseUser = functions.auth.user().onDelete(
     (user) => {
@@ -31,7 +45,7 @@ exports.t002_onDeleteFirebaseUser = functions.auth.user().onDelete(
         numberOfUsers: admin.firestore.FieldValue.increment(-1),
       });
     });
-
+// -------------------------------------
 // T003 : DB / admin / statistics.numberOfCountries increment 1
 exports.t003_onCreateNewCountry = fireFunction.document("countries/{countryID}")
     .onCreate(
@@ -43,7 +57,7 @@ exports.t003_onCreateNewCountry = fireFunction.document("countries/{countryID}")
           });
         }
     );
-
+// -------------------------------------
 // T004 : DB / admin / statistics.numberOfCountries increment -1
 exports.t004_onDeleteCountry = fireFunction.document("countries/{countryID}")
     .onDelete(
@@ -55,7 +69,7 @@ exports.t004_onDeleteCountry = fireFunction.document("countries/{countryID}")
           });
         }
     );
-
+// -------------------------------------
 // T005 : DB / admin / statistics.numberOfBzz increment 1
 exports.t005_onCreateNewBz = fireFunction.document("bzz/{bzID}")
     .onCreate(
@@ -67,7 +81,7 @@ exports.t005_onCreateNewBz = fireFunction.document("bzz/{bzID}")
           });
         }
     );
-
+// -------------------------------------
 // T006 : DB / admin / statistics.numberOfBzz increment -1
 exports.t006_onDeleteBz = fireFunction.document("bzz/{bzID}")
     .onDelete(
@@ -79,7 +93,7 @@ exports.t006_onDeleteBz = fireFunction.document("bzz/{bzID}")
           });
         }
     );
-
+// -------------------------------------
 // T007 : DB / admin / statistics.numberOfBzz increment 1
 exports.t007_onCreateNewFlyer = fireFunction.document("flyers/{id}")
     .onCreate(
@@ -98,7 +112,7 @@ exports.t007_onCreateNewFlyer = fireFunction.document("flyers/{id}")
           ]);
         }
     );
-
+// -------------------------------------
 // T008 : DB / admin / statistics.numberOfBzz increment -1
 exports.t008_onDeleteFlyer = fireFunction.document("flyers/{id}")
     .onDelete(
@@ -112,26 +126,26 @@ exports.t008_onDeleteFlyer = fireFunction.document("flyers/{id}")
           });
         }
     );
-
+// -------------------------------------
 // http request 1
 exports.x_randomNumber = functions.https.onRequest((request, response) => {
   const number = Math.round(Math.random() * 100);
   console.log("The Random Number is : " + number);
   response.send(number.toString());
 });
-
+// -------------------------------------
 // // http request 2
 // exports.toBlackHole = functions.https.onRequest((request, response) => {
 //   response.redirect("https://www.google.com");
 // });
-
+// -------------------------------------
 // http callable function
 exports.x_sayHello = functions.https.onCall((data, context) => {
   const name = data.name +
   "xx this string added inside the callable function xx";
   return `hello, Bldrs, welcome Mr ${name}`;
 });
-
+// -------------------------------------
 exports.x_myFunction = fireFunction
     .document("flyers/{flyer}")
     .onCreate((snapshot, context) => {
@@ -143,7 +157,7 @@ exports.x_myFunction = fireFunction
         },
       });
     });
-
+// -------------------------------------
 // to call a function for any db change u can say "/{collection}/{id}"
 exports.x_logEverything = fireFunction.document("{collection}/{id}")
     .onCreate((snap, context) => {
@@ -166,12 +180,12 @@ exports.x_logEverything = fireFunction.document("{collection}/{id}")
 
       return null;
     });
-
+// -------------------------------------
 // exports.onUserCreation = db.document("/users/{userID}")
 //     .onCreate((snap, context) => {
 //       const collection = context.params
 //   });
-
+// -------------------------------------
 // auth trigger (background trigger)
 // exports.userDeleted = functions.auth.user().onDelete((user) => {
 //  console.log(
@@ -181,11 +195,11 @@ exports.x_logEverything = fireFunction.document("{collection}/{id}")
 //  const doc = fire.collection("users").doc(user.uid);
 //  return doc.delete;
 // });
-
+// -------------------------------------
 // exports.newUserSignedUp = db.document(userDoc).onCreate(doc, context) => {
 // return fbm.sendToTopic
 // }
-
+// -------------------------------------
 // exports.notifyBzOnFlyerReview = functions.firestore
 //     .document("flyers/{flyerID}/reviews/{reviewID}")
 //     .onCreate((snapshot, context) => {
@@ -197,8 +211,13 @@ exports.x_logEverything = fireFunction.document("{collection}/{id}")
 //         },
 //       });
 //     });
+// --------------------------------------------------------------------------
 
+/// DEPLOYMENT
+
+// -------------------------------------
 // firebase deploy --only functions
 // firebase deploy --only functions:t007_onCreateNewFlyer
 // firebase login --reauth
 // firebase functions:log --only increaseNumberOfUsersInStatistics
+// --------------------------------------------------------------------------
