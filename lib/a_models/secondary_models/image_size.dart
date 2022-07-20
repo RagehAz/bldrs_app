ed730acc5e2476a151205c4401db40ced519ac83
RagehAz <rageh.az@gmail.com>
2022-07-20 19:36:10 +0200

    - upgraded to flutter 3. 5ara and fixed gradle shits

--- lib/a_models/secondary_models/image_size.dart
+++ lib/a_models/secondary_models/image_size.dart
@@ -6,7 +6,7 @@ import 'package:bldrs/f_helpers/drafters/imagers.dart' as Imagers;
 import 'package:bldrs/f_helpers/drafters/object_checkers.dart' as ObjectChecker;
 import 'package:bldrs/f_helpers/drafters/tracers.dart';
 import 'package:flutter/material.dart';
-import 'package:multi_image_picker2/multi_image_picker2.dart';
+import 'package:bldrs/image_picker/multi_image_picker_2/multi_image_picker2.dart';
 
 @immutable
 class ImageSize {
