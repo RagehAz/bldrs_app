c6c79299f0bfe931e025b5e76ae990b355c41f04
RagehAz <rageh.az@gmail.com>
2022-07-24 15:22:48 +0200

    - added refactors

--- lib/c_protocols/bz_protocols/a_bz_protocols.dart
+++ lib/c_protocols/bz_protocols/a_bz_protocols.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:bldrs/a_models/bz/bz_model.dart';
+import 'package:bldrs/a_models/user/user_model.dart';
 import 'package:bldrs/c_protocols/bz_protocols/compose_bzz.dart';
 import 'package:bldrs/c_protocols/bz_protocols/fetch_bzz.dart';
 import 'package:bldrs/c_protocols/bz_protocols/renovate_bzz.dart';
@@ -17,7 +18,15 @@ class BzProtocols {
 /// COMPOSE
 
 // ----------------------------------
-  static Future<void> composeBz() => ComposeBzProtocol.compose();
+  static Future<void> composeBz({
+    @required BuildContext context,
+    @required BzModel newBzModel,
+    @required UserModel userModel,
+  }) => ComposeBzProtocol.compose(
+    context: context,
+    newBzModel: newBzModel,
+    userModel: userModel,
+  );
 // -----------------------------------------------------------------------------
 
 /// FETCH
@@ -43,12 +52,58 @@ class BzProtocols {
 /// RENOVATE
 
 // ----------------------------------
-  static Future<void> renovateBz() => RenovateBzProtocol.renovateBz();
+  static Future<BzModel> renovateBz({
+    @required BuildContext context,
+    @required BzModel newBzModel,
+    @required BzModel oldBzModel,
+    @required bool showWaitDialog,
+    @required bool navigateToBzInfoPageOnEnd,
+  }) => RenovateBzProtocol.renovateBz(
+    context: context,
+    newBzModel: newBzModel,
+    oldBzModel: oldBzModel,
+    showWaitDialog: showWaitDialog,
+    navigateToBzInfoPageOnEnd: navigateToBzInfoPageOnEnd,
+  );
+// ----------------------------------
+  static Future<void> updateBzLocally({
+    @required BuildContext context,
+    @required BzModel newBzModel,
+    @required BzModel oldBzModel,
+}) => RenovateBzProtocol.updateBzLocally(
+    context: context,
+    newBzModel: newBzModel,
+    oldBzModel: oldBzModel,
+  );
+// ----------------------------------
+  static Future<BzModel> completeBzZoneModel({
+    @required BuildContext context,
+    @required BzModel bzModel,
+  }) => RenovateBzProtocol.completeBzZoneModel(
+      context: context,
+      bzModel: bzModel
+  );
 // -----------------------------------------------------------------------------
 
 /// WIPE
 
 // ----------------------------------
-  static Future<void> wipeBz() => WipeBzProtocol.wipeBz();
+  static Future<void> wipeBz({
+    @required BuildContext context,
+    @required BzModel bzModel,
+    @required bool showWaitDialog,
+  }) => WipeBzProtocol.wipeBz(
+    context: context,
+    bzModel: bzModel,
+    showWaitDialog: showWaitDialog,
+  );
+// ----------------------------------
+  static Future<void> deleteLocally({
+    @required BuildContext context,
+    @required String bzID,
+  }) => WipeBzProtocol.deleteLocally(
+    context: context,
+    bzID: bzID,
+  );
 // -----------------------------------------------------------------------------
 }
