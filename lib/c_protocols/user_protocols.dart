74d190634dc6b20a208bea7f669e8749d329a4fd
RagehAz <rageh.az@gmail.com>
2022-07-24 16:05:13 +0200

    - cool refactors

--- lib/c_protocols/user_protocols.dart
+++ lib/c_protocols/user_protocols.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
+
 import 'package:bldrs/a_models/bz/author_model.dart';
 import 'package:bldrs/a_models/bz/bz_model.dart';
-import 'package:bldrs/a_models/user/auth_model.dart';
 import 'package:bldrs/a_models/user/user_model.dart';
 import 'package:bldrs/b_views/z_components/dialogs/wait_dialog/wait_dialog.dart';
 import 'package:bldrs/b_views/z_components/sizing/expander.dart';
@@ -9,6 +9,7 @@ import 'package:bldrs/c_controllers/g_bz_controllers/a_bz_profile/aaa3_bz_author
 import 'package:bldrs/c_protocols/author_protocols/a_author_protocols.dart';
 import 'package:bldrs/c_protocols/bz_protocols/a_bz_protocols.dart';
 import 'package:bldrs/c_protocols/record_protocols.dart';
+import 'package:bldrs/c_protocols/user_protocols/a_user_protocols.dart';
 import 'package:bldrs/d_providers/bzz_provider.dart';
 import 'package:bldrs/d_providers/user_provider.dart';
 import 'package:bldrs/e_db/fire/ops/user_ops.dart';
@@ -37,89 +38,7 @@ class UserProtocol {
   /// UPDATE
 
 // ----------------------------------
-  /// TESTED : WORKS PERFECT
-  static Future<UserModel> updateMyUserEverywhereProtocol({
-    @required BuildContext context,
-    @required UserModel newUserModel,
-  }) async {
 
-    blog('UserProtocol.updateMyUserEverywhereProtocol : START');
-
-    UserModel _uploadedModel;
-
-    final UserModel _oldUserModel = UsersProvider.proGetMyUserModel(
-        context: context,
-        listen: false,
-    );
-
-    final bool _modelsAreIdentical = UserModel.checkUsersAreIdentical(
-        user1: newUserModel,
-        user2: _oldUserModel
-    );
-
-    if (_modelsAreIdentical == false){
-
-      /// UPDATE USER IN FIRE STORE
-      _uploadedModel = await UserFireOps.updateUser(
-        context: context,
-        newUserModel: newUserModel,
-        oldUserModel: _oldUserModel,
-      );
-
-      await updateUserModelLocally(
-        context: context,
-        newUserModel: _uploadedModel,
-      );
-
-    }
-
-    blog('UserProtocol.updateMyUserEverywhereProtocol : END');
-
-    return _uploadedModel ?? newUserModel;
-  }
-// ----------------------------------
-  /// TESTED : WORKS PERFECT
-  static Future<void> updateUserModelLocally({
-    @required UserModel newUserModel,
-    @required BuildContext context,
-  }) async {
-
-    blog('UserProtocol.updateUserModelLocally : START');
-
-    final UserModel _oldUserModel = UsersProvider.proGetMyUserModel(
-      context: context,
-      listen: false,
-    );
-
-    final bool _modelsAreIdentical = UserModel.checkUsersAreIdentical(
-        user1: newUserModel,
-        user2: _oldUserModel
-    );
-
-    if (_modelsAreIdentical == false){
-
-      /// UPDATE USER AND AUTH IN PRO
-      UsersProvider.proUpdateUserAndAuthModels(
-        context: context,
-        userModel: newUserModel,
-        notify: true,
-      );
-
-      /// UPDATE USER MODEL IN LDB
-      await UserLDBOps.updateUserModel(newUserModel);
-
-      /// UPDATE AUTH MODEL IN LDB
-      final AuthModel _authModel = UsersProvider.proGetAuthModel(
-        context: context,
-        listen: false,
-      );
-      await AuthLDBOps.updateAuthModel(_authModel);
-
-    }
-
-    blog('UserProtocol.updateUserModelLocally : END');
-
-  }
 // ----------------------------------
   /// TESTED : WORKS PERFECT
   static Future<void> followingProtocol({
@@ -144,7 +63,7 @@ class UserProtocol {
         bzIDToFollow: bzID,
       );
 
-      await updateMyUserEverywhereProtocol(
+      await UserProtocols.renovateMyUserModel(
           context: context,
           newUserModel: _updatedModel,
       );
@@ -160,7 +79,7 @@ class UserProtocol {
         bzIDToUnFollow: bzID,
       );
 
-      await updateMyUserEverywhereProtocol(
+      await UserProtocols.renovateMyUserModel(
         context: context,
         newUserModel: _updatedModel,
       );
@@ -201,7 +120,7 @@ class UserProtocol {
         flyerIDToAdd: flyerID,
       );
 
-      await updateMyUserEverywhereProtocol(
+      await UserProtocols.renovateMyUserModel(
         context: context,
         newUserModel: _updatedModel,
       );
@@ -222,7 +141,7 @@ class UserProtocol {
         flyerIDToRemove: flyerID,
       );
 
-      await updateMyUserEverywhereProtocol(
+      await UserProtocols.renovateMyUserModel(
         context: context,
         newUserModel: _updatedModel,
       );
