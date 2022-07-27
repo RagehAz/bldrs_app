558771e83ad19630ed01563c79daf235e8dabae2
RagehAz <rageh.az@gmail.com>
2022-07-27 20:37:37 +0200

    - progress with loading screen

--- lib/b_views/x_screens/b_auth/a_auth_screen.dart
+++ lib/b_views/x_screens/b_auth/a_auth_screen.dart
@@ -15,11 +15,52 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
 // -----------------------------------------------------------------------------
+  /// --- LOADING
+  final ValueNotifier<bool> _loading = ValueNotifier(false); /// tamam disposed
+// -----------
+//   Future<void> _triggerLoading({bool setTo}) async {
+//     if (mounted == true){
+//       if (setTo == null){
+//         _loading.value = !_loading.value;
+//       }
+//       else {
+//         _loading.value = setTo;
+//       }
+//       blogLoading(loading: _loading.value, callerName: 'SearchBzzScreen',);
+//     }
+//   }
+// -----------------------------------------------------------------------------
+  bool _isInit = true;
+  @override
+  void didChangeDependencies() {
+    if (_isInit && mounted) {
+
+      // _triggerLoading().then((_) async {
+      //
+      //   final List<UserModel> _myUserModels = await UserLDBOps.readMyModels();
+      //
+      //   await _triggerLoading();
+      // });
+
+      _isInit = false;
+    }
+    super.didChangeDependencies();
+  }
+// -----------------------------------------------------------------------------
+  @override
+  void dispose() {
+    _loading.dispose();
+    super.dispose();
+  }
+// -----------------------------------------------------------------------------
   @override
   void initState() {
     super.initState();
   }
 // -----------------------------------------------------------------------------
+
+
+
   @override
   Widget build(BuildContext context) {
 
