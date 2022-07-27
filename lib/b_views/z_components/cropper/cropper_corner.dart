1dab2efe6f653da94de077defdf83c1c7d9817b6
RagehAz <rageh.az@gmail.com>
2022-07-27 20:19:30 +0200

    - ESHTA CROPPER PAGE

---
+++ lib/b_views/z_components/cropper/cropper_corner.dart
@@ -0,0 +1,32 @@
+import 'package:bldrs/b_views/z_components/images/super_image.dart';
+import 'package:bldrs/f_helpers/drafters/borderers.dart';
+import 'package:bldrs/f_helpers/theme/colorz.dart';
+import 'package:bldrs/f_helpers/theme/iconz.dart';
+import 'package:flutter/material.dart';
+
+class CropperCorner extends StatelessWidget {
+
+  const CropperCorner({
+    Key key
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      width: 32,
+      height: 32,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colorz.black20,
+        borderRadius: superBorderAll(context, 16),
+      ),
+      child: const SuperImage(
+        width: 10,
+        height: 10,
+        pic: Iconz.plus,
+      ),
+    );
+
+  }
+}
