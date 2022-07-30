3a9c675950b998e73afdea253e057b008a35078d
RagehAz <rageh.az@gmail.com>
2022-07-30 06:11:54 +0200

    - fixes fixes fixes in flyer maker - slide editor and good job ,,
    Yalla push

--- lib/b_views/z_components/flyer/b_flyer_parts/b_footer/c_footer_shadow.dart
+++ lib/b_views/z_components/flyer/b_flyer_parts/b_footer/c_footer_shadow.dart
@@ -1,4 +1,5 @@
 import 'package:bldrs/b_views/z_components/flyer/b_flyer_parts/b_footer/b_footer_box.dart';
+import 'package:bldrs/f_helpers/theme/colorz.dart';
 import 'package:bldrs/f_helpers/theme/iconz.dart';
 import 'package:flutter/material.dart';
 import 'package:websafe_svg/websafe_svg.dart';
@@ -17,7 +18,7 @@ class FooterShadow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    // final double _flyerBottomCorners = FooterBox.boxCornersValue(flyerBoxWidth);
+    final double _flyerBottomCorners = FooterBox.boxCornersValue(flyerBoxWidth);
 
     final double _footerHeight = FooterBox.collapsedHeight(
       context: context,
@@ -28,27 +29,12 @@ class FooterShadow extends StatelessWidget {
     return Align(
       key: const ValueKey<String>('FooterShadow'),
       alignment: Alignment.bottomCenter,
-      child: SizedBox(
+      child: WebsafeSvg.asset(
+        Iconz.footerShadow,
+        fit: BoxFit.fitWidth,
         width: flyerBoxWidth,
         height: _footerHeight,
-        // decoration: BoxDecoration(
-        //   // borderRadius: BorderRadius.only(
-        //   //   bottomLeft: Radius.circular(_flyerBottomCorners),
-        //   //   bottomRight: Radius.circular(_flyerBottomCorners),
-        //   // ),
-        //   // gradient: const LinearGradient(
-        //   //     begin: Alignment.topCenter,
-        //   //     end: Alignment.bottomCenter,
-        //   //     colors: <Color>[Colorz.black0, Colorz.black125, Colorz.black230],
-        //   //     stops: <double>[0.35, 0.85, 1]
-        //   // ),
-        // ),
-        child: WebsafeSvg.asset(
-          Iconz.footerShadow,
-          fit: BoxFit.fitWidth,
-          width: flyerBoxWidth,
-          height: _footerHeight,
-        ),
+        alignment: Alignment.bottomCenter,
       ),
     );
   }
