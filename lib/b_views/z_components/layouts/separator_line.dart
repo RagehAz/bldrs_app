47b3d6469de013b35866b001eebd7fe8a7972210
RagehAz <rageh.az@gmail.com>
2022-08-05 07:11:40 +0200

    - took some time,, but good job anyways,, rabbena yefregha isa

--- lib/b_views/z_components/layouts/separator_line.dart
+++ lib/b_views/z_components/layouts/separator_line.dart
@@ -11,6 +11,7 @@ class SeparatorLine extends StatelessWidget {
     this.width,
     this.margins,
     this.thickness = standardThickness,
+    this.color = Colorz.yellow255,
     Key key
   }) : super(key: key);
   /// --------------------------------------------------------------------------
@@ -18,8 +19,9 @@ class SeparatorLine extends StatelessWidget {
   final double width;
   final dynamic margins;
   final double thickness;
+  final Color color;
   /// --------------------------------------------------------------------------
-  static const double standardThickness = 0.25;
+  static const double standardThickness = 0.5;
   /// --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
@@ -34,7 +36,7 @@ class SeparatorLine extends StatelessWidget {
         height: thickness,
         // alignment: Aligners.superCenterAlignment(context),
         margin: Scale.superMargins(margins: _margins) ,
-        color: lineIsON ? Colorz.yellow255 : null,
+        color: lineIsON ? color : null,
         // child: Container(
         //   width: _width * 0.8,
         //   height: 0.25,
