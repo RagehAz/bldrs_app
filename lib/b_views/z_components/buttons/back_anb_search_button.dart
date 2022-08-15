c43948fcc694a3226fa1d3080bb77f4e0bb67197
RagehAz <rageh.az@gmail.com>
2022-08-15 17:01:28 +0200

    - fixed app bar

--- lib/b_views/z_components/buttons/back_anb_search_button.dart
+++ lib/b_views/z_components/buttons/back_anb_search_button.dart
@@ -1,5 +1,5 @@
-import 'package:bldrs/b_views/z_components/buttons/dream_box/dream_box.dart';
 import 'package:bldrs/b_views/x_screens/c_main_search/a_main_search_screen.dart';
+import 'package:bldrs/b_views/z_components/buttons/dream_box/dream_box.dart';
 import 'package:bldrs/d_providers/ui_provider.dart';
 import 'package:bldrs/f_helpers/drafters/iconizers.dart';
 import 'package:bldrs/f_helpers/drafters/keyboarders.dart';
@@ -88,8 +88,9 @@ class BackAndSearchButton extends StatelessWidget {
 
             else if (backAndSearchAction == BackAndSearchAction.goToSearchScreen) {
               await Nav.goToNewScreen(
-                  context: context,
-                  screen: const MainSearchScreen(),
+                context: context,
+                screen: const MainSearchScreen(),
+                transitionType: Nav.superHorizontalTransition(context, inverse: true),
               );
             }
 
