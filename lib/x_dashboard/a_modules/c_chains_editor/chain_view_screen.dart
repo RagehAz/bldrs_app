394de6918b796b7ffd2ec0120996e2bf75186dae
RagehAz <rageh.az@gmail.com>
2022-08-20 06:13:52 +0200

    - yalla more push

--- lib/x_dashboard/a_modules/c_chains_editor/chain_view_screen.dart
+++ lib/x_dashboard/a_modules/c_chains_editor/chain_view_screen.dart
@@ -1,9 +1,9 @@
 import 'package:bldrs/a_models/chain/chain.dart';
 import 'package:bldrs/b_views/x_screens/j_chains/components/expander_structure/b_chain_splitter.dart';
 import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
+import 'package:bldrs/b_views/z_components/sizing/expander.dart';
 import 'package:bldrs/b_views/z_components/sizing/stratosphere.dart';
 import 'package:bldrs/f_helpers/drafters/scalers.dart';
-import 'package:bldrs/f_helpers/theme/colorz.dart';
 import 'package:flutter/material.dart';
 
 class ChainViewScreen extends StatelessWidget {
@@ -25,13 +25,19 @@ class ChainViewScreen extends StatelessWidget {
       layoutWidget: Container(
         width: Scale.superScreenWidth(context),
         height: Scale.superScreenHeightWithoutSafeArea(context),
-        color: Colorz.bloodTest,
         alignment: Alignment.topCenter,
         child: SingleChildScrollView(
           padding: Stratosphere.stratosphereSandwich,
           child: ChainSplitter(
             initiallyExpanded: false,
             chainOrChainsOrSonOrSons: chain,
+            onSelectPhid: (String path, String phid){
+
+              blog('phid : $phid');
+              blog('path : $path');
+
+            },
+
           ),
         ),
       ),
