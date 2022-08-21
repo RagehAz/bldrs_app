ccca69fd5052750876615712c6b0cfaa29ed6424
RagehAz <rageh.az@gmail.com>
2022-08-21 17:19:48 +0200

    - cleanups

--- lib/x_dashboard/a_modules/c_chains_editor/old_editor/chains_manager_screen.dart
+++ lib/x_dashboard/a_modules/c_chains_editor/old_editor/chains_manager_screen.dart
@@ -8,7 +8,7 @@ import 'package:bldrs/b_views/z_components/layouts/night_sky.dart';
 import 'package:bldrs/b_views/z_components/sizing/expander.dart';
 import 'package:bldrs/d_providers/chains_provider.dart';
 import 'package:bldrs/d_providers/phrase_provider.dart';
-import 'package:bldrs/e_db/fire/ops/chain_fire_ops.dart';
+import 'package:bldrs/x_dashboard/a_modules/c_chains_editor/old_editor/old_methods/chain_fire_ops.dart';
 import 'package:bldrs/f_helpers/drafters/scalers.dart';
 import 'package:bldrs/f_helpers/drafters/sliders.dart';
 import 'package:bldrs/f_helpers/router/navigators.dart';
