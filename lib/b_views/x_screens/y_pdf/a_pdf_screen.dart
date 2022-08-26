f711a0fd6f5ed18504818f531eb86337b17b0349
RagehAz <rageh.az@gmail.com>
2022-08-26 05:32:49 +0200

    - test push

--- lib/b_views/x_screens/y_pdf/a_pdf_screen.dart
+++ lib/b_views/x_screens/y_pdf/a_pdf_screen.dart
@@ -70,6 +70,7 @@ class _PDFScreenState extends State<PDFScreen> {
     super.didChangeDependencies();
   }
 // -----------------------------------------------------------------------------
+
   /// TAMAM
   @override
   void dispose() {
@@ -79,6 +80,8 @@ class _PDFScreenState extends State<PDFScreen> {
     _pdfController.dispose();
     super.dispose();
   }
+
+
 // -----------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
