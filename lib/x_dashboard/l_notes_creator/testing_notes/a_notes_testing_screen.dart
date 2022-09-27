675e173f098126dacf2bdb3a14ddfc7df1a410d5
RagehAz <rageh.az@gmail.com>
2022-09-27 13:39:57 +0200

    working on notes again

---
+++ lib/x_dashboard/l_notes_creator/testing_notes/a_notes_testing_screen.dart
@@ -0,0 +1,108 @@
+import 'package:bldrs/b_views/z_components/bubble/bubble_header.dart';
+import 'package:bldrs/f_helpers/drafters/tracers.dart';
+import 'package:bldrs/f_helpers/router/navigators.dart';
+import 'package:bldrs/x_dashboard/l_notes_creator/testing_notes/c_awesome_noti_test_screen.dart';
+import 'package:bldrs/x_dashboard/l_notes_creator/testing_notes/b_fcm_tet_screen.dart';
+import 'package:bldrs/x_dashboard/l_notes_creator/testing_notes/cc_note_route_to_screen.dart';
+import 'package:bldrs/x_dashboard/z_widgets/layout/dashboard_layout.dart';
+import 'package:bldrs/x_dashboard/z_widgets/wide_button.dart';
+import 'package:flutter/material.dart';
+
+class NotesTestingScreen extends StatefulWidget {
+  /// --------------------------------------------------------------------------
+  const NotesTestingScreen({
+    Key key
+  }) : super(key: key);
+  /// --------------------------------------------------------------------------
+  @override
+  _NotesTestingScreenState createState() => _NotesTestingScreenState();
+/// --------------------------------------------------------------------------
+}
+
+class _NotesTestingScreenState extends State<NotesTestingScreen> {
+  // -----------------------------------------------------------------------------
+  /// --- LOADING
+  final ValueNotifier<bool> _loading = ValueNotifier(false);
+  // --------------------
+  Future<void> _triggerLoading({bool setTo}) async {
+    if (mounted == true){
+      if (setTo == null){
+        _loading.value = !_loading.value;
+      }
+      else {
+        _loading.value = setTo;
+      }
+      blogLoading(loading: _loading.value, callerName: 'TestingTemplate',);
+    }
+  }
+  // -----------------------------------------------------------------------------
+  @override
+  void initState() {
+    super.initState();
+  }
+  // --------------------
+  bool _isInit = true;
+  @override
+  void didChangeDependencies() {
+    if (_isInit && mounted) {
+
+      _triggerLoading().then((_) async {
+
+        /// FUCK
+
+        await _triggerLoading();
+      });
+
+      _isInit = false;
+    }
+    super.didChangeDependencies();
+  }
+  // --------------------
+  /// XXXX
+  @override
+  void dispose() {
+    _loading.dispose();
+    super.dispose();
+  }
+  // -----------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    // --------------------
+    return DashBoardLayout(
+      loading: _loading,
+      listWidgets: <Widget>[
+
+        /// FCM TEST
+        WideButton(
+          verse: Verse.plain('FCM tests'),
+          onTap: () => Nav.goToNewScreen(
+            context: context,
+            screen: const FCMTestScreen(),
+          ),
+        ),
+
+        /// AWESOME NOTIFICATIONS TEST
+        WideButton(
+          verse: Verse.plain('Awesome Notifications tests'),
+          onTap: () => Nav.goToNewScreen(
+            context: context,
+            screen: const AwesomeNotiTestScreen(),
+          ),
+        ),
+
+        /// NOTE ROUTE TO SCREEN
+        WideButton(
+          verse: Verse.plain('Note route to screen'),
+          onTap: () => Nav.goToNewScreen(
+            context: context,
+            screen: const NoteRouteToScreen(),
+          ),
+        ),
+
+
+      ],
+    );
+    // --------------------
+  }
+// -----------------------------------------------------------------------------
+}
