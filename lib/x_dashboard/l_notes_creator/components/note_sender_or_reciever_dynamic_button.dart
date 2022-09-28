a6d7ce765d920c151f6cd107848b027a2f024398
RagehAz <rageh.az@gmail.com>
2022-09-28 21:51:57 +0200

    - clean ups - refining and tuning notes

---
+++ lib/x_dashboard/l_notes_creator/components/note_sender_or_reciever_dynamic_button.dart
@@ -0,0 +1,99 @@
+import 'package:bldrs/a_models/secondary_models/note_model.dart';
+import 'package:bldrs/b_views/d_user/z_components/user_tile_button.dart';
+import 'package:bldrs/b_views/z_components/buttons/wide_country_button.dart';
+import 'package:bldrs/b_views/z_components/texting/super_verse/verse_model.dart';
+import 'package:bldrs/c_protocols/bz_protocols/a_bz_protocols.dart';
+import 'package:bldrs/c_protocols/user_protocols/a_user_protocols.dart';
+import 'package:bldrs/f_helpers/theme/iconz.dart';
+import 'package:bldrs/x_dashboard/f_bzz_manager/bz_long_button.dart';
+import 'package:bldrs/x_dashboard/z_widgets/wide_button.dart';
+import 'package:flutter/material.dart';
+
+class NoteSenderOrRecieverDynamicButton extends StatelessWidget {
+  /// --------------------------------------------------------------------------
+  const NoteSenderOrRecieverDynamicButton({
+    @required this.type,
+    @required this.id,
+    @required this.width,
+    Key key
+  }) : super(key: key);
+  /// --------------------------------------------------------------------------
+  final NoteSenderOrRecieverType type;
+  final String id;
+  final double width;
+  /// --------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+
+    /// USER WIDE BUTTON
+    if (type == NoteSenderOrRecieverType.user){
+
+      return FutureBuilder(
+        future: UserProtocols.fetchUser(
+          context: context,
+          userID: id,
+        ),
+          builder: (_, AsyncSnapshot<Object> snap){
+
+          return UserTileButton(
+            boxWidth: width,
+            userModel: snap.data,
+          );
+
+          },
+      );
+
+    }
+
+    /// BZ WIDE BUTTON
+    else if (type == NoteSenderOrRecieverType.bz){
+
+      return FutureBuilder(
+          future: BzProtocols.fetchBz(
+              context: context,
+              bzID: id
+          ),
+          builder: (_, AsyncSnapshot<Object> snap){
+
+            return BzLongButton(
+              boxWidth: width,
+              bzModel: snap.data,
+            );
+
+          }
+      );
+
+    }
+
+    /// COUNTRY WIDE BUTTON
+    else if (type == NoteSenderOrRecieverType.country){
+
+      return WideCountryButton(
+        width: width,
+        countryID: id,
+        onTap: null,
+      );
+
+    }
+
+    /// BLDRS IN MAP MODEL
+    else if (type == NoteSenderOrRecieverType.bldrs){
+
+      // NoteModel.bldrsSenderModel
+
+      return WideButton(
+        width: width,
+        verse: const Verse(text: 'phid_bldrs_net', translate: true),
+        icon: Iconz.bldrsNameSquare,
+        iconSizeFactor: 0.7,
+      );
+    }
+
+    /// NOTHINGNESS
+    else {
+      return const SizedBox();
+    }
+
+  }
+  /// --------------------------------------------------------------------------
+}
