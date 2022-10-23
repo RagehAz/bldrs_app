a00af7642cc27ce2cfa229f692e1507dae4e3834
RagehAz <rageh.az@gmail.com>
2022-10-23 04:56:30 +0200

    - can refresh bz profile and user profile by pulling now

--- lib/b_views/f_bz/a_bz_profile_screen/aaa2_bz_about_page.dart
+++ lib/b_views/f_bz/a_bz_profile_screen/aaa2_bz_about_page.dart
@@ -1,22 +1,25 @@
 import 'package:bldrs/a_models/b_bz/bz_model.dart';
 import 'package:bldrs/b_views/j_flyer/z_components/b_parts/b_footer/info_button/expanded_info_page_parts/info_page_keywords.dart';
+import 'package:bldrs/b_views/j_flyer/z_components/c_groups/grid/flyers_grid.dart';
 import 'package:bldrs/b_views/z_components/app_bar/a_bldrs_app_bar.dart';
 import 'package:bldrs/b_views/z_components/bubbles/a_structure/bubble.dart';
 import 'package:bldrs/b_views/z_components/bubbles/a_structure/bubble_header.dart';
 import 'package:bldrs/b_views/z_components/bubbles/a_structure/bubbles_separator.dart';
+import 'package:bldrs/b_views/z_components/bubbles/b_variants/contacts_bubble/contacts_bubble.dart';
+import 'package:bldrs/b_views/z_components/bubbles/b_variants/paragraph_bubble/paragraph_bubble.dart';
 import 'package:bldrs/b_views/z_components/bz_profile/info_page/bz_banner.dart';
 import 'package:bldrs/b_views/z_components/bz_profile/info_page/bz_stats_bubble.dart';
-import 'package:bldrs/b_views/z_components/bubbles/b_variants/contacts_bubble/contacts_bubble.dart';
-import 'package:bldrs/b_views/j_flyer/z_components/c_groups/grid/flyers_grid.dart';
+import 'package:bldrs/b_views/z_components/layouts/pull_to_refresh.dart';
 import 'package:bldrs/b_views/z_components/sizing/horizon.dart';
 import 'package:bldrs/b_views/z_components/sizing/stratosphere.dart';
-import 'package:bldrs/b_views/z_components/bubbles/b_variants/paragraph_bubble/paragraph_bubble.dart';
 import 'package:bldrs/b_views/z_components/texting/super_verse/super_verse.dart';
+import 'package:bldrs/c_protocols/bz_protocols/a_bz_protocols.dart';
 import 'package:bldrs/d_providers/bzz_provider.dart';
 import 'package:bldrs/f_helpers/drafters/mappers.dart';
 import 'package:bldrs/f_helpers/drafters/scalers.dart';
 import 'package:bldrs/f_helpers/drafters/text_checkers.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class BzAboutPage extends StatelessWidget {
   /// --------------------------------------------------------------------------
@@ -36,6 +39,59 @@ class BzAboutPage extends StatelessWidget {
 
     final BzModel _bzModel = bzModel ?? BzzProvider.proGetActiveBzModel(context: context, listen: true);
 
+    if (bzModel == null){
+      return PullToRefresh(
+        fadeOnBuild: true,
+        onRefresh: () async {
+
+          final BzModel _bz = await BzProtocols.refetch(
+              context: context,
+              bzID:_bzModel.id,
+          );
+
+          final BzzProvider _bzzProvider = Provider.of<BzzProvider>(context, listen: false);
+          _bzzProvider.setActiveBz(
+              bzModel: _bz,
+              notify: true,
+          );
+
+        },
+        child: AboutBzBubbles(
+          bzModel: _bzModel,
+          showContacts: showContacts,
+          showGallery: showGallery,
+        ),
+      );
+    }
+
+    else {
+      return AboutBzBubbles(
+        bzModel: _bzModel,
+        showContacts: showContacts,
+        showGallery: showGallery,
+      );
+    }
+
+  }
+  // -----------------------------------------------------------------------------
+}
+
+class AboutBzBubbles extends StatelessWidget {
+
+  const AboutBzBubbles({
+    @required this.bzModel,
+    @required this.showGallery,
+    @required this.showContacts,
+    Key key
+  }) : super(key: key);
+
+  final BzModel bzModel;
+  final bool showGallery;
+  final bool showContacts;
+
+  @override
+  Widget build(BuildContext context) {
+
     return ListView(
       physics: const BouncingScrollPhysics(),
       padding: Stratosphere.stratosphereSandwich,
@@ -46,13 +102,13 @@ class BzAboutPage extends StatelessWidget {
           boxWidth: BldrsAppBar.width(context),
           boxHeight: BldrsAppBar.width(context),
           // margins: 10,
-          bzModel: _bzModel,
+          bzModel: bzModel,
           corners: Bubble.cornersValue,
           bigName: true,
         ),
 
         /// ABOUT
-        if (TextCheck.isEmpty(_bzModel?.about) == false)
+        if (TextCheck.isEmpty(bzModel?.about) == false)
           ParagraphBubble(
             headerViewModel: const BubbleHeaderVM(
               headlineVerse: Verse(
@@ -61,22 +117,22 @@ class BzAboutPage extends StatelessWidget {
               ),
             ),
             paragraph: Verse(
-              text: _bzModel?.about,
+              text: bzModel?.about,
               translate: false,
             ),
           ),
 
         /// BZ CONTACT
-        if (showContacts == true && Mapper.checkCanLoopList(_bzModel?.contacts) == true)
+        if (showContacts == true && Mapper.checkCanLoopList(bzModel?.contacts) == true)
           ContactsBubble(
-            contacts: _bzModel?.contacts,
-            location: _bzModel?.position,
+            contacts: bzModel?.contacts,
+            location: bzModel?.position,
             canLaunchOnTap: true,
           ),
 
 
         /// SCOPE
-        if (Mapper.checkCanLoopList(_bzModel?.scope) == true)
+        if (Mapper.checkCanLoopList(bzModel?.scope) == true)
           Bubble(
             headerViewModel: const BubbleHeaderVM(
               headlineVerse: Verse(
@@ -88,19 +144,19 @@ class BzAboutPage extends StatelessWidget {
 
               PhidsViewer(
                 pageWidth: Scale.superScreenWidth(context),
-                phids: _bzModel?.scope,
+                phids: bzModel?.scope,
               ),
 
             ],
           ),
 
         /// SEPARATOR
-        if (Mapper.checkCanLoopList(_bzModel?.scope) == true)
+        if (Mapper.checkCanLoopList(bzModel?.scope) == true)
           const DotSeparator(),
 
         /// STATS
         BzStatsBubble(
-          bzModel: _bzModel,
+          bzModel: bzModel,
         ),
 
         /// FLYERS GALLERY TITLE
@@ -122,7 +178,7 @@ class BzAboutPage extends StatelessWidget {
         if (showGallery == true)
           FlyersGrid(
             scrollController: ScrollController(),
-            paginationFlyersIDs: _bzModel?.flyersIDs,
+            paginationFlyersIDs: bzModel?.flyersIDs,
             topPadding: 0,
             heroPath: 'BzAboutPageFlyersGrid',
           ),
@@ -133,5 +189,4 @@ class BzAboutPage extends StatelessWidget {
     );
 
   }
-// -----------------------------------------------------------------------------
 }
