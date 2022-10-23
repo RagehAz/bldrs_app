cf03f57cf4b92b71363b3eab0001bd7262b4f995
RagehAz <rageh.az@gmail.com>
2022-10-23 04:56:30 +0200

    - can refresh bz profile and user profile by pulling now

--- lib/b_views/d_user/a_user_profile_screen/a_user_profile_screen.dart
+++ lib/b_views/d_user/a_user_profile_screen/a_user_profile_screen.dart
@@ -1,16 +1,8 @@
-import 'dart:async';
-
 import 'package:bldrs/a_models/a_user/user_model.dart';
 import 'package:bldrs/b_views/d_user/a_user_profile_screen/aa_user_screen_view_pages.dart';
-import 'package:bldrs/b_views/z_components/dialogs/wait_dialog/wait_dialog.dart';
-import 'package:bldrs/b_views/z_components/sizing/expander.dart';
-import 'package:bldrs/b_views/z_components/texting/super_verse/verse_model.dart';
-import 'package:bldrs/c_protocols/user_protocols/a_user_protocols.dart';
-import 'package:bldrs/d_providers/user_provider.dart';
-import 'package:bldrs/e_back_end/x_ops/fire_ops/auth_fire_ops.dart';
-import 'package:bldrs/e_back_end/x_ops/fire_ops/user_fire_ops.dart';
 import 'package:bldrs/b_views/z_components/layouts/obelisk_layout/structure/nav_model.dart';
 import 'package:bldrs/b_views/z_components/layouts/obelisk_layout/structure/obelisk_layout.dart';
+import 'package:bldrs/d_providers/user_provider.dart';
 import 'package:flutter/material.dart';
 
 class UserProfileScreen extends StatelessWidget {
@@ -31,37 +23,6 @@ class UserProfileScreen extends StatelessWidget {
     return ObeliskLayout(
       initiallyExpanded: true,
       canGoBack: true,
-      appBarRowWidgets: <Widget>[
-
-        const Expander(),
-
-        AppBarButton(
-          verse: Verse.plain('Reload'),
-          onTap: () async {
-
-            unawaited(WaitDialog.showWaitDialog(
-              context: context,
-              loadingVerse: Verse.plain('Re-loading your profile'),
-              canManuallyGoBack: true,
-            ));
-
-            final UserModel _fireUserModel = await UserFireOps.readUser(
-              userID: AuthFireOps.superUserID(),
-            );
-
-            _fireUserModel.blogUserModel();
-
-            await UserProtocols.updateLocally(
-              context: context,
-              newUserModel: _fireUserModel,
-            );
-
-            await WaitDialog.closeWaitDialog(context);
-
-          },
-        ),
-
-      ],
       navModels: <NavModel>[
 
         ...List.generate(UserModel.userProfileTabsList.length, (index){
--- lib/b_views/d_user/a_user_profile_screen/aaa1_user_profile_page.dart
+++ lib/b_views/d_user/a_user_profile_screen/aaa1_user_profile_page.dart
@@ -1,6 +1,11 @@
+import 'package:bldrs/a_models/a_user/user_model.dart';
 import 'package:bldrs/b_views/d_user/z_components/banners/a_user_profile_banners.dart';
+import 'package:bldrs/b_views/z_components/layouts/pull_to_refresh.dart';
 import 'package:bldrs/b_views/z_components/sizing/stratosphere.dart';
+import 'package:bldrs/c_protocols/user_protocols/a_user_protocols.dart';
+import 'package:bldrs/d_providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserProfilePage extends StatelessWidget {
   // -----------------------------------------------------------------------------
@@ -11,11 +16,29 @@ class UserProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return const SingleChildScrollView(
-      physics: BouncingScrollPhysics(),
-      padding: Stratosphere.stratosphereSandwich,
-      child: UserProfileBanners(
-          showContacts: true
+    return PullToRefresh(
+      fadeOnBuild: true,
+      onRefresh: () async {
+
+        final UsersProvider _usersProvider = Provider.of<UsersProvider>(context, listen: false);
+
+        final UserModel _userModel = await UserProtocols.refetchUser(
+            context: context,
+            userID: _usersProvider.myUserModel.id,
+        );
+
+        _usersProvider.setMyUserModelAndAuthModel(
+            userModel: _userModel,
+            notify: true,
+        );
+
+      },
+      child: const SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        padding: Stratosphere.stratosphereSandwich,
+        child: UserProfileBanners(
+            showContacts: true
+        ),
       ),
     );
 
