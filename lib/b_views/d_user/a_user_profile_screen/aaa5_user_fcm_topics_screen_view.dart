a9525ea925b1d6e1c7846298c88afbdf03951760
RagehAz <rageh.az@gmail.com>
2022-10-25 02:02:44 +0200

    - can subscribe and un subscribe to all topics at once now
    - adjusted protocols and stuff,, things are good el7amdollellah

--- lib/b_views/d_user/a_user_profile_screen/aaa5_user_fcm_topics_screen_view.dart
+++ lib/b_views/d_user/a_user_profile_screen/aaa5_user_fcm_topics_screen_view.dart
@@ -1,3 +1,4 @@
+import 'package:bldrs/a_models/a_user/user_model.dart';
 import 'package:bldrs/a_models/e_notes/aa_note_parties_model.dart';
 import 'package:bldrs/a_models/e_notes/aa_topic_model.dart';
 import 'package:bldrs/b_views/z_components/bubbles/a_structure/bubble_header.dart';
@@ -7,7 +8,10 @@ import 'package:bldrs/b_views/z_components/notes/topics_editor/topics_expanding_
 import 'package:bldrs/b_views/z_components/sizing/horizon.dart';
 import 'package:bldrs/b_views/z_components/sizing/stratosphere.dart';
 import 'package:bldrs/c_protocols/user_protocols/a_user_protocols.dart';
+import 'package:bldrs/d_providers/user_provider.dart';
+import 'package:bldrs/f_helpers/drafters/stringers.dart';
 import 'package:bldrs/f_helpers/theme/colorz.dart';
+import 'package:bldrs/f_helpers/theme/iconz.dart';
 import 'package:flutter/material.dart';
 
 class UserFCMTopicsScreenView extends StatelessWidget {
@@ -29,11 +33,67 @@ class UserFCMTopicsScreenView extends StatelessWidget {
     );
 
   }
+  // --------------------
+  ///
+  Future<void> _onSwitchAll({
+    @required BuildContext context,
+    @required bool value,
+  }) async {
+
+    final UserModel _userModel = UsersProvider.proGetMyUserModel(
+        context: context,
+        listen: false,
+    );
+
+    /// SUBSCRIBE TO ALL USER TOPICS
+    if (value == true){
+
+      final List<String> _updatedTopics = Stringer.addStringsToStringsIfDoNotContainThem(
+          listToTake: _userModel.fcmTopics,
+          listToAdd: TopicModel.getAllPossibleUserTopicsIDs(),
+      );
+
+      await UserProtocols.renovateMyUserModel(
+          context: context,
+          newUserModel: _userModel.copyWith(
+            fcmTopics: _updatedTopics,
+          ),
+      );
+
+    }
+
+    /// UNSUBSCRIBE FROM ALL USER TOPICS
+    else {
+
+      final List<String> _updatedTopics = Stringer.removeStringsFromStrings(
+        removeFrom: _userModel.fcmTopics,
+        removeThis: TopicModel.getAllPossibleUserTopicsIDs(),
+      );
+
+      await UserProtocols.renovateMyUserModel(
+        context: context,
+        newUserModel: _userModel.copyWith(
+          fcmTopics: _updatedTopics,
+        ),
+      );
+
+    }
+
+
+  }
   /// --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
 
     final Map<String, dynamic> _map = TopicModel.getTopicsMapByPartyType(PartyType.user);
+
+    final UserModel _userModel = UsersProvider.proGetMyUserModel(context: context, listen: true);
+
+    final List<String> _thisBzUserSubscribedTopics = TopicModel.getUserTopicsFromTopics(
+      topics: _userModel.fcmTopics,
+    );
+
+    final bool _allIsOn = _thisBzUserSubscribedTopics.isNotEmpty;
 
     return ListView(
       key: const ValueKey<String>('FCMTopicsScreenView'),
@@ -43,6 +103,27 @@ class UserFCMTopicsScreenView extends StatelessWidget {
       children: <Widget>[
 
         const Stratosphere(),
+
+        /// ALL SWITCHER
+        TileBubble(
+          bubbleWidth: PageBubble.clearWidth(context),
+          bubbleHeaderVM: BubbleHeaderVM(
+            headlineVerse: Verse.plain('All Notifications'),
+            leadingIcon: Iconz.notification,
+            leadingIconSizeFactor: 0.6,
+            leadingIconBoxColor: _allIsOn == true ? Colorz.green255 : Colorz.white10,
+            hasSwitch: true,
+            switchValue: _allIsOn,
+            onSwitchTap: (bool value) => _onSwitchAll(
+              context: context,
+              value: !_allIsOn,
+            ),
+          ),
+          onTileTap: () => _onSwitchAll(
+            context: context,
+            value: !_allIsOn,
+          ),
+        ),
 
         ...TopicsExpandingTile.buildTopicsMapTiles(
           map: _map,
