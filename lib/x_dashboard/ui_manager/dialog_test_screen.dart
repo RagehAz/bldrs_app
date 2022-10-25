8e26e7fa2337df18625ac484501fe26d9a419e26
RagehAz <rageh.az@gmail.com>
2022-10-25 09:05:38 +0200

    - added ability to invert center dialog buttons

--- lib/x_dashboard/ui_manager/dialog_test_screen.dart
+++ lib/x_dashboard/ui_manager/dialog_test_screen.dart
@@ -1,6 +1,9 @@
+import 'package:bldrs/b_views/z_components/dialogs/center_dialog/center_dialog.dart';
+import 'package:bldrs/b_views/z_components/layouts/custom_layouts/centered_list_layout.dart';
 import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
 import 'package:bldrs/b_views/z_components/layouts/night_sky.dart';
 import 'package:bldrs/b_views/z_components/sizing/expander.dart';
+import 'package:bldrs/x_dashboard/zz_widgets/wide_button.dart';
 import 'package:flutter/material.dart';
 
 class DialogsTestScreen extends StatelessWidget {
@@ -29,7 +32,26 @@ class DialogsTestScreen extends StatelessWidget {
         ),
 
       ],
-      layoutWidget: Container(),
+      layoutWidget: FloatingCenteredList(
+        columnChildren: [
+
+          WideButton(
+            verse: Verse.plain('Center'),
+            onTap: () async {
+
+              await CenterDialog.showCenterDialog(
+                context: context,
+                titleVerse: Verse.plain('What the fuck ?'),
+                bodyVerse: Verse.plain('fuck the fucking mother fucking bitches'),
+                boolDialog: true,
+                invertButtons: true
+              );
+
+            },
+          ),
+
+        ],
+      ),
     );
     // --------------------
   }
