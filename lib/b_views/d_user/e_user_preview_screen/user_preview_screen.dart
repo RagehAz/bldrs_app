1c73bdc6e11c0dae7c59a6c15e365714df48473c
RagehAz <rageh.az@gmail.com>
2022-10-30 11:11:34 +0200

    - fixes

--- lib/b_views/d_user/e_user_preview_screen/user_preview_screen.dart
+++ lib/b_views/d_user/e_user_preview_screen/user_preview_screen.dart
@@ -2,8 +2,12 @@ import 'package:bldrs/a_models/a_user/user_model.dart';
 import 'package:bldrs/b_views/d_user/z_components/banners/a_user_profile_banners.dart';
 import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
 import 'package:bldrs/b_views/z_components/layouts/night_sky.dart';
+import 'package:bldrs/b_views/z_components/layouts/pull_to_refresh.dart';
 import 'package:bldrs/b_views/z_components/sizing/stratosphere.dart';
+import 'package:bldrs/c_protocols/user_protocols/a_user_protocols.dart';
+import 'package:bldrs/f_helpers/router/navigators.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class UserPreviewScreen extends StatelessWidget {
   /// --------------------------------------------------------------------------
@@ -21,12 +25,40 @@ class UserPreviewScreen extends StatelessWidget {
       appBarType: AppBarType.basic,
       pyramidsAreOn: true,
       skyType: SkyType.black,
-      layoutWidget: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        padding: Stratosphere.stratosphereSandwich,
-        child: UserProfileBanners(
-          userModel: userModel,
-          showContacts: userModel.contactsArePublic ?? true,
+      layoutWidget: PullToRefresh(
+        onRefresh: () async {
+
+          final UserModel _userModel = await UserProtocols.refetchUser(
+              context: context,
+              userID: userModel.id,
+          );
+
+          final bool _identical = UserModel.checkUsersAreIdentical(
+              user1: userModel,
+              user2: _userModel,
+          );
+
+          if (_identical == false){
+
+            await Nav.replaceScreen(
+                context: context,
+                transitionType: PageTransitionType.fade,
+                screen: UserPreviewScreen(
+                  userModel: _userModel,
+                ),
+            );
+
+          }
+
+        },
+        fadeOnBuild: true,
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          padding: Stratosphere.stratosphereSandwich,
+          child: UserProfileBanners(
+            userModel: userModel,
+            showContacts: userModel.contactsArePublic ?? true,
+          ),
         ),
       ),
     );
