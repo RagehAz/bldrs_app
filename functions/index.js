ba377ce94f117ff56fd1d97726acedceeacfc4c1
RagehAz <rageh.az@gmail.com>
2022-11-01 04:28:18 +0200

    - successfully added delete storage directory cloud function

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,7 @@
 const admin = require('firebase-admin');
 // const functions = require('firebase-functions');
 const fcmMethods = require('./methods/fcm_methods');
+const storageMethods = require('./methods/storage_methods');
 // const userMethods = require('./methods/user_methods');
 // --------------------------------------------------------------------------
 
@@ -28,6 +29,10 @@ exports.callSendFCMToDevice = fcmMethods.callSendFCMToDevice;
 exports.callSendFCMToDevices = fcmMethods.callSendFCMToDevices;
 exports.callSendFCMsToDevices = fcmMethods.callSendFCMsToDevices;
 exports.callSendFCMToTopic = fcmMethods.callSendFCMToTopic;
+// --------------------
+/// STORAGE FUNCTIONS
+// --------------------
+exports.callDeleteStorageDirectory = storageMethods.callDeleteStorageDirectory;
 // --------------------------------------------------------------------------
 
 // DEPLOYMENT
