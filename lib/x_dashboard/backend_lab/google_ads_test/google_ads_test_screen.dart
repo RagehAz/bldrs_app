47313b056c69e655a2db2885cf68f6c225e3d246
RagehAz <rageh.az@gmail.com>
2022-12-16 03:41:15 +0200

    - irritating progress

--- lib/x_dashboard/backend_lab/google_ads_test/google_ads_test_screen.dart
+++ lib/x_dashboard/backend_lab/google_ads_test/google_ads_test_screen.dart
@@ -1,6 +1,11 @@
+import 'package:bldrs/a_models/x_utilities/dimensions_model.dart';
+import 'package:bldrs/b_views/j_flyer/z_components/x_helpers/x_flyer_dim.dart';
 import 'package:bldrs/b_views/z_components/app_bar/a_bldrs_app_bar.dart';
+import 'package:bldrs/c_protocols/app_state_protocols/provider/ui_provider.dart';
 import 'package:bldrs/e_back_end/j_ads/google_ads.dart';
+import 'package:bldrs/f_helpers/drafters/scalers.dart';
 import 'package:bldrs/f_helpers/drafters/tracers.dart';
+import 'package:bldrs/f_helpers/theme/colorz.dart';
 import 'package:bldrs/f_helpers/theme/ratioz.dart';
 import 'package:bldrs/x_dashboard/zz_widgets/layout/dashboard_layout.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +25,7 @@ class GoogleAdsTestScreen extends StatefulWidget {
 class _GoogleAdsTestScreenState extends State<GoogleAdsTestScreen> {
   // -----------------------------------------------------------------------------
   BannerAd _bannerAd;
+  AdSize _adSize;
   // -----------------------------------------------------------------------------
   /// --- LOADING
   final ValueNotifier<bool> _loading = ValueNotifier(false);
@@ -34,6 +40,17 @@ class _GoogleAdsTestScreenState extends State<GoogleAdsTestScreen> {
   // -----------------------------------------------------------------------------
   @override
   void initState() {
+
+    final Dimensions _screenDims = UiProvider.proGetScreenDimensions(
+      context: context,
+      listen: false,
+    );
+
+    _adSize = AdSize(
+      width: _screenDims.width.toInt(),
+      height: _screenDims.height.toInt(),
+    );
+
     super.initState();
   }
   // --------------------
@@ -44,7 +61,11 @@ class _GoogleAdsTestScreenState extends State<GoogleAdsTestScreen> {
 
       _triggerLoading(setTo: true).then((_) async {
 
-        final BannerAd _ad = GoogleAds.createBannerAd();
+
+
+        final BannerAd _ad = GoogleAds.createBannerAd(
+          adSize: _adSize,
+        );
         await _ad.load();
 
         setState(() {
@@ -68,18 +89,32 @@ class _GoogleAdsTestScreenState extends State<GoogleAdsTestScreen> {
   @override
   Widget build(BuildContext context) {
     // --------------------
+    final double _neededWidth = BldrsAppBar.width(context);
+    final double _scaleFactor = _neededWidth / _adSize.width;
+    // --------------------
     return DashBoardLayout(
       loading: _loading,
       listWidgets: <Widget>[
 
-        if (_bannerAd != null)
-        Container(
-          width: _bannerAd.size.width.toDouble(),
-          height: _bannerAd.size.height.toDouble(),
-          decoration: const BoxDecoration(
-            borderRadius: BldrsAppBar.corners,
+        Center(
+          child: Container(
+            width: _adSize.width.toDouble() * _scaleFactor,
+            height: _adSize.height.toDouble() * _scaleFactor,
+            decoration: const BoxDecoration(
+              borderRadius: BldrsAppBar.corners,
+              color: Colorz.bloodTest,
+            ),
+            child: ClipRRect(
+              borderRadius: BldrsAppBar.corners,
+              child:
+              _bannerAd == null ?
+              const SizedBox()
+                  :
+              AdWidget(
+                ad: _bannerAd,
+              ),
+            ),
           ),
-          child: AdWidget(ad: _bannerAd),
         ),
 
       ],
