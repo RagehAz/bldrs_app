a9a4f4ae947827fe6200cee59c9c4b8f0274beaf
RagehAz <rageh.az@gmail.com>
2022-12-16 02:53:48 +0200

    - save keda l7ad keda dejavu fash5,,, i lost the code ,,, messed up packages,,, rolled back then wrote the code again,,, HYPNOTIC STATUS OF MENTAL DIGITALIZATION IN A BUGGY UNIVERSE

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ import 'package:bldrs/e_back_end/e_fcm/fcm_starter.dart';
 import 'package:bldrs/e_back_end/e_fcm/z_noot_controller.dart';
 import 'package:bldrs/e_back_end/f_cloud/dynamic_links.dart';
 import 'package:bldrs/e_back_end/i_app_check/app_check.dart';
+import 'package:bldrs/e_back_end/j_ads/google_ads.dart';
 import 'package:bldrs/f_helpers/drafters/sounder.dart';
 import 'package:bldrs/f_helpers/drafters/text_mod.dart';
 import 'package:bldrs/f_helpers/drafters/tracers.dart';
@@ -86,11 +87,18 @@ Future<void> main() async {
     // options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  await Future.wait(<Future>[
 
-  // --------------------
-  await FCMStarter.preInitializeNootsInMainFunction();
-  // --------------------
-  await AppCheck.preInitialize();
+    /// FCM
+    FCMStarter.preInitializeNootsInMainFunction(),
+
+    /// APP CHECK
+    AppCheck.preInitialize(),
+
+    /// GOOGLE ADS
+    GoogleAds.initialize(),
+
+  ]);
   /// --------------------
   runApp(const BldrsAppStarter());
   /// -----------------------------------------------------------------------------
