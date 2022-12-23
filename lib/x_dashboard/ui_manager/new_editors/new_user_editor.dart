79895d8f06503de1471979731606312b55f68eb1
RagehAz <rageh.az@gmail.com>
2022-12-23 13:28:06 +0200

    - Himalayas

--- lib/x_dashboard/ui_manager/new_editors/new_user_editor.dart
+++ lib/x_dashboard/ui_manager/new_editors/new_user_editor.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_positional_boolean_parameters
+
 import 'dart:async';
 
 import 'package:bldrs/a_models/a_user/draft/draft_user.dart';
@@ -7,12 +9,14 @@ import 'package:bldrs/a_models/d_zone/a_zoning/zone_stages.dart';
 import 'package:bldrs/a_models/x_secondary/contact_model.dart';
 import 'package:bldrs/b_views/d_user/b_user_editor_screen/x_user_editor_controllers.dart';
 import 'package:bldrs/b_views/g_zoning/x_zone_selection_ops.dart';
+import 'package:bldrs/b_views/z_components/app_bar/progress_bar_swiper_model.dart';
 import 'package:bldrs/b_views/z_components/bubbles/a_structure/bubble_header.dart';
 import 'package:bldrs/b_views/z_components/bubbles/b_variants/contacts_bubble/contact_field_editor_bubble.dart';
 import 'package:bldrs/b_views/z_components/bubbles/b_variants/gender_bubble/gender_editor_bubble.dart';
 import 'package:bldrs/b_views/z_components/bubbles/b_variants/pic_bubble/add_gallery_pic_bubble.dart';
 import 'package:bldrs/b_views/z_components/bubbles/b_variants/text_field_bubble/text_field_bubble.dart';
 import 'package:bldrs/b_views/z_components/bubbles/b_variants/zone_bubble/zone_selection_bubble.dart';
+import 'package:bldrs/b_views/z_components/buttons/editor_confirm_button.dart';
 import 'package:bldrs/b_views/z_components/layouts/custom_layouts/floating_layout.dart';
 import 'package:bldrs/b_views/z_components/layouts/custom_layouts/pages_layout.dart';
 import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
@@ -20,7 +24,6 @@ import 'package:bldrs/c_protocols/user_protocols/ldb/user_ldb_ops.dart';
 import 'package:bldrs/f_helpers/drafters/formers.dart';
 import 'package:bldrs/f_helpers/drafters/pic_maker.dart';
 import 'package:bldrs/f_helpers/drafters/tracers.dart';
-import 'package:bldrs/f_helpers/theme/colorz.dart';
 import 'package:flutter/material.dart';
 
 class NewUserEditor extends StatefulWidget {
@@ -48,6 +51,9 @@ class NewUserEditor extends StatefulWidget {
 }
 
 class _NewUserEditorState extends State<NewUserEditor> {
+
+  final ValueNotifier<ProgressBarModel> _progressBarModel = ValueNotifier(null);
+  ConfirmButtonModel _confirmButtonModel;
   // -----------------------------------------------------------------------------
   bool _canValidate = false;
   void _switchOnValidation(){
@@ -76,6 +82,14 @@ class _NewUserEditorState extends State<NewUserEditor> {
   @override
   void initState() {
     super.initState();
+
+    setNotifier(
+        notifier: _progressBarModel,
+        mounted: mounted,
+        value: ProgressBarModel.initialModel(
+            numberOfStrips: 4,
+        ),
+    );
 
   }
   // --------------------
@@ -143,29 +157,12 @@ class _NewUserEditorState extends State<NewUserEditor> {
     /// ON DRAFT
     _draftUser.addListener(() async {
       // _switchOnValidation();
-      await UserLDBOps.saveEditorSession(draft: _draftUser.value,);
-    });
 
-    // _draftUser.value.nameController.addListener(() async {
-    //   blog('shoulde save new name');
-    //   await UserLDBOps.saveEditorSession(draft: _draftUser.value);
-    // });
-    //
-    // _draftUser.value.titleController.addListener(() async {
-    //   await UserLDBOps.saveEditorSession(draft: _draftUser.value);
-    // });
-    //
-    // _draftUser.value.companyController.addListener(() async {
-    //   await UserLDBOps.saveEditorSession(draft: _draftUser.value);
-    // });
-    //
-    // _draftUser.value.phoneController.addListener(() async {
-    //   await UserLDBOps.saveEditorSession(draft: _draftUser.value);
-    // });
-    //
-    // _draftUser.value.emailController.addListener(() async {
-    //   await UserLDBOps.saveEditorSession(draft: _draftUser.value);
-    // });
+      _stripsListener();
+
+      await UserLDBOps.saveEditorSession(draft: _draftUser.value,);
+
+    });
 
   }
   // --------------------
@@ -186,12 +183,144 @@ class _NewUserEditorState extends State<NewUserEditor> {
 
   }
   // -----------------------------------------------------------------------------
+  ///
+  void _stripsListener(){
+
+    // -----------------
+    /// STRIP 1 : PICTURE - GENDER
+
+    final bool _picIsValid = Formers.picValidator(
+        context: context,
+        pic: _draftUser.value?.picModel,
+        canValidate: _canValidate,
+    ) == null;
+    final bool _genderIsValid = Formers.genderValidator(
+        context: context,
+        gender: _draftUser.value?.gender,
+        canValidate: _canValidate
+    ) == null;
+
+    if (_picIsValid == false || _genderIsValid == false){
+      setStripIsValid(0, false);
+    }
+    else {
+      setStripIsValid(0, true);
+    }
+
+    // -----------------
+    /// STRIP 2 : NAME - TITLE - COMPANY
+
+    final bool _nameIsValid = Formers.personNameValidator(
+        context: context,
+        name: _draftUser.value?.name,
+        canValidate: _canValidate,
+    ) == null;
+    final bool _titleIsValid = Formers.jobTitleValidator(
+        context: context,
+        jobTitle: _draftUser.value?.title,
+        canValidate: _canValidate,
+    ) == null;
+    final bool _companyIsValid = Formers.companyNameValidator(
+        context: context,
+        companyName: _draftUser.value?.company,
+        canValidate: _canValidate,
+    ) == null;
+
+    if (_nameIsValid == false || _titleIsValid == false || _companyIsValid == false){
+      setStripIsValid(1, false);
+    }
+    else {
+      setStripIsValid(1, true);
+    }
+
+    // -----------------
+    /// STRIP 3 : ZONE
+
+    final bool _zoneIsValid = Formers.zoneValidator(
+      context: context,
+      zoneModel: _draftUser.value?.zone,
+      selectCountryAndCityOnly: true,
+      selectCountryIDOnly: false,
+      canValidate: _canValidate,
+    ) == null;
+
+    if (_zoneIsValid == false){
+      setStripIsValid(2, false);
+    }
+    else {
+      setStripIsValid(2, true);
+    }
+
+    // -----------------
+    /// STRIP 4 : PHONE - EMAIL
+
+    final bool _phoneIsValid = Formers.contactsPhoneValidator(
+      contacts: _draftUser.value?.contacts,
+      zoneModel: _draftUser.value?.zone,
+      canValidate: _canValidate,
+      context: context,
+      isRequired: false,
+      // focusNode: draft?.phoneNode,
+    ) == null;
+    final bool _emailIsValid = Formers.contactsEmailValidator(
+      context: context,
+      contacts: _draftUser.value?.contacts,
+      canValidate: _canValidate,
+      // focusNode: draft?.emailNode,
+    ) == null;
+
+    if (_phoneIsValid == false || _emailIsValid == false){
+      setStripIsValid(3, false);
+    }
+    else {
+      setStripIsValid(3, true);
+    }
+
+    // ------->
+
+    _controlConfirmButton();
+
+  }
+  // --------------------
+  ///
+  void setStripIsValid(int index, bool isValid){
+    ProgressBarModel.setStripColor(
+      notifier: _progressBarModel,
+      mounted: mounted,
+      index: index,
+      color: isValid == true ? Colors.green : Colors.red,
+    );
+  }
+  // --------------------
+  ///
+  void _controlConfirmButton(){
+
+    if (_progressBarModel.value.stripsColors.contains(Colors.red) == true){
+      setState(() {
+        _confirmButtonModel = null;
+      });
+    }
+    else {
+      setState(() {
+        _confirmButtonModel = ConfirmButtonModel(
+            firstLine: Verse.plain('Confirm'),
+            onTap: () async {
+
+            },
+        );
+      });
+    }
+
+
+  }
+  // -----------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
     // --------------------
     return PagesLayout(
       title: const Verse(text: 'phid_updateProfile', translate: true),
-      progressColors: const [Colorz.white255, Colorz.red255, Colorz.white255, Colorz.white255,],
+      progressBarModel: _progressBarModel,
+      confirmButtonModel: _confirmButtonModel,
       pageBubbles: <Widget>[
 
         /// PIC - GENDER
@@ -223,6 +352,17 @@ class _NewUserEditorState extends State<NewUserEditor> {
                       pic: draft?.picModel,
                       canValidate: _canValidate,
                     ),
+                    onPicLongTap: (){
+
+                      setNotifier(
+                          notifier: _draftUser,
+                          mounted: mounted,
+                          value: draft.nullifyField(
+                            picModel: true,
+                          ),
+                      );
+
+                    },
                   ),
 
                   /// GENDER
@@ -482,5 +622,5 @@ class _NewUserEditorState extends State<NewUserEditor> {
     );
     // --------------------
   }
-// -----------------------------------------------------------------------------
+  // -----------------------------------------------------------------------------
 }
