1f0e1539513c65664d2267e5972a2124f27f010a
RagehAz <rageh.az@gmail.com>
2022-12-26 22:40:41 +0200

    - trying to package bldrs colors

--- lib/c_protocols/flyer_protocols/protocols/renovate_flyers.dart
+++ lib/c_protocols/flyer_protocols/protocols/renovate_flyers.dart
@@ -231,3 +231,4 @@ class RenovateFlyerProtocols {
 }
 
 // git remote set-head origin https://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@github.com/Bldrs-net/bldrs.net.git
+// git remote add origin https://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@github.com/Bldrs-net/bldrs.net.git
