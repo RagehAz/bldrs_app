39e34d1ac9ba6ea02d7f5da94b4ac8da4e4cde08
RagehAz <rageh.az@gmail.com>
2022-12-27 00:55:52 +0200

    - ya sater

--- lib/b_views/z_components/cropper/cropping_screen.dart
+++ lib/b_views/z_components/cropper/cropping_screen.dart
@@ -10,7 +10,8 @@ import 'package:bldrs/f_helpers/drafters/scalers.dart';
 import 'package:bldrs/f_helpers/drafters/tracers.dart';
 import 'package:bldrs/f_helpers/router/navigators.dart';
 import 'package:bldrs/f_helpers/theme/ratioz.dart';
-// import 'package:crop_your_image/crop_your_image.dart';
+import 'package:crop_your_image/crop_your_image.dart';
+
 import 'package:flutter/material.dart';
 
 class CroppingScreen extends StatefulWidget {
