7167d5f03768cf1f5a1655af151da6ac7846345c
RagehAz <rageh.az@gmail.com>
2022-12-27 21:29:52 +0200

    - commit with new token

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,6 @@ git remote set-head origin https://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@gith
 git remote add origin https://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@github.com/Bldrs-net/bldrs.net.git
 git remote add origin https://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@github.com/Bldrs-net/bldrscolors.git
 git remote add origin https://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@github.com/Bldrs-net/bldrs.net.git
-
  */
 // ---------------------------------------------------------------------------
 /// TO CREATE A NEW PACKAGE
