7441bd3398e1866f8e1fc549974654234dfd1f8f
RagehAz <rageh.az@gmail.com>
2022-12-27 21:17:29 +0200

    - packaged super_image

--- lib/b_views/j_flyer/z_components/b_parts/b_footer/info_button/collapsed_info_button_parts/info_graphic.dart
+++ lib/b_views/j_flyer/z_components/b_parts/b_footer/info_button/collapsed_info_button_parts/info_graphic.dart
@@ -6,6 +6,7 @@ import 'package:bldrs/b_views/z_components/texting/super_verse/super_verse.dart'
 import 'package:bldrs_theme/bldrs_theme.dart';
 import 'package:flutter/material.dart';
 
+
 class InfoGraphic extends StatelessWidget {
   /// --------------------------------------------------------------------------
   const InfoGraphic({
