acb58aac3d81cab9dd65f76e8eed845710cd946b
RagehAz <rageh.az@gmail.com>
2023-01-02 08:03:53 +0200

    - we can run from intellij now

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Rageh on 02/01/2023.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
