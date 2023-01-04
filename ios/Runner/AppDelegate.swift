a0119343a06d1d53fa08248e34c1e1b03367d4f9
RagehAz <rageh.az@gmail.com>
2023-01-04 13:39:06 +0200

    - this is building on simulator - switched off google maps / google auth plugins

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,5 @@
 import UIKit
 import Flutter
-import Firebase
-import FirebaseMessaging
-// import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,9 +7,7 @@ import FirebaseMessaging
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-        FirebaseApp.configure()
-        // GMServices.provideAPIKey("AIzaSyDQGuhqhKu1mSdNxAbS_BCP8NfCB1ENmaI")
-        GeneratedPluginRegistrant.register(with: self)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-   }
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
