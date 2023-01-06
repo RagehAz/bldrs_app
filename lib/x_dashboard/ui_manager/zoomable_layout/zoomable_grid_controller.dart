0a651bc01718edb760bce4eb03faae5c3ab8aac9
RagehAz <rageh.az@gmail.com>
2023-01-06 04:14:02 +0200

    - zoomable layout is impressive

---
+++ lib/x_dashboard/ui_manager/zoomable_layout/zoomable_grid_controller.dart
@@ -0,0 +1,12 @@
+
+
+class ZoomableGridController {
+  // --------------------------------------------------------------------------
+  const ZoomableGridController();
+  // --------------------------------------------------------------------------
+  static ZoomableGridController initialize(){
+    return ZoomableGridController();
+  }
+  // --------------------------------------------------------------------------
+  void f(){}
+}
