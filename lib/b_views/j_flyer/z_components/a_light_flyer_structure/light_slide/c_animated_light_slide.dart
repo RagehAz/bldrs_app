ae1d0066c26dc418d53a7fdc2fab34a081732be2
RagehAz <rageh.az@gmail.com>
2023-01-09 13:59:14 +0200

    - working on new light slide - should be easy, but i'm lacking energy - good night

---
+++ lib/b_views/j_flyer/z_components/a_light_flyer_structure/light_slide/c_animated_light_slide.dart
@@ -0,0 +1,96 @@
+import 'package:bldrs/a_models/f_flyer/sub/slide_model.dart';
+import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
+import 'package:bldrs/f_helpers/drafters/tracers.dart';
+import 'package:bldrs/x_dashboard/zz_widgets/layout/dashboard_layout.dart';
+import 'package:bldrs/x_dashboard/zz_widgets/wide_button.dart';
+import 'package:flutter/material.dart';
+
+class AnimatedLightSlide extends StatefulWidget {
+  /// --------------------------------------------------------------------------
+  const AnimatedLightSlide({
+    @required this.flyerBoxWidth,
+    @required this.slideModel,
+    Key key
+  }) : super(key: key);
+  /// --------------------------------------------------------------------------
+  final double flyerBoxWidth;
+  final SlideModel slideModel;
+  /// --------------------------------------------------------------------------
+  @override
+  _AnimatedLightSlideState createState() => _AnimatedLightSlideState();
+  /// --------------------------------------------------------------------------
+}
+
+class _AnimatedLightSlideState extends State<AnimatedLightSlide> {
+  // -----------------------------------------------------------------------------
+  /// --- LOADING
+  final ValueNotifier<bool> _loading = ValueNotifier(false);
+  // --------------------
+  Future<void> _triggerLoading({@required bool setTo}) async {
+    setNotifier(
+      notifier: _loading,
+      mounted: mounted,
+      value: setTo,
+    );
+  }
+  // -----------------------------------------------------------------------------
+  @override
+  void initState() {
+    super.initState();
+  }
+  // --------------------
+  bool _isInit = true;
+  @override
+  void didChangeDependencies() {
+    if (_isInit && mounted) {
+
+      _triggerLoading(setTo: true).then((_) async {
+
+        /// FUCK
+
+        await _triggerLoading(setTo: false);
+      });
+
+      _isInit = false;
+    }
+    super.didChangeDependencies();
+  }
+  // --------------------
+  /*
+  @override
+  void didUpdateWidget(TheStatefulScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.thing != widget.thing) {
+      unawaited(_doStuff());
+    }
+  }
+   */
+  // --------------------
+  @override
+  void dispose() {
+    _loading.dispose();
+    super.dispose();
+  }
+  // -----------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    // --------------------
+    return DashBoardLayout(
+      loading: _loading,
+      listWidgets: <Widget>[
+
+        WideButton(
+          verse: Verse.plain('~~~~~~~~~test~~~~~~~~~~'),
+          onTap: () async {
+
+            blog('~~~~~~~~~test~~~~~~~~~~ onTap');
+
+          },
+        ),
+
+      ],
+    );
+    // --------------------
+  }
+// -----------------------------------------------------------------------------
+}
