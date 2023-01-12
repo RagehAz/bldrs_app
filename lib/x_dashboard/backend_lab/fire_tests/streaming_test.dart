9f7facfd8140b621cbeb438bf08fc81cd319fb16
RagehAz <rageh.az@gmail.com>
2023-01-13 00:50:51 +0200

    - migrated clock_rebuilder widget into its own separate package

--- lib/x_dashboard/backend_lab/fire_tests/streaming_test.dart
+++ lib/x_dashboard/backend_lab/fire_tests/streaming_test.dart
@@ -2,22 +2,21 @@ import 'dart:async';
 
 import 'package:bldrs/b_views/z_components/buttons/dream_box/dream_box.dart';
 import 'package:bldrs/b_views/z_components/dialogs/dialogz/dialogs.dart';
-import 'package:bldrs/b_views/z_components/clocking/clock_rebuilder.dart';
-import 'package:bldrs/e_back_end/b_fire/widgets/fire_coll_streamer.dart';
-import 'package:bldrs/e_back_end/b_fire/widgets/fire_doc_streamer.dart';
 import 'package:bldrs/e_back_end/b_fire/fire_models/fire_query_model.dart';
 import 'package:bldrs/e_back_end/b_fire/foundation/fire.dart';
-import 'package:colorizer/colorizer.dart';
-import 'package:mapper/mapper.dart';
-import 'package:numeric/numeric.dart';
-import 'package:scale/scale.dart';
+import 'package:bldrs/e_back_end/b_fire/widgets/fire_coll_streamer.dart';
+import 'package:bldrs/e_back_end/b_fire/widgets/fire_doc_streamer.dart';
 import 'package:bldrs/f_helpers/drafters/timers.dart';
 import 'package:bldrs/f_helpers/drafters/tracers.dart';
-
 import 'package:bldrs/x_dashboard/zz_widgets/layout/dashboard_layout.dart';
 import 'package:bldrs/x_dashboard/zz_widgets/wide_button.dart';
 import 'package:bldrs_theme/bldrs_theme.dart';
+import 'package:clock_rebuilder/clock_rebuilder.dart';
+import 'package:colorizer/colorizer.dart';
 import 'package:flutter/material.dart';
+import 'package:mapper/mapper.dart';
+import 'package:numeric/numeric.dart';
+import 'package:scale/scale.dart';
 
 class StreamingTest extends StatefulWidget {
   /// --------------------------------------------------------------------------
@@ -232,7 +231,10 @@ class ColorButton extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return ClockRebuilder(
-      startTime: Timers.decipherTime(time: map['time'], fromJSON: mapIsFromJSON),
+      startTime: Timers.decipherTime(
+          time: map['time'],
+          fromJSON: mapIsFromJSON,
+      ),
       builder: (int seconds, Widget child){
 
         final DateTime _time = Timers.createClockFromSeconds(seconds);
