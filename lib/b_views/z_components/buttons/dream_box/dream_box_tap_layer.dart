f3ef707cd63c2033f3d788669178600780893bfb
RagehAz <rageh.az@gmail.com>
2023-01-12 22:09:36 +0200

    - fixed pyramid expansion outside tap closes pyramids

--- lib/b_views/z_components/buttons/dream_box/dream_box_tap_layer.dart
+++ lib/b_views/z_components/buttons/dream_box/dream_box_tap_layer.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
-
 import 'package:bldrs/f_helpers/drafters/sounder.dart';
 import 'package:bldrs_theme/bldrs_theme.dart';
-
 import 'package:flutter/material.dart';
 
 class DreamBoxTapLayer extends StatelessWidget {
