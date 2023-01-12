9d33726190a5d1cf7da27827eaaeb6e664c9cd06
RagehAz <rageh.az@gmail.com>
2023-01-12 02:41:07 +0200

    - fixed black screen after launch screen issue

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ import 'package:bldrs_theme/bldrs_theme.dart';
 import 'package:bldrs/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 // ---------------------------------------------------------------------------
@@ -158,12 +159,13 @@ Future<void> main() async {
   */
   // --------------------
   /// insures awaiting async methods below to finish then continue
-  WidgetsFlutterBinding.ensureInitialized();
+  final WidgetsBinding _binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: _binding);
   // --------------------
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  // --------------------
   await Future.wait(<Future>[
 
     /// FCM
