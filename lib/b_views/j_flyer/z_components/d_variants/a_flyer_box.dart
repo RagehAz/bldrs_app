0fd63f606fb42f4991f6bbd422fae491b0a80936
RagehAz <rageh.az@gmail.com>
2023-01-15 18:44:37 +0200

    - yalla lets fix ios notification and go app store yalla

--- lib/b_views/j_flyer/z_components/d_variants/a_flyer_box.dart
+++ lib/b_views/j_flyer/z_components/d_variants/a_flyer_box.dart
@@ -1,6 +1,5 @@
 import 'package:bldrs/b_views/j_flyer/z_components/x_helpers/x_flyer_dim.dart';
 import 'package:bldrs/f_helpers/drafters/shadowers.dart';
-import 'package:bldrs/f_helpers/drafters/tracers.dart';
 import 'package:bldrs_theme/bldrs_theme.dart';
 
 import 'package:flutter/material.dart';
