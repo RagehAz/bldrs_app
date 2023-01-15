b904ee6d477f06f50658b852b1e5b1c4940178a7
RagehAz <rageh.az@gmail.com>
2023-01-15 18:15:22 +0200

    - seems like cropper feature decided to work properly now good -lets continue

--- lib/b_views/j_flyer/z_components/c_groups/slide_editor/image_filter_animated_name.dart
+++ lib/b_views/j_flyer/z_components/c_groups/slide_editor/image_filter_animated_name.dart
@@ -3,7 +3,6 @@ import 'package:widget_fader/widget_fader.dart';
 import 'package:bldrs/b_views/z_components/images/super_filter/color_filter_generator.dart';
 import 'package:bldrs/b_views/z_components/texting/super_verse/super_verse.dart';
 import 'package:bldrs_theme/bldrs_theme.dart';
-
 import 'package:flutter/material.dart';
 
 class ImageFilterAnimatedName extends StatelessWidget {
