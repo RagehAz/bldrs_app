d5864e717151cc387e0462a3ae243defb474749d
RagehAz <rageh.az@gmail.com>
2023-01-18 20:11:06 +0200

    - pushed invite buttons, mesh wa2to

--- lib/b_views/h_app_settings/a_app_settings_screen/a_app_settings_screen.dart
+++ lib/b_views/h_app_settings/a_app_settings_screen/a_app_settings_screen.dart
@@ -1,12 +1,13 @@
 import 'package:bldrs/a_models/a_user/auth_model.dart';
 import 'package:bldrs/b_views/z_components/bubbles/a_structure/bubbles_separator.dart';
+import 'package:bldrs/b_views/z_components/buttons/dream_box/dream_box.dart';
 import 'package:bldrs/b_views/z_components/buttons/settings_wide_button.dart';
 import 'package:bldrs/b_views/z_components/layouts/custom_layouts/floating_layout.dart';
 import 'package:bldrs/b_views/h_app_settings/a_app_settings_screen/x_app_settings_controllers.dart';
-import 'package:bldrs/b_views/z_components/texting/super_verse/verse_model.dart';
-import 'package:bldrs/f_helpers/drafters/iconizers.dart';
+import 'package:bldrs/b_views/z_components/texting/super_verse/super_verse.dart';
 import 'package:bldrs_theme/bldrs_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:super_image/super_image.dart';
 
 class AppSettingsScreen extends StatelessWidget {
   /// --------------------------------------------------------------------------
@@ -59,22 +60,60 @@ class AppSettingsScreen extends StatelessWidget {
             onTap: () => onTermsAndRegulationsTap(context),
           ),
 
-          /// INVITE FRIENDS
-          SettingsWideButton(
-            verse: const Verse(text: 'phid_inviteFriends', translate: true,),
-            icon: Iconizer.shareAppIcon(),
-            onTap: () => onInviteFriendsTap(context),
-          ),
+          // /// INVITE FRIENDS
+          // SettingsWideButton(
+          //   verse: const Verse(text: 'phid_inviteFriends', translate: true,),
+          //   icon: Iconizer.shareAppIcon(),
+          //   onTap: () => onInviteFriendsTap(context),
+          // ),
 
           const DotSeparator(),
 
-          /// ADD NEW BZ
-          SettingsWideButton(
-            verse: const Verse(text: 'phid_createBzAccount', translate: true),
-            icon: Iconz.bz,
-            isOn : _userIsOnline,
-            onTap: () => onCreateNewBzTap(context),
-          ),
+            /// CREATE NEW BZ ACCOUNT BUTTON
+            SizedBox(
+              width: SettingsWideButton.width,
+              height: 130,
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+
+                  const Positioned(
+                    left: -60,
+                    top: -(130 / 2) - 30,
+                    child: SuperImage(
+                      width: 300,
+                      height: 300,
+                      pic: Iconz.bz,
+                      iconColor: Colorz.yellow20,
+                    ),
+                  ),
+
+                  const SuperVerse(
+                    verse: Verse(
+                        text: 'phid_createBzAccount',
+                        translate: true,
+                      casing: Casing.upperCase,
+                    ),
+                    italic: true,
+                    size: 3,
+                    maxLines: 2,
+                    margin: 30,
+                    weight: VerseWeight.black,
+                    shadow: true,
+                  ),
+
+                  DreamBox(
+                    width: SettingsWideButton.width,
+                    height: 130,
+                    color: Colorz.yellow50,
+                    isDisabled: !_userIsOnline,
+                    onTap: () => onCreateNewBzTap(context),
+                  ),
+
+                ],
+
+              ),
+            ),
 
             const DotSeparator(),
 
