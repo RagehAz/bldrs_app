409a73b6a4e2b457ef83c9ac252ed2c4399d5095
RagehAz <rageh.az@gmail.com>
2023-02-06 17:35:34 +0200

    - 3.0.12: refactored DreamBox on top of SuperBox

--- lib/b_views/z_components/texting/super_text_field/migration guide.dart
+++ lib/b_views/z_components/texting/super_text_field/migration guide.dart
@@ -10,3 +10,24 @@
 
 
  */
+
+// --------------------------------------------------------------------------
+
+/*
+
+  static EdgeInsets getFieldScrollPadding({
+    @required BuildContext context,
+    @required AppBarType appBarType,
+
+  }){
+
+    final EdgeInsets _scrollPadding = EdgeInsets.only(
+      bottom: 100 + MediaQuery.of(context).viewInsets.bottom,
+      top: BldrsAppBar.height(context, appBarType) + BubbleHeader.getHeight() + Bubble.paddingValue() + 20,
+    );
+
+    return _scrollPadding;
+  }
+
+
+ */
