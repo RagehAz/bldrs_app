bdf6c3b6527b088426b797e894afb04dd0a27588
RagehAz <rageh.az@gmail.com>
2023-02-17 20:45:28 +0200

    - 3.1.4+5 : Packaged PullToRefresh Widget

--- lib/b_views/d_user/a_user_profile_screen/a_profile_page/aaa1_user_profile_page.dart
+++ lib/b_views/d_user/a_user_profile_screen/a_profile_page/aaa1_user_profile_page.dart
@@ -1,11 +1,12 @@
 import 'package:bldrs/a_models/a_user/user_model.dart';
 import 'package:bldrs/b_views/d_user/z_components/banners/a_user_profile_banners.dart';
-import 'package:bldrs/b_views/z_components/layouts/pull_to_refresh.dart';
 import 'package:bldrs/b_views/z_components/sizing/stratosphere.dart';
 import 'package:bldrs/c_protocols/auth_protocols/fire/auth_fire_ops.dart';
 import 'package:bldrs/c_protocols/user_protocols/protocols/a_user_protocols.dart';
 import 'package:bldrs/c_protocols/user_protocols/user/user_provider.dart';
+import 'package:bldrs_theme/bldrs_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:layouts/layouts.dart';
 
 class UserProfilePage extends StatelessWidget {
   // -----------------------------------------------------------------------------
@@ -18,6 +19,7 @@ class UserProfilePage extends StatelessWidget {
 
     return PullToRefresh(
       fadeOnBuild: true,
+      indicatorColor: Colorz.yellow255,
       onRefresh: () async {
 
         final UserModel _userModel = await UserProtocols.refetch(
