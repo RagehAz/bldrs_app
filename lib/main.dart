1acf714bf1c2c3b18f474d90c1b2f574aa36c4a6
RagehAz <rageh.az@gmail.com>
2023-03-17 22:16:57 +0200

    - 3.1.9 : Packaged Permissions in Devicer Package

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:stringer/stringer.dart';
 
-const String bldrsAppVersion = '3.1.4';
+const String bldrsAppVersion = '3.1.9';
 
 Future<void> main() async {
   /// -----------------------------------------------------------------------------
--- lib/x_dashboard/backend_lab/permissions_tests/permissions_test_screen.dart
+++ lib/x_dashboard/backend_lab/permissions_tests/permissions_test_screen.dart
@@ -1,10 +1,9 @@
 import 'package:bldrs/b_views/z_components/buttons/wide_button.dart';
 import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
-import 'package:bldrs/f_helpers/permissions/permits.dart';
-import 'package:bldrs/f_helpers/permissions/permits_protocols.dart';
 import 'package:bldrs/x_dashboard/backend_lab/permissions_tests/permission_button.dart';
 import 'package:bldrs/x_dashboard/zz_widgets/dashboard_layout.dart';
 import 'package:bldrs_theme/bldrs_theme.dart';
+import 'package:devicer/devicer.dart';
 import 'package:filers/filers.dart';
 import 'package:flutter/material.dart';
 import 'package:layouts/layouts.dart';
