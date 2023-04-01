6c3c649dafca440a3fe73ba1643648eca0b7bdf9
RagehAz <rageh.az@gmail.com>
2023-04-01 22:00:26 +0200

    3.1.17 : fixed info.plist syntax

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:stringer/stringer.dart';
 
-const String bldrsAppVersion = '3.1.13';
+const String bldrsAppVersion = '3.1.17';
 
 Future<void> main() async {
   /// -----------------------------------------------------------------------------
