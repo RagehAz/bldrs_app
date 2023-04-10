03b0997b07af94db648b052053ebea446f0f8067
RagehAz <rageh.az@gmail.com>
2023-04-10 03:44:42 +0200

    - 3.4.2 : MAJOR FIXES

--- lib/c_protocols/flyer_protocols/real/flyer_record_real_ops.dart
+++ lib/c_protocols/flyer_protocols/real/flyer_record_real_ops.dart
@@ -8,6 +8,7 @@ import 'package:bldrs/c_protocols/bz_protocols/real/bz_record_real_ops.dart';
 import 'package:bldrs/c_protocols/record_protocols/real/record_real_ops.dart';
 import 'package:bldrs/e_back_end/c_real/foundation/real_paths.dart';
 import 'package:filers/filers.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:mapper/mapper.dart';
 import 'package:real/real.dart';
