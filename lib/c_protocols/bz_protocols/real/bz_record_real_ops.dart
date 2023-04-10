9f52b8e0b1389f86e2e0de40d0b4cb25d5686cab
RagehAz <rageh.az@gmail.com>
2023-04-10 03:44:42 +0200

    - 3.4.2 : MAJOR FIXES

--- lib/c_protocols/bz_protocols/real/bz_record_real_ops.dart
+++ lib/c_protocols/bz_protocols/real/bz_record_real_ops.dart
@@ -5,6 +5,7 @@ import 'package:bldrs/a_models/x_secondary/contact_model.dart';
 import 'package:bldrs/c_protocols/record_protocols/real/record_real_ops.dart';
 import 'package:bldrs/e_back_end/c_real/foundation/real_paths.dart';
 import 'package:filers/filers.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:mapper/mapper.dart';
 import 'package:real/real.dart';
