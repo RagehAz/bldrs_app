951bb64e6f539c66fbfe13474fcce636c473b377
RagehAz <rageh.az@gmail.com>
2023-05-01 23:53:49 +0200

    - 3.5.21 : packaging world_zones

---
+++ lib/world_zoning/assets/currencies.dart
@@ -0,0 +1,7 @@
+part of world_zoning;
+
+class BldrsThemeCurrencies {
+  // -----------------------------------------------------------------------------
+  static const String currenciesFilePath = 'packages/bldrs_theme/lib/assets/planet/currencies.json';
+  // -----------------------------------------------------------------------------
+}
