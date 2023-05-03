2c728e147d3e4e975f67ac997b63ba7cab1138ec
RagehAz <rageh.az@gmail.com>
2023-05-03 04:16:10 +0200

    - 3.5.24+1 : moved all tests to dashboard

--- nodejstest/fire_docs.js
+++
@@ -1,6 +0,0 @@
-// const admin = require("firebase-admin");
-// const fire = admin.firestore();
-
-// const flyerDoc = "flyers/{flyerID}";
-// const userDoc = "users/{userID}";
-// const docStatistics = fire.collection("admin").doc("statistics");
