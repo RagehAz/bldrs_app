b0edd44cbade58a9ce10e437caa0ca868199adbd
RagehAz <rageh.az@gmail.com>
2023-05-03 04:16:10 +0200

    - 3.5.24+1 : moved all tests to dashboard

--- nodejstest/main.js
+++
@@ -1,7 +0,0 @@
-// const docs = require('./docs');
-
-// console.log("Starting main node");
-// console.log(`Fuck sum : ${docs.sum(2,4)}`);
-// console.log(`rageh are ${docs.rageh}`);
-// console.log(new docs.MathObject());
-// console.log(docs.map);
