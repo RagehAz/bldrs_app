4c534387324d85c8964efef0f375da7d2b81f4e9
RagehAz <rageh.az@gmail.com>
2023-05-16 09:49:43 +0200

    - 3.5.50 : fixed bldrs app bar for good

--- lib/b_views/z_components/layouts/main_layout/app_bar/components/app_bar_title.dart
+++ lib/b_views/z_components/layouts/main_layout/app_bar/components/app_bar_title.dart
@@ -4,16 +4,16 @@ class AppBarTitle extends StatelessWidget {
   /// --------------------------------------------------------------------------
   const AppBarTitle({
     @required this.pageTitleVerse,
-    @required this.backButtonIsOn,
+    // @required this.backButtonIsOn,
     @required this.width,
-    @required this.appBarRowWidgets,
+    // @required this.appBarRowWidgets,
     Key key
   }) : super(key: key);
   /// --------------------------------------------------------------------------
   final Verse pageTitleVerse;
-  final bool backButtonIsOn;
+  // final bool backButtonIsOn;
   final double width;
-  final List<Widget> appBarRowWidgets;
+  // final List<Widget> appBarRowWidgets;
   /// --------------------------------------------------------------------------
   static double getTitleHorizontalMargin({
     @required bool backButtonIsOn,
@@ -35,8 +35,8 @@ class AppBarTitle extends StatelessWidget {
           _HeadlineSuperVerse(
             title: pageTitleVerse,
             width: width,
-            appBarRowWidgets: appBarRowWidgets,
-            backButtonIsOn: backButtonIsOn,
+            // appBarRowWidgets: appBarRowWidgets,
+            // backButtonIsOn: backButtonIsOn,
           )
 
               :
@@ -48,8 +48,8 @@ class AppBarTitle extends StatelessWidget {
               return _HeadlineSuperVerse(
                 title: pageTitleVerse.copyWith(id: value),
                 width: width,
-                appBarRowWidgets: appBarRowWidgets,
-                backButtonIsOn: backButtonIsOn,
+                // appBarRowWidgets: appBarRowWidgets,
+                // backButtonIsOn: backButtonIsOn,
               );
 
             },
@@ -67,58 +67,45 @@ class _HeadlineSuperVerse extends StatelessWidget {
   /// --------------------------------------------------------------------------
   const _HeadlineSuperVerse({
     @required this.title,
-    @required this.backButtonIsOn,
     @required this.width,
-    @required this.appBarRowWidgets,
+    // @required this.appBarRowWidgets,
     Key key
   }) : super(key: key);
   /// --------------------------------------------------------------------------
   final Verse title;
-  final bool backButtonIsOn;
   final double width;
-  final List<Widget> appBarRowWidgets;
+  // final List<Widget> appBarRowWidgets;
   // --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
 
-    final double _titleHorizontalMargins = AppBarTitle.getTitleHorizontalMargin(
-      backButtonIsOn: backButtonIsOn,
-    );
 
-    if (Mapper.checkCanLoopList(appBarRowWidgets) == true){
+    // if (Mapper.checkCanLoopList(appBarRowWidgets) == true){
+    //   return BldrsText(
+    //     verse: title.copyWith(casing: Casing.upperCase),
+    //     weight: VerseWeight.black,
+    //     color: Colorz.white200,
+    //     shadow: true,
+    //     italic: true,
+    //     // maxLines: 1,
+    //     centered: false,
+    //     scaleFactor: 0.9,
+    //   );
+    // }
+    //
+    // else {
       return BldrsText(
+        width: width,
         verse: title.copyWith(casing: Casing.upperCase),
         weight: VerseWeight.black,
         color: Colorz.white200,
-        margin: 0,
         shadow: true,
         italic: true,
-        // maxLines: 1,
+        maxLines: 2,
         centered: false,
         scaleFactor: 0.9,
       );
-    }
-
-    else {
-      return Container(
-        width: width,
-        // color: Colorz.bloodTest,
-        margin: EdgeInsets.symmetric(
-            horizontal: _titleHorizontalMargins
-        ),
-        child: BldrsText(
-          verse: title.copyWith(casing: Casing.upperCase),
-          weight: VerseWeight.black,
-          color: Colorz.white200,
-          margin: 0,
-          shadow: true,
-          italic: true,
-          maxLines: 2,
-          centered: false,
-          scaleFactor: 0.9,
-        ),
-      );
-    }
+    // }
 
   }
   // --------------------------------------------------------------------------
