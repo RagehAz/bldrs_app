b013ae25706e8ed9459bb865946d56fd7268ef46
RagehAz <rageh.az@gmail.com>
2023-06-22 07:33:09 +0200

    1.0.2 : yalla publish

--- lib/b_views/a_starters/b_home_screen/x_home_screen_controllers.dart
+++ lib/b_views/a_starters/b_home_screen/x_home_screen_controllers.dart
@@ -63,7 +63,7 @@ List<NavModel> generateMainNavModels({
   @required UserModel userModel,
 }){
 
-  final String _countryFlag = currentZone?.icon;
+  final String _countryFlag = currentZone?.icon ?? Iconz.contAfrica;
   final bool _userIsSignedUp = Authing.userIsSignedUp(userModel?.signInMethod);
 
   // blog('generateMainNavModels() _userIsSignedUp: $_userIsSignedUp : ${userModel?.signInMethod}');
