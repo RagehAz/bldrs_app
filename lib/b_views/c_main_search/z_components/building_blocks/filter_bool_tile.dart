7cbbedfc6cf608ae2746ab717f99ef30b79fae8f
RagehAz <rageh.az@gmail.com>
2023-07-07 22:55:00 +0200

    1.0.4 : fixed bubble arabic responsiveness, reviews pagination & stuff

--- lib/b_views/c_main_search/z_components/building_blocks/filter_bool_tile.dart
+++ lib/b_views/c_main_search/z_components/building_blocks/filter_bool_tile.dart
@@ -2,6 +2,7 @@ import 'package:basics/bubbles/tile_bubble/tile_bubble.dart';
 import 'package:bldrs/b_views/z_components/bubbles/a_structure/bldrs_bubble_header_vm.dart';
 import 'package:bldrs/b_views/z_components/texting/super_verse/verse_model.dart';
 import 'package:bldrs/b_views/c_main_search/super_search_screen.dart';
+import 'package:bldrs/c_protocols/main_providers/ui_provider.dart';
 
 import 'package:flutter/material.dart';
 
@@ -28,18 +29,20 @@ class FilterBoolTile extends StatelessWidget {
     final double _tileWidth = SuperSearchScreen.getFilterTileWidth(context);
 
     return TileBubble(
-          bubbleWidth: _tileWidth,
-          bubbleHeaderVM: BldrsBubbleHeaderVM.bake(
-            context: context,
-            leadingIcon: icon,
-            leadingIconSizeFactor: iconIsBig == true ? 1 : 0.7,
-            headerWidth: _tileWidth,
-            headlineVerse: verse,
-            hasSwitch: true,
-            switchValue: switchValue,
-            onSwitchTap: onSwitchTap,
-          ),
-        );
+      bubbleWidth: _tileWidth,
+      bubbleHeaderVM: BldrsBubbleHeaderVM.bake(
+        context: context,
+        leadingIcon: icon,
+        leadingIconSizeFactor: iconIsBig == true ? 1 : 0.7,
+        headerWidth: _tileWidth,
+        headlineVerse: verse,
+        hasSwitch: true,
+        switchValue: switchValue,
+        onSwitchTap: onSwitchTap,
+      ),
+      textDirection: UiProvider.getAppTextDir(),
+      appIsLTR: UiProvider.checkAppIsLeftToRight(),
+    );
 
   }
   // -----------------------------------------------------------------------------
