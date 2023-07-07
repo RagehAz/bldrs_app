531e14be590f5cbfd4932044509847277c8df8f9
RagehAz <rageh.az@gmail.com>
2023-07-07 22:46:15 +0200

    1.0.4 : Arabic is good isa

--- lib/b_views/z_components/static_progress_bar/static_strips.dart
+++ lib/b_views/z_components/static_progress_bar/static_strips.dart
@@ -1,13 +1,11 @@
 import 'package:basics/animators/helpers/sliders.dart';
 import 'package:basics/bldrs_theme/classes/ratioz.dart';
-import 'package:basics/helpers/classes/checks/tracers.dart';
+import 'package:basics/helpers/classes/maps/mapper.dart';
+import 'package:bldrs/b_views/j_flyer/z_components/b_parts/d_progress_bar/d_progress_box.dart';
 import 'package:bldrs/b_views/j_flyer/z_components/x_helpers/x_flyer_color.dart';
 import 'package:bldrs/b_views/j_flyer/z_components/x_helpers/x_flyer_dim.dart';
-import 'package:bldrs/b_views/j_flyer/z_components/b_parts/d_progress_bar/d_progress_box.dart';
 import 'package:bldrs/b_views/z_components/static_progress_bar/static_strip.dart';
-import 'package:basics/helpers/classes/maps/mapper.dart';
 import 'package:flutter/material.dart';
-import 'package:basics/helpers/classes/space/scale.dart';
 
 class StaticStrips extends StatelessWidget {
   /// --------------------------------------------------------------------------
