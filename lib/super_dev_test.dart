07b27b82c4a79a89054c0d137046620f256ed8b6
RagehAz <rageh.az@gmail.com>
2023-07-23 22:04:49 +0200

    1.4.0 : removed flyer external shadow

--- lib/super_dev_test.dart
+++ lib/super_dev_test.dart
@@ -7,47 +7,28 @@
 // import 'package:fire/super_fire.dart';
 // import 'package:flutter/cupertino.dart';
 
+import 'package:basics/helpers/classes/checks/tracers.dart';
+import 'package:bldrs/c_protocols/main_providers/ui_provider.dart';
+import 'package:fire/super_fire.dart';
+import 'package:flutter/material.dart';
+
 /// SUPER_DEV_TEST
 Future<void> superDevTest() async {
 
-  // final BuildContext context = getMainContext();
-  // final List<Map<String, dynamic>> _maps = await Fire.readAllColl(coll: 'bzz');
-  //
-  // int i = 0;
-  // for (final Map<String, dynamic> map in _maps){
-  //
-  //   blog('$i : Doing with ${map['id']}');
-  //
-  //   i++;
-  //
-  //   final BzModel? _bz = BzModel.decipherBz(map: map, fromJSON: false);
-  //
-  //   final List<FlyerModel> _flyers = await FlyerProtocols.fetchFlyers(
-  //     context: context,
-  //     flyersIDs: _bz?.flyersIDs,
-  //   );
-  //
-  //   PublicationModel _publication = PublicationModel.emptyModel;
-  //
-  //   for (final FlyerModel _flyer in _flyers){
-  //
-  //     _publication = PublicationModel.insertFlyerInPublications(
-  //         pub: _publication,
-  //         flyerID: _flyer.id!,
-  //         toState: _flyer.publishState!,
-  //     );
-  //
-  //   }
-  //
-  //   await Fire.updateDocField(
-  //       coll: 'bzz',
-  //       doc: _bz!.id!,
-  //       field: 'publication',
-  //       input: _publication.toMap(),
-  //   );
-  //
-  // }
-  //
-  // blog('done all : i : $i');
+  final BuildContext context = getMainContext();
+  final List<Map<String, dynamic>> _maps = await Fire.readAllColl(coll: 'bzz');
+
+  int i = 0;
+  for (final Map<String, dynamic> map in _maps){
+
+    blog('$i : Doing with ${map['id']}');
+
+    i++;
+
+    await Fire.deleteDocField(coll: 'bzz', doc: map['id'], field: 'flyersIDs');
+
+  }
+
+  blog('done all : i : $i');
 
 }
