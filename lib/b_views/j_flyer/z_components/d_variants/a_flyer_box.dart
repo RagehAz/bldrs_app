276c34f8fbb27fd2cc7c7d65c0b5621da2246ba5
RagehAz <rageh.az@gmail.com>
2023-07-23 22:04:49 +0200

    1.4.0 : removed flyer external shadow

--- lib/b_views/j_flyer/z_components/d_variants/a_flyer_box.dart
+++ lib/b_views/j_flyer/z_components/d_variants/a_flyer_box.dart
@@ -1,5 +1,4 @@
 import 'package:basics/bldrs_theme/classes/colorz.dart';
-import 'package:basics/bldrs_theme/classes/shadowers.dart';
 import 'package:bldrs/b_views/j_flyer/z_components/x_helpers/x_flyer_dim.dart';
 import 'package:flutter/material.dart';
 
@@ -46,7 +45,6 @@ class FlyerBox extends StatelessWidget {
           decoration: BoxDecoration(
             color: boxColor,
             borderRadius: _flyerBorders,
-            boxShadow: shadowIsOn == true ? Shadower.flyerZoneShadow : null,
           ),
           child: ClipRRect(
             borderRadius: _flyerBorders,
@@ -62,60 +60,3 @@ class FlyerBox extends StatelessWidget {
   }
   // -----------------------------------------------------------------------------
 }
-
-/// NEW PROPOSAL
-// class FlyerBox extends StatelessWidget {
-//   /// --------------------------------------------------------------------------
-//   const FlyerBox({
-//     required this.flyerBoxWidth,
-//     this.stackWidgets,
-//     this.boxColor = Colorz.white20,
-//     this.onTap,
-//     this.shadowIsOn = false,
-//     super.key
-//   });
-//   /// --------------------------------------------------------------------------
-//   // --- NEAT AND CLEAN
-//   /// width factor * screenWidth = flyerWidth
-//   final double flyerBoxWidth;
-//   /// internal parts of the flyer
-//   final List<Widget>? stackWidgets;
-//   final Color? boxColor;
-//   final void Function()? onTap;
-//   final bool shadowIsOn;
-//   // -----------------------------------------------------------------------------
-//   @override
-//   Widget build(BuildContext context) {
-//     // --------------------
-//     final BorderRadius _flyerBorders = FlyerDim.flyerCorners(flyerBoxWidth);
-//     // --------------------
-//     final double _flyerBoxHeight = FlyerDim.flyerHeightByFlyerWidth(
-//       flyerBoxWidth: flyerBoxWidth,
-//     );
-//     // --------------------
-//     return Center(
-//       key: const ValueKey<String>('flyerBoxWidget'),
-//       child: AspectRatio(
-//         aspectRatio: FlyerDim.flyerAspectRatio(),
-//         child: TapLayer(
-//           onTap: onTap,
-//           splashColor: boxColor?.withOpacity(0.2),
-//           corners: _flyerBorders,
-//           width: flyerBoxWidth,
-//           height: _flyerBoxHeight,
-//           boxColor: boxColor,
-//           alignment: Alignment.topCenter,
-//           child: ClipRRect(
-//             borderRadius: FlyerDim.flyerCorners(flyerBoxWidth),
-//             child: Stack(
-//               alignment: Alignment.topCenter,
-//               children: stackWidgets ?? <Widget>[],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//     // --------------------
-//   }
-//   // -----------------------------------------------------------------------------
-// }
