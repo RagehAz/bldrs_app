fa220790acad2c51f969570edac1b5046a3e03a3
RagehAz <rageh.az@gmail.com>
2023-07-25 09:25:12 +0200

    1.4.0 : refactoring & integrating new pic picking method

--- lib/f_helpers/drafters/bldrs_pic_maker.dart
+++ lib/f_helpers/drafters/bldrs_pic_maker.dart
@@ -1,7 +1,12 @@
 import 'dart:typed_data';
+
+import 'package:basics/helpers/classes/maps/mapper.dart';
+import 'package:basics/mediator/pic_maker/pic_maker.dart';
+import 'package:bldrs/a_models/i_pic/pic_model.dart';
 import 'package:bldrs/b_views/z_components/texting/super_verse/verse_model.dart';
 import 'package:bldrs/c_protocols/main_providers/ui_provider.dart';
-import 'package:basics/mediator/pic_maker/pic_maker.dart';
+import 'package:bldrs/f_helpers/theme/standards.dart';
+import 'package:flutter/foundation.dart';
 import 'package:photo_manager/photo_manager.dart';
 
 // // -----------------------------------------------------------------------------
@@ -25,12 +30,123 @@ class BldrsPicMaker {
 
   const BldrsPicMaker();
 
-  // --------------------------------------------------------------------------
-  /// TESTED : WORKS PERFECT
-  static Future<Uint8List?> pickAndCropSinglePic({
+  // -----------------------------------------------------------------------------
+
+  /// MAKERS
+
+  // --------------------
+  ///
+  static Future<PicModel?> makePic({
+    required PicMakerType picMakerType,
     required bool cropAfterPick,
     required double aspectRatio,
-    double? resizeToWidth,
+    required int compressionQuality,
+    required double finalWidth,
+    required String assignPath,
+    required List<String> ownersIDs,
+    required String name,
+  }) async {
+    PicModel? _output;
+    Uint8List? _bytes;
+
+    if(picMakerType == PicMakerType.galleryImage){
+      _bytes = await _pickAndCropSinglePic(
+        cropAfterPick: cropAfterPick,
+        aspectRatio: aspectRatio,
+        finalWidth: finalWidth,
+        compressionQuality: compressionQuality,
+        onlyCompress: Standards.onlyCompressOnResizing,
+        // selectedAsset:
+      );
+    }
+
+    else if (picMakerType == PicMakerType.cameraImage){
+      _bytes = await _shootAndCropCameraPic(
+        cropAfterPick: cropAfterPick,
+        aspectRatio: aspectRatio,
+        finalWidth: finalWidth,
+        compressionQuality: compressionQuality,
+        onlyCompress: Standards.onlyCompressOnResizing,
+      );
+    }
+
+    if (_bytes != null){
+
+      _output = await PicModel.combinePicModel(
+        ownersIDs: ownersIDs,
+        name: name,
+        bytes: _bytes,
+        compressionQuality: compressionQuality,
+        picMakerType: picMakerType,
+        assignPath: assignPath,
+      );
+
+    }
+
+    return _output;
+  }
+  // --------------------
+  ///
+  static Future<List<PicModel>> makePics({
+    required bool cropAfterPick,
+    required double aspectRatio,
+    required int compressionQuality,
+    required double finalWidth,
+    required String Function(int index) assignPath,
+    required List<String> ownersIDs,
+    required String groupName,
+    required int maxAssets,
+  }) async {
+
+    final List<PicModel> _output = [];
+
+    final List<Uint8List> _bytezz = await _pickAndCropMultiplePics(
+      aspectRatio: aspectRatio,
+      cropAfterPick: cropAfterPick,
+      finalWidth: finalWidth,
+      compressionQuality: compressionQuality,
+      onlyCompress: Standards.onlyCompressOnResizing,
+      maxAssets: maxAssets,
+      // selectedAssets:
+    );
+
+    if (Mapper.checkCanLoopList(_bytezz) == true){
+
+      for (int i = 0; i < _bytezz.length; i++){
+
+        final Uint8List bytes = _bytezz[i];
+
+        final PicModel? _picModel = await PicModel.combinePicModel(
+          ownersIDs: ownersIDs,
+          name: '${groupName}_$i',
+          bytes: bytes,
+          compressionQuality: compressionQuality,
+          picMakerType: PicMakerType.galleryImage,
+          assignPath: assignPath(i),
+        );
+
+        if (_picModel != null){
+          _output.add(_picModel);
+        }
+
+      }
+
+    }
+
+    return _output;
+  }
+  // -----------------------------------------------------------------------------
+
+  /// PICKERS - SHOOTERS
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  static Future<Uint8List?> _pickAndCropSinglePic({
+    required bool cropAfterPick,
+    required double aspectRatio,
+    required int compressionQuality,
+    required bool onlyCompress,
+    double? finalWidth,
     AssetEntity? selectedAsset,
   }) async {
 
@@ -38,20 +154,24 @@ class BldrsPicMaker {
       context: getMainContext(),
       cropAfterPick: cropAfterPick,
       aspectRatio: aspectRatio,
-      resizeToWidth: resizeToWidth,
+      finalWidth: finalWidth,
       selectedAsset: selectedAsset,
       appIsLTR: UiProvider.checkAppIsLeftToRight(),
       confirmText: Verse.transBake('phid_continue')!,
+      onlyCompress: onlyCompress,
+      compressionQuality: compressionQuality,
     );
 
     return _bytes;
   }
   // --------------------
   /// TESTED : WORKS PERFECT
-  static Future<List<Uint8List>> pickAndCropMultiplePics({
+  static Future<List<Uint8List>> _pickAndCropMultiplePics({
     required double aspectRatio,
     required bool cropAfterPick,
-    double? resizeToWidth,
+    required int compressionQuality,
+    required bool onlyCompress,
+    double? finalWidth,
     int maxAssets = 10,
     List<AssetEntity>? selectedAssets,
   }) async {
@@ -60,13 +180,44 @@ class BldrsPicMaker {
       context: getMainContext(),
       cropAfterPick: cropAfterPick,
       aspectRatio: aspectRatio,
-      resizeToWidth: resizeToWidth,
+      finalWidth: finalWidth,
       appIsLTR: UiProvider.checkAppIsLeftToRight(),
+      confirmText: Verse.transBake('phid_continue')!,
+      selectedAssets: selectedAssets,
+      maxAssets: maxAssets,
+      compressionQuality: compressionQuality,
+      onlyCompress: onlyCompress,
+    );
+
+    return _bytes;
+  }
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  static Future<Uint8List?> _shootAndCropCameraPic({
+    required double aspectRatio,
+    required bool cropAfterPick,
+    required int compressionQuality,
+    required bool onlyCompress,
+    required double finalWidth,
+  }) async {
+
+    final Uint8List? _bytes = await PicMaker.shootAndCropCameraPic(
+      context: getMainContext(),
+      cropAfterPick: cropAfterPick,
+      aspectRatio: aspectRatio,
+      finalWidth: finalWidth,
+      appIsLTR: UiProvider.checkAppIsLeftToRight(),
+      onlyCompress: onlyCompress,
+      compressionQuality: compressionQuality,
       confirmText: Verse.transBake('phid_continue')!,
     );
 
     return _bytes;
   }
+  // -----------------------------------------------------------------------------
+
+  /// CROPPERS
+
   // --------------------
   /// TESTED : WORKS PERFECT
   static Future<Uint8List?> cropPic({
--- lib/super_dev_test.dart
+++ lib/super_dev_test.dart
@@ -1,21 +1,10 @@
-// import 'package:basics/helpers/classes/checks/tracers.dart';
-// import 'package:bldrs/a_models/b_bz/bz_model.dart';
-// import 'package:bldrs/a_models/f_flyer/flyer_model.dart';
-// import 'package:bldrs/a_models/f_flyer/publication_model.dart';
-// import 'package:bldrs/c_protocols/flyer_protocols/protocols/a_flyer_protocols.dart';
-// import 'package:bldrs/c_protocols/main_providers/ui_provider.dart';
-// import 'package:fire/super_fire.dart';
-// import 'package:flutter/cupertino.dart';
-
 import 'package:basics/helpers/classes/checks/tracers.dart';
-import 'package:bldrs/c_protocols/main_providers/ui_provider.dart';
 import 'package:fire/super_fire.dart';
-import 'package:flutter/material.dart';
 
 /// SUPER_DEV_TEST
 Future<void> superDevTest() async {
 
-  final BuildContext context = getMainContext();
+  // final BuildContext context = getMainContext();
   final List<Map<String, dynamic>> _maps = await Fire.readAllColl(coll: 'bzz');
 
   int i = 0;
@@ -25,7 +14,7 @@ Future<void> superDevTest() async {
 
     i++;
 
-    await Fire.deleteDocField(coll: 'bzz', doc: map['id'], field: 'flyersIDs');
+    // await Fire.deleteDocField(coll: 'bzz', doc: map['id'], field: 'flyersIDs');
 
   }
 
