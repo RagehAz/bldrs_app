f29c332163d7a9704ca3e329b300d83a63b4c8c1
RagehAz <rageh.az@gmail.com>
2023-08-07 12:33:55 +0200

    1.4.0 : should update bz types icons now

--- lib/b_views/j_on_boarding/components/phids_wheel.dart
+++ lib/b_views/j_on_boarding/components/phids_wheel.dart
@@ -1,4 +1,5 @@
 import 'package:basics/bldrs_theme/classes/colorz.dart';
+import 'package:basics/helpers/classes/checks/tracers.dart';
 import 'package:basics/helpers/classes/nums/numeric.dart';
 import 'package:bldrs/b_views/z_components/buttons/dream_box/bldrs_box.dart';
 import 'package:bldrs/b_views/z_components/texting/super_verse/super_verse.dart';
@@ -9,8 +10,47 @@ import 'package:flutter/material.dart';
 import 'package:wheel_chooser/wheel_chooser.dart';
 
 class PhidsWheel extends StatelessWidget {
-  // --------------------------------------------------------------------------
+  // --------------------
   const PhidsWheel({
+    required this.width,
+    required this.phids,
+    required this.height,
+    required this.autoRotate,
+    super.key
+  });
+  // --------------------
+  final double width;
+  final double height;
+  final List<String> phids;
+  final bool autoRotate;
+  // --------------------
+  @override
+  Widget build(BuildContext context) {
+
+    if (autoRotate == true){
+      return AutoAnimatedPhidsWheel(
+        width: width,
+        height: height,
+        phids: phids,
+        // autoRotate: true,
+      );
+    }
+
+    else {
+      return NonAnimatedPhidsWheel(
+        height: height,
+        width: width,
+        phids: phids,
+      );
+    }
+
+  }
+  // --------------------
+}
+
+class NonAnimatedPhidsWheel extends StatelessWidget {
+  // --------------------
+  const NonAnimatedPhidsWheel({
     required this.width,
     required this.phids,
     required this.height,
@@ -20,7 +60,7 @@ class PhidsWheel extends StatelessWidget {
   final double width;
   final double height;
   final List<String> phids;
-  // --------------------------------------------------------------------------
+  // --------------------
   @override
   Widget build(BuildContext context) {
     // --------------------
@@ -33,7 +73,7 @@ class PhidsWheel extends StatelessWidget {
         /// PHYSICS
         // ------
         /// WHEN INCREASED SHRINKS SPACING
-        squeeze: 1,
+        // squeeze: 1,
         /// 0.0005 IS FLAT - 0.01 IS MAXIMUM WHEEL CURVATURE
         // perspective: 0.01, // is best value to avoid top and bottom flickering
         /// FLICKERS THE SCROLLING AND FUCKS EVERYTHING WHEN AT 0.1
@@ -87,5 +127,158 @@ class PhidsWheel extends StatelessWidget {
     );
     // --------------------
   }
-  /// --------------------------------------------------------------------------
+}
+
+class AutoAnimatedPhidsWheel extends StatefulWidget {
+  // --------------------------------------------------------------------------
+  const AutoAnimatedPhidsWheel({
+    required this.width,
+    required this.phids,
+    required this.height,
+    this.autoRotate = true,
+    super.key
+  });
+  // --------------------
+  final double width;
+  final double height;
+  final List<String> phids;
+  final bool autoRotate;
+  // --------------------
+  @override
+  State<AutoAnimatedPhidsWheel> createState() => _AutoAnimatedPhidsWheelState();
+  // --------------------------------------------------------------------------
+}
+
+class _AutoAnimatedPhidsWheelState extends State<AutoAnimatedPhidsWheel> {
+  // -----------------------------------------------------------------------------
+  FixedExtentScrollController? _controller;
+  // -----------------------------------------------------------------------------
+  @override
+  void initState() {
+    super.initState();
+    _controller = FixedExtentScrollController(
+      // initialItem: 0,
+    );
+  }
+  // --------------------
+  bool _isInit = true;
+  @override
+  void didChangeDependencies() {
+
+    if (_isInit && mounted) {
+      _isInit = false; // good
+
+      asyncInSync(() async {
+
+         if (widget.autoRotate == true){
+           await _animate();
+         }
+
+      });
+
+    }
+    super.didChangeDependencies();
+  }
+  // --------------------
+  /*
+  @override
+  void didUpdateWidget(TheStatefulScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.thing != widget.thing) {
+      unawaited(_doStuff());
+    }
+  }
+   */
+  // --------------------
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+  // --------------------
+  Future<void> _animate() async {
+
+    const int _unitDuration = 800;
+
+    await Future.delayed(const Duration(milliseconds: 1));
+
+    await _controller?.animateToItem(
+      widget.phids.length - 1,
+      duration: Duration(milliseconds: widget.phids.length * _unitDuration),
+      curve: Curves.easeInOut,
+    );
+  }
+  // -----------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    // --------------------
+    return SizedBox(
+      width: widget.width,
+      height: 40,
+      // color: Colorz.blue80,
+      // alignment: Alignment.center,
+      child: WheelChooser.custom(
+        controller: _controller,
+        /// PHYSICS
+        // ------
+        /// WHEN INCREASED SHRINKS SPACING
+        // squeeze: 1,
+        /// 0.0005 IS FLAT - 0.01 IS MAXIMUM WHEEL CURVATURE
+        // perspective: 0.01, // is best value to avoid top and bottom flickering
+        /// FLICKERS THE SCROLLING AND FUCKS EVERYTHING WHEN AT 0.1
+        magnification: 0.001,
+        // ------------------------
+        /// BEHAVIOUR
+        // ------
+        /// WHEEL STARTING INDEX
+        startPosition: null, //Numeric.createRandomIndex(listLength: widget.phids.length),
+        /// ROTATION DIRECTION
+        horizontal: true,
+        /// LOOPS THE WHEEL LIST
+        isInfinite: true,
+        // ------------------------
+        /// SIZING
+        // ------
+        listHeight: widget.height,
+        itemSize: 100,
+        listWidth: widget.width,
+        // ------------------------
+        /// SIZING
+        // ------
+        onValueChanged: (dynamic value) async {
+          // final int _index = value;
+          // final String _selectedIcon = standardLockIcons[_index].key!;
+          // // blog('value changed to : $_selectedIcon');
+          // onChanged(_selectedIcon);
+        },
+        children: <Widget>[
+
+          ...List.generate(widget.phids.length, (index){
+            final String _phid = widget.phids[index];
+            final Verse? _verse = getVerse(_phid);
+            final String? _icon = ChainsProvider.proGetPhidIcon(son: _phid);
+            return BldrsBox(
+              height: 30,
+              width: 100,
+              icon: _icon,
+              verse: _verse,
+              verseMaxLines: 2,
+              verseWeight: VerseWeight.thin,
+              verseCentered: _icon == null,
+              verseScaleFactor: 0.6,
+              color: Colorz.white20,
+              bubble: false,
+              onTap: () async {
+
+                await _animate();
+
+              },
+            );
+          }),
+
+        ],
+      ),
+    );
+    // --------------------
+  }
 }
