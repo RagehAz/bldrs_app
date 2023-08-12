68475ca940c4c63731e8666a20fbb00b122d090c
RagehAz <rageh.az@gmail.com>
2023-08-12 19:36:30 +0200

    2.2.0 : translated countries to italian

--- lib/super_dev_test.dart
+++ lib/super_dev_test.dart
@@ -1,19 +1,7 @@
-import 'package:basics/helpers/classes/checks/tracers.dart';
-import 'package:bldrs/b_views/z_components/dialogs/dialogz/dialogs.dart';
 
 /// SUPER_DEV_TEST
 Future<void> superDevTest() async {
 
-  blog('a7a');
 
-  // final String _code = Localizer.getCurrentLangCode();
-  // final String? _ldb = await Localizer.readLDBLangCode();
-  // blog('kos omak : $_code : $_ldb');
-  //
-  // await Dialogs.topNotice(verse: Verse.plain('kos omak : $_code : $_ldb'));
-
-  await Dialogs.confirmProceed();
-
-  // await BldrsNav.goToLogoScreenAndRemoveAllBelow(animatedLogoScreen: false);
 
 }
