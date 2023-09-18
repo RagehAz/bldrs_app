4cdf8aa504bc8baaa8c28d1c574b03361b7e6149
RagehAz <rageh.az@gmail.com>
2023-09-19 01:28:46 +0200

    2.7.0 : fixed flyer record box limit concluder and dynamic user banner sizing

--- lib/b_views/j_flyer/z_components/b_parts/b_footer/info_button/a_info_button_structure/f_info_page_contents.dart
+++ lib/b_views/j_flyer/z_components/b_parts/b_footer/info_button/a_info_button_structure/f_info_page_contents.dart
@@ -11,7 +11,7 @@ import 'package:bldrs/a_models/g_statistics/counters/flyer_counter_model.dart';
 import 'package:bldrs/a_models/g_statistics/records/record_type.dart';
 import 'package:bldrs/a_models/x_utilities/pdf_model.dart';
 import 'package:bldrs/b_views/f_bz/e_flyer_maker_screen/c_pdf_screen.dart';
-import 'package:bldrs/b_views/j_flyer/z_components/b_parts/b_footer/info_button/a_info_button_structure/g_flyer_counters_and_records.dart';
+import 'package:bldrs/b_views/j_flyer/z_components/b_parts/b_footer/info_button/expanded_info_page_parts/g_flyer_counters_and_records.dart';
 import 'package:bldrs/b_views/j_flyer/z_components/b_parts/b_footer/info_button/expanded_info_page_parts/info_page_headline.dart';
 import 'package:bldrs/b_views/j_flyer/z_components/b_parts/b_footer/info_button/expanded_info_page_parts/info_page_keywords.dart';
 import 'package:bldrs/b_views/j_flyer/z_components/b_parts/b_footer/info_button/expanded_info_page_parts/info_page_main_details.dart';
