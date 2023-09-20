b3d027d6031318a8e84871f93575fcae20daf182
RagehAz <rageh.az@gmail.com>
2023-09-20 23:14:35 +0200

    2.8.0 : going 280 now

--- lib/b_views/z_components/layouts/pyramids/pyramid_floating_button.dart
+++ lib/b_views/z_components/layouts/pyramids/pyramid_floating_button.dart
@@ -1,5 +1,7 @@
 import 'package:bldrs/b_views/z_components/buttons/general_buttons/bldrs_box.dart';
+import 'package:bldrs/b_views/z_components/layouts/super_tool_tip.dart';
 import 'package:bldrs/b_views/z_components/notes/x_components/red_dot_badge.dart';
+import 'package:bldrs/b_views/z_components/texting/super_verse/verse_model.dart';
 import 'package:flutter/material.dart';
 
 class PyramidFloatingButton extends StatelessWidget {
@@ -12,6 +14,7 @@ class PyramidFloatingButton extends StatelessWidget {
     this.onLongTap,
     this.isDeactivated,
     this.iconColor,
+    this.toolTip,
     super.key
   });
   /// ---------------------------------------------------------------------------
@@ -22,28 +25,33 @@ class PyramidFloatingButton extends StatelessWidget {
   final Function? onLongTap;
   final bool? isDeactivated;
   final Color? iconColor;
+  final Verse? toolTip;
   /// ---------------------------------------------------------------------------
   static double size = 45;
   /// ---------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
 
-    return RedDotBadge(
-      childWidth: size,
-      redDotIsOn: redDotCount > 0,
-      count: redDotCount,
-      shrinkChild: true,
-      child: BldrsBox(
-        height: size,
-        width: size,
-        corners: size/2,
-        color: color,
-        icon: icon,
-        iconColor: iconColor,
-        iconSizeFactor: 0.6,
-        onTap: onTap,
-        onLongTap: onLongTap,
-        isDisabled: isDeactivated,
+    return SuperToolTip(
+      triggerMode: TooltipTriggerMode.longPress,
+      verse: toolTip,
+      child: RedDotBadge(
+        childWidth: size,
+        redDotIsOn: redDotCount > 0,
+        count: redDotCount,
+        shrinkChild: true,
+        child: BldrsBox(
+          height: size,
+          width: size,
+          corners: size/2,
+          color: color,
+          icon: icon,
+          iconColor: iconColor,
+          iconSizeFactor: 0.6,
+          onTap: onTap,
+          onLongTap: onLongTap,
+          isDisabled: isDeactivated,
+        ),
       ),
     );
 
