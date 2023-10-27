589fc10a22695e7768d1cd5c443ed8e1fb2266c8
RagehAz <rageh.az@gmail.com>
2023-10-27 20:25:33 +0200

    2.13.0 : TAMAM gta protocols

--- lib/b_views/z_components/layouts/custom_layouts/tabs_layout.dart
+++ lib/b_views/z_components/layouts/custom_layouts/tabs_layout.dart
@@ -3,10 +3,10 @@ import 'package:basics/bldrs_theme/classes/colorz.dart';
 import 'package:basics/helpers/classes/checks/tracers.dart';
 import 'package:basics/helpers/classes/space/scale.dart';
 import 'package:bldrs/a_models/x_ui/nav_model.dart';
+import 'package:bldrs/b_views/z_components/blur/blur_layer.dart';
 import 'package:bldrs/b_views/z_components/buttons/general_buttons/bldrs_box.dart';
 import 'package:bldrs/b_views/z_components/layouts/custom_layouts/pages_builder.dart';
 import 'package:bldrs/b_views/z_components/layouts/main_layout/main_layout.dart';
-import 'package:bldrs/b_views/z_components/sizing/stratosphere.dart';
 import 'package:bldrs/b_views/z_components/static_progress_bar/progress_bar_model.dart';
 import 'package:bldrs/b_views/z_components/texting/super_verse/verse_model.dart';
 import 'package:bldrs/c_protocols/main_providers/ui_provider.dart';
@@ -18,23 +18,29 @@ class TabsLayout extends StatefulWidget {
     required this.views,
     required this.title,
     this.appBarRowWidgets,
+    this.onBack,
+    this.loading,
     super.key
   });
   /// -----------------------
   final List<NavModel> views;
   final Verse? title;
   final List<Widget>? appBarRowWidgets;
+  final Function? onBack;
+  final ValueNotifier<bool>? loading;
   /// --------------------------------------------------------------------------
   @override
   _TabsLayoutState createState() => _TabsLayoutState();
   /// --------------------------------------------------------------------------
   static double getPageHeight(){
     final double _screenHeight = Scale.screenHeight(getMainContext());
-    final double _stratosphere = Stratosphere.stratosphereSandwich.top;
-    const double _footerHeight = 50;
-    final double _pageHeight = _screenHeight - _stratosphere - _footerHeight;
-    return _pageHeight;
+    // final double _stratosphere = Stratosphere.stratosphereSandwich.top;
+    // const double _footerHeight = 50;
+    // final double _pageHeight = _screenHeight - _stratosphere - _footerHeight;
+    return _screenHeight;
   }
+  /// --------------------------------------------------------------------------
+  static const double footerHeight = 50;
   /// --------------------------------------------------------------------------
 }
 
@@ -105,7 +111,6 @@ class _TabsLayoutState extends State<TabsLayout> {
   Widget build(BuildContext context) {
     // --------------------
     final double _screenWidth = Scale.screenWidth(context);
-    const double _footerHeight = 50;
     final double _pageHeight = TabsLayout.getPageHeight();
     final double _footerButtonWidth = Scale.getUniformRowItemWidth(
       context: context,
@@ -114,18 +119,16 @@ class _TabsLayoutState extends State<TabsLayout> {
       considerMargins: false,
       spacing: 0,
     );
-    const double _footerButtonHeight = _footerHeight - 10;
+    const double _footerButtonHeight = TabsLayout.footerHeight - 10;
     // --------------------
     return MainLayout(
       title: widget.title,
       progressBarModel: _progressBarModel,
-      loading: _loading,
+      loading: widget.loading ?? _loading,
       appBarRowWidgets: widget.appBarRowWidgets,
-      child: Column(
+      onBack: widget.onBack,
+      child: Stack(
         children: <Widget>[
-
-          /// STRATOSPHERE
-          const Stratosphere(),
 
           /// PAGES
           PagerBuilder(
@@ -141,44 +144,46 @@ class _TabsLayoutState extends State<TabsLayout> {
                 return _view;
                 
               }),
-              
+
             ],
           ),
 
           /// FOOTER
-          Container(
-            width: _screenWidth,
-            height: _footerHeight,
-            color: Colorz.black255,
-            child: ValueListenableBuilder(
-              valueListenable: _progressBarModel,
-              builder: (context, ProgressBarModel? model, Widget? child) {
+          Positioned(
+            bottom: 0,
+            child: BlurLayer(
+              width: _screenWidth,
+              height: TabsLayout.footerHeight,
+              color: Colorz.black50,
+              blurIsOn: true,
+              child: ValueListenableBuilder(
+                  valueListenable: _progressBarModel,
+                  builder: (context, ProgressBarModel? model, Widget? child) {
+                    return Row(
+                      children: <Widget>[
 
-                return Row(
-                  children: <Widget>[
+                        ...List.generate(widget.views.length, (index){
 
-                    ...List.generate(widget.views.length, (index){
+                          final NavModel _view = widget.views[index];
 
-                      final NavModel _view = widget.views[index];
+                          return BldrsBox(
+                            height: _footerButtonHeight,
+                            width: _footerButtonWidth,
+                            icon: _view.icon,
+                            iconSizeFactor: 0.6,
+                            verse: Verse.plain(_view.titleVerse?.id ?? ''),
+                            color: model?.index == index ? Colorz.yellow255 : Colorz.white10,
+                            verseColor: model?.index == index ? Colorz.black255 : Colorz.white255,
+                            iconColor: model?.index == index ? Colorz.black255 : Colorz.white255,
+                            verseCentered: false,
+                            onTap: () => _onSwipeTo(index: index),
+                          );
 
-                      return BldrsBox(
-                        height: _footerButtonHeight,
-                        width: _footerButtonWidth,
-                        icon: _view.icon,
-                        iconSizeFactor: 0.6,
-                        verse: Verse.plain(_view.titleVerse?.id ?? ''),
-                        color: model?.index == index ? Colorz.yellow255 : Colorz.white10,
-                        verseColor: model?.index == index ? Colorz.black255 : Colorz.white255,
-                        iconColor: model?.index == index ? Colorz.black255 : Colorz.white255,
-                        verseCentered: false,
-                        onTap: () => _onSwipeTo(index: index),
-                      );
-
-                    }),
-
-                  ],
-                );
-              }
+                        }),
+                      ],
+                    );
+                  }
+                  ),
             ),
           ),
 
