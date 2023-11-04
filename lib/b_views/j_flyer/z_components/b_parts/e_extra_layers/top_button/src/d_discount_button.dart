f9c6b7b16b4925edd5f9b0a50687bbfcb4debeb6
RagehAz <rageh.az@gmail.com>
2023-11-04 20:20:14 +0200

    2.13.0 : top button is good

---
+++ lib/b_views/j_flyer/z_components/b_parts/e_extra_layers/top_button/src/d_discount_button.dart
@@ -0,0 +1,87 @@
+// ignore_for_file: unused_element
+part of top_button;
+
+class _DiscountButton extends StatelessWidget {
+  // -----------------------------------------------------------------------------
+  const _DiscountButton({
+    required this.flyerModel,
+    required this.flyerBoxWidth,
+    super.key,
+  });
+  // --------------------
+  final FlyerModel? flyerModel;
+  final double flyerBoxWidth;
+  // -----------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+   // --------------------
+    /// MAIN SCALES
+    final double _height = getTopButtonHeight(
+      flyerBoxWidth: flyerBoxWidth,
+    );
+    final double _width = getTopButtonWidth(
+      flyerBoxWidth: flyerBoxWidth,
+      flyerModel: flyerModel,
+    );
+    // --------------------
+    /// LEFT DISCOUNT BOX
+    final double _discountBoxWidth = _height;
+    // --------------------
+    /// RIGHT PRICE BOX
+    final double _pricesBoxWidth = _width - _discountBoxWidth;
+    // --------------------
+    final double _bottomLineScaleFactor = getBottomLineScaleFactor(
+      flyerBoxWidth: flyerBoxWidth,
+    );
+    final double _topTextScaleFactor = getTopLineScaleFactor(
+      flyerBoxWidth: flyerBoxWidth,
+    );
+    // --------------------
+    return _LabelStructure(
+      flyerBoxWidth: flyerBoxWidth,
+      width: _width,
+      color: _basicColor,
+      child: Row(
+        children: <Widget>[
+
+          /// DISCOUNT PERCENTAGE
+          _LabelStructure(
+            width: _discountBoxWidth,
+            flyerBoxWidth: flyerBoxWidth,
+            color: _darkColor,
+            child: _PercentageBox(
+              flyerBoxWidth: flyerBoxWidth,
+              flyerModel: flyerModel,
+            ),
+          ),
+
+          /// CURRENT - OLD PRICES
+          _TwoLinesStructure(
+            columnWidth: _pricesBoxWidth,
+            flyerBoxWidth: flyerBoxWidth,
+
+            topChild: BldrsText(
+              verse: generateLine_current_price(
+                flyerModel: flyerModel,
+              ),
+              scaleFactor: _topTextScaleFactor,
+              centered:  false,
+              weight: VerseWeight.black,
+              // appIsLTR: true,
+              // textDirection: TextDirection.ltr,
+              margin: getTextMargins(flyerBoxWidth: flyerBoxWidth),
+            ),
+
+            bottomChild: _WasLine(
+              flyerBoxWidth: flyerBoxWidth,
+              flyerModel: flyerModel,
+            ),
+          ),
+
+        ],
+      ),
+    );
+    // --------------------
+  }
+  // -----------------------------------------------------------------------------
+}
---
+++ lib/b_views/j_flyer/z_components/b_parts/e_extra_layers/top_button/src/e_amazon_price_button.dart
@@ -0,0 +1,101 @@
+// ignore_for_file: unused_element
+part of top_button;
+
+class _AmazonPriceButton extends StatelessWidget {
+  // -----------------------------------------------------------------------------
+  const _AmazonPriceButton({
+    required this.flyerModel,
+    required this.flyerBoxWidth,
+    super.key,
+  });
+  // --------------------
+  final FlyerModel? flyerModel;
+  final double flyerBoxWidth;
+  // -----------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    // --------------------
+    final double _height = getTopButtonHeight(
+      flyerBoxWidth: flyerBoxWidth,
+    );
+    final double _width = getTopButtonWidth(
+      flyerModel: flyerModel,
+      flyerBoxWidth: flyerBoxWidth,
+    );
+    final Verse? _priceLine = generateLine_current_price(
+      flyerModel: flyerModel,
+    );
+    final Verse? _buyOnAmazonLine = generateBuyOnAmazonLine(
+      flyerModel: flyerModel,
+    );
+    // --------------------
+    /// RIGHT PRICE BOX
+    final double _pricesBoxWidth = _width - _height;
+    // --------------------
+    final double _bottomLineScaleFactor = getBottomLineScaleFactor(
+      flyerBoxWidth: flyerBoxWidth,
+    );
+    // --------------------
+    return _LabelStructure(
+      flyerBoxWidth: flyerBoxWidth,
+      color: _amazonColor,
+      width: _width,
+      splashColor: Colorz.white200,
+      onTap: () => Launcher.launchURL(flyerModel?.affiliateLink),
+      child: Row(
+        children: <Widget>[
+
+          /// AMAZON LOGO
+          _LabelStructure(
+            width: _height,
+            flyerBoxWidth: flyerBoxWidth,
+            color: _darkColor,
+            child: BldrsBox(
+              width: _height,
+              height: _height,
+              icon: Iconz.amazon,
+              iconColor: _amazonColor,
+              iconSizeFactor: 0.6,
+              corners: getButtonCorners(flyerBoxWidth: flyerBoxWidth),
+            ),
+          ),
+
+          /// PRICE
+          _TwoLinesStructure(
+            columnWidth: _pricesBoxWidth,
+            flyerBoxWidth: flyerBoxWidth,
+
+            /// PRICE LINE
+            topChild: BldrsText(
+              verse: _priceLine,
+              scaleFactor: getTopLineScaleFactor(
+                flyerBoxWidth: flyerBoxWidth,
+              ),
+              centered:  false,
+              color: _darkColor,
+              // appIsLTR: true,
+              // textDirection: TextDirection.ltr,
+              margin: getTextMargins(flyerBoxWidth: flyerBoxWidth),
+            ),
+
+            /// BUY ON AMAZON
+            bottomChild: BldrsText(
+              // width: _priceLineWidth,
+              verse: _buyOnAmazonLine,
+              scaleFactor: _bottomLineScaleFactor,
+              centered:  false,
+              italic: true,
+              color: _darkColor,
+              margin: getTextMargins(flyerBoxWidth: flyerBoxWidth),
+              // appIsLTR: true,
+              // textDirection: TextDirection.ltr,
+            ),
+          ),
+
+        ],
+      ),
+    );
+
+  }
+  // -----------------------------------------------------------------------------
+}
