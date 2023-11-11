b83a4c06dd1b7002980a981005ea0a47ac3c8346
RagehAz <rageh.az@gmail.com>
2023-11-11 10:48:59 +0200

    2.14.0 : we need to push towards money making efforts

--- lib/b_views/z_components/texting/data_strip/the_strip_of_data_strip.dart
+++ lib/b_views/z_components/texting/data_strip/the_strip_of_data_strip.dart
@@ -1,5 +1,6 @@
 import 'package:basics/bldrs_theme/classes/colorz.dart';
 import 'package:basics/helpers/classes/space/borderers.dart';
+import 'package:basics/super_box/src/f_super_box_tap_layer/x_tap_layer.dart';
 import 'package:bldrs/b_views/z_components/buttons/general_buttons/bldrs_box.dart';
 import 'package:bldrs/b_views/z_components/texting/super_verse/super_verse.dart';
 import 'package:bldrs/b_views/z_components/texting/super_verse/verse_model.dart';
@@ -14,26 +15,29 @@ class TheStripOfDataStrip extends StatelessWidget {
     this.color = Colorz.bloodTest,
     this.highlightText,
     this.onTap,
+    this.textColor = Colorz.white255,
     super.key
   });
   /// --------------------------------------------------------------------------
   final double height;
-  final double width;
+  final double? width;
   final Color color;
   final String? text;
+  final Color textColor;
   final ValueNotifier<dynamic>? highlightText;
   final Function? onTap;
   /// --------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
     // --------------------
-    return Container(
+    return TapLayer(
+      height: height,
       width: width,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: Borderers.constantCornersAll10,
-      ),
+      boxColor: color,
+      splashColor: Colorz.yellow200,
+      corners: Borderers.constantCornersAll10,
       margin: const EdgeInsets.only(bottom: 5),
+      onTap: onTap,
       child: ClipRRect(
         borderRadius: Borderers.constantCornersAll10,
         child: SingleChildScrollView(
@@ -42,14 +46,15 @@ class TheStripOfDataStrip extends StatelessWidget {
           child:  BldrsBox(
             height: height,
             verse: Verse.plain(text),
+            corners: Borderers.constantCornersAll10,
             // verseShadow: false,
             verseScaleFactor: 0.8,
+            verseColor: textColor,
             bubble: false,
             color: color,
             verseWeight: VerseWeight.thin,
             verseCentered: false,
             verseHighlight: highlightText,
-            onTap: onTap,
           ),
         ),
       ),
