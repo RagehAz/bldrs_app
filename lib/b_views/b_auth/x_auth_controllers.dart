8cc1973f760e0ae334bdbec697a0e9fd278a2e22
RagehAz <rageh.az@gmail.com>
2023-11-12 04:58:37 +0200

    2.14.0 : fixes

--- lib/b_views/b_auth/x_auth_controllers.dart
+++ lib/b_views/b_auth/x_auth_controllers.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'package:basics/layouts/nav/nav.dart';
 import 'package:bldrs/a_models/a_user/user_model.dart';
 import 'package:bldrs/b_views/d_user/b_user_editor_screen/user_editor_screen.dart';
--- lib/b_views/d_user/a_user_profile_screen/a_profile_page/aaa1_user_profile_page.dart
+++ lib/b_views/d_user/a_user_profile_screen/a_profile_page/aaa1_user_profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:basics/bldrs_theme/classes/colorz.dart';
 import 'package:basics/layouts/handlers/pull_to_refresh.dart';
+import 'package:bldrs/b_views/a_starters/b_home_screen/x_initialization_controllers.dart';
 import 'package:fire/super_fire.dart';
 import 'package:bldrs/a_models/a_user/user_model.dart';
 import 'package:bldrs/b_views/d_user/z_components/banners/a_user_profile_banners.dart';
@@ -26,10 +27,19 @@ class UserProfilePage extends StatelessWidget {
             userID: Authing.getUserID(),
         );
 
-        UsersProvider.proSetMyUserModel(
-          userModel: _userModel,
-          notify: true,
-        );
+        if (_userModel != null){
+
+          UsersProvider.proSetMyUserModel(
+            userModel: _userModel,
+            notify: true,
+          );
+
+          await initializeUserBzz(
+            notify: true,
+          );
+
+        }
+
 
       },
       child: const SingleChildScrollView(
