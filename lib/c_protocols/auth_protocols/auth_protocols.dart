476d9b22443648dba270562055254186160d2da1
RagehAz <rageh.az@gmail.com>
2023-12-06 22:04:42 +0200

    2.18.0 : tracking bugs

--- lib/c_protocols/auth_protocols/auth_protocols.dart
+++ lib/c_protocols/auth_protocols/auth_protocols.dart
@@ -1,20 +1,18 @@
 import 'dart:async';
-import 'package:basics/helpers/classes/time/timers.dart';
+
 import 'package:bldrs/a_models/a_user/account_model.dart';
 import 'package:bldrs/a_models/a_user/user_model.dart';
-import 'package:bldrs/a_models/e_notes/aa_device_model.dart';
 import 'package:bldrs/a_models/x_secondary/contact_model.dart';
 import 'package:bldrs/b_views/z_components/dialogs/center_dialog/center_dialog.dart';
 import 'package:bldrs/b_views/z_components/dialogs/dialogz/dialogs.dart';
 import 'package:bldrs/b_views/z_components/texting/super_verse/verse_model.dart';
 import 'package:bldrs/c_protocols/auth_protocols/account_ldb_ops.dart';
-import 'package:bldrs/c_protocols/main_providers/ui_provider.dart';
 import 'package:bldrs/c_protocols/user_protocols/fire/user_fire_ops.dart';
 import 'package:bldrs/c_protocols/user_protocols/ldb/user_ldb_ops.dart';
 import 'package:bldrs/c_protocols/user_protocols/protocols/a_user_protocols.dart';
 import 'package:bldrs/c_protocols/user_protocols/user/user_provider.dart';
 import 'package:bldrs/e_back_end/b_fire/foundation/fire_paths.dart';
-import 'package:basics/helpers/classes/checks/errorize.dart';
+import 'package:bldrs/f_helpers/drafters/debuggers.dart';
 import 'package:bldrs/f_helpers/localization/localizer.dart';
 import 'package:fire/super_fire.dart';
 import 'package:flutter/material.dart';
@@ -396,32 +394,13 @@ class AuthProtocols {
     required String invoker,
   }) async {
 
-    final String _errorMessage = error ?? getWord('phid_something_went_wrong_error');
-    final DeviceModel _device = await DeviceModel.generateDeviceModel();
-    final UserModel? _userModel = UsersProvider.proGetMyUserModel(
-        context: getMainContext(),
-        listen: false
-    );
-
-    await Errorize.throwMap(
-        invoker: 'onAuthError',
-        map: {
-          'userID': _userModel?.id,
-          'userName': _userModel?.name,
-          'userEmail': UserModel.getUserEmail(_userModel),
-          'signInMethod': _userModel?.signInMethod,
-          'zone': _userModel?.zone?.toMap(),
-          'appState': _userModel?.appState?.toMap(toUserModel: true),
-          'time': Timers.cipherTime(time: DateTime.now(), toJSON: false),
-          'device': _device.toMap(),
-          'invoker': invoker,
-          'error': error,
-          'errorMessage': _errorMessage,
-        },
+    await throwStandardError(
+      invoker: 'onAuthError',
+      error: error,
     );
 
     await Dialogs.authErrorDialog(
-        result: _errorMessage,
+      result: error ?? getWord('phid_something_went_wrong_error'),
     );
 
   }
