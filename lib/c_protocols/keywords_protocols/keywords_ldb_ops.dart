13ba16c5e6e885367c0ef9254a73555b390bf930
RagehAz <rageh.az@gmail.com>
2023-12-25 14:08:27 +0200

    2.19.0 : working on keywords

---
+++ lib/c_protocols/keywords_protocols/keywords_ldb_ops.dart
@@ -0,0 +1,60 @@
+import 'package:basics/ldb/methods/ldb_ops.dart';
+import 'package:bldrs/e_back_end/d_ldb/ldb_doc.dart';
+/// => TAMAM
+class KeywordsLDBOps {
+  // --------------------------------------------------------------------------
+
+  KeywordsLDBOps();
+
+  // --------------------------------------------------------------------------
+
+  /// INSERT
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  static Future<void> insert({
+    required Map<String, dynamic>? map,
+  }) async {
+
+    await LDBOps.insertMap(
+        docName: LDBDoc.keywords,
+        primaryKey: 'id',
+        input: {
+          'id': 'keywords',
+          'map': map,
+        },
+    );
+
+  }
+  // --------------------------------------------------------------------------
+
+  /// READ
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  static Future<Map<String, dynamic>?> read() async {
+
+    final Map<String, dynamic>? _map = await LDBOps.readMap(
+      docName: LDBDoc.keywords,
+      id: 'keywords',
+      primaryKey: 'id',
+    );
+
+    return _map?['map'];
+
+  }
+  // --------------------------------------------------------------------------
+
+  /// DELETE
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  static Future<void> delete() async {
+
+    await LDBOps.deleteAllMapsAtOnce(
+        docName: LDBDoc.keywords,
+    );
+
+  }
+  // --------------------------------------------------------------------------
+}
