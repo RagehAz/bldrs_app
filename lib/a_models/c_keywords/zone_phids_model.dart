e0903e58df196f317c7a9bf9f22849e5e036fb35
RagehAz <rageh.az@gmail.com>
2024-01-20 20:52:53 +0200

    3.0.0 : Chains have been eradicated from Bldrs App

---
+++ lib/a_models/c_keywords/zone_phids_model.dart
@@ -0,0 +1,420 @@
+import 'package:basics/helpers/checks/tracers.dart';
+import 'package:basics/helpers/maps/lister.dart';
+import 'package:basics/helpers/maps/mapper.dart';
+import 'package:basics/helpers/maps/mapper_si.dart';
+import 'package:basics/models/flag_model.dart';
+import 'package:bldrs/a_models/f_flyer/flyer_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+/// => TAMAM
+@immutable
+class ZonePhidsModel {
+  /// --------------------------------------------------------------------------
+  const ZonePhidsModel({
+    required this.zoneID,
+    required this.phidsMap,
+  });
+  /// --------------------------------------------------------------------------
+  final String? zoneID;
+  final Map<String, int>? phidsMap;
+  // -----------------------------------------------------------------------------
+
+  /// CLONING
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  ZonePhidsModel copyWith({
+    String? zoneID,
+    Map<String, int>? phidsMap,
+  }){
+    return ZonePhidsModel(
+        zoneID: zoneID ?? this.zoneID,
+        phidsMap: phidsMap ?? this.phidsMap,
+    );
+  }
+  // -----------------------------------------------------------------------------
+
+  /// CYPHERS
+
+  // --------------------
+  /// AI TESTED
+  Map<String, int> toMap(){
+    final Map<String, int> _map = phidsMap ?? {};
+    return _map;
+  }
+  // --------------------
+  /// AI TESTED
+  static ZonePhidsModel? decipherCityNodePhids({
+    required Map<String, dynamic>? map,
+    required String? cityID,
+  }){
+    ZonePhidsModel? _zonePhids;
+
+    if (map != null && cityID != null){
+      _zonePhids = ZonePhidsModel(
+        zoneID: cityID,
+        phidsMap: MapperSI.convertDynamicMap(
+          originalMap: map,
+          transformDoubles: true,
+        ),
+      );
+    }
+
+    return _zonePhids;
+  }
+  // --------------------
+  /// AI TESTED
+  static ZonePhidsModel? decipherCountryNodeMap({
+    required Map<String, dynamic>? map,
+  }) {
+    ZonePhidsModel? _output;
+
+    if (map != null) {
+
+      final List<String>? _citiesIDs = map.keys.toList();
+      final String? countryID = map['id'];
+      _citiesIDs?.remove('id');
+
+      if (countryID != null && Lister.checkCanLoop(_citiesIDs) == true) {
+
+        for (final String cityID in _citiesIDs!) {
+
+          Map<String, dynamic> _cityPhids = map[cityID];
+          _cityPhids = Mapper.insertPairInMap(
+            map: _cityPhids,
+            key: 'id',
+            value: cityID,
+            overrideExisting: true,
+          );
+
+          final ZonePhidsModel? _model = ZonePhidsModel.decipherCityNodePhids(
+            map: _cityPhids,
+            cityID: cityID,
+          );
+
+          // _model?.blogZonePhidsModel(invoker: 'decipherCountryNodeMap : $cityID');
+
+          _output = ZonePhidsModel.combineModels(
+            zoneID: countryID,
+            base: _output,
+            add: _model,
+          );
+
+        }
+      }
+    }
+
+    // _output?.blogZonePhidsModel(invoker: 'decipherCountryNodeMap : countryID');
+
+    return _output;
+  }
+  // --------------------
+  /// TESTED: WORKS PERFECT
+  static ZonePhidsModel? decipherPlanetNodeMap({
+    required Map<String, dynamic>? map,
+  }){
+    ZonePhidsModel? _output;
+
+    // blog('decipherPlanetNodeMap : START -------------------------------->');
+
+    if (map != null) {
+
+      final List<String> _countriesIDs = map.keys.toList();
+      _countriesIDs.remove('id'); // this is the folder name in REAL DB : zonesPhids
+
+      // Mapper.blogMap(map, invoker: 'decipherPlanetNodeMap input map');
+
+      if (Lister.checkCanLoop(_countriesIDs) == true) {
+
+        // blog('decipherPlanetNodeMap here we start : _countriesIDs : $_countriesIDs');
+
+        for (final String countryID in _countriesIDs){
+
+          Map<String, dynamic> _countryPhidsMap = map[countryID];
+          _countryPhidsMap = Mapper.insertPairInMap(
+              map: _countryPhidsMap,
+              key: 'id',
+              value: countryID,
+              overrideExisting: true,
+          );
+
+          // Mapper.blogMap(_countryPhidsMap, invoker: '_countryPhidsMap');
+
+          // blog('1. decipherPlanetNodeMap : $countryID : has ${_countryPhidsMap.keys} maps before cipher');
+
+          final ZonePhidsModel? _model = ZonePhidsModel.decipherCountryNodeMap(
+            map: _countryPhidsMap,
+          );
+
+          // blog('2. decipherPlanetNodeMap : $countryID : is null ? : ${_model == null}');
+          // _model?.blogZonePhidsModel(invoker: 'decipherPlanetNodeMap : $countryID');
+
+          _output = ZonePhidsModel.combineModels(
+            zoneID: Flag.planetID,
+            base: _output,
+            add: _model,
+          );
+
+        }
+
+      }
+    }
+
+    // _output?.blogZonePhidsModel(invoker: 'decipherPlanetNodeMap : ALL');
+    // blog('decipherPlanetNodeMap : END -------------------------------->');
+
+    return _output;
+  }
+  // -----------------------------------------------------------------------------
+
+  /// CREATOR
+
+
+  // --------------------
+  /// AI TESTED
+  static Map<String, int> createPhidsMapFromFlyerPhids({
+    required List<String>? phids,
+  }){
+    final Map<String, int> _map = {};
+
+    if (Lister.checkCanLoop(phids) == true){
+
+      for (final String phid in phids!){
+
+        final int? _existingValueWithThisKey = _map[phid];
+
+        /// THIS KEY IS NEW : ADD NEW MAP MODEL TO THE LIST
+        if (_existingValueWithThisKey == null){
+
+          _map[phid] = 1;
+
+        }
+
+        /// KEY EXISTS ALREADY : INCREMENT VALUE
+        else {
+
+          final int _newValue = _existingValueWithThisKey + 1;
+
+          _map[phid] = _newValue;
+
+        }
+
+      }
+
+    }
+
+    return _map;
+  }
+  // --------------------
+  /// AI TESTED
+  static ZonePhidsModel? createZonePhidModelFromFlyer({
+    required FlyerModel? flyerModel,
+  }){
+    ZonePhidsModel? _zonePhids;
+
+    if (flyerModel != null){
+
+      _zonePhids = ZonePhidsModel(
+        zoneID: flyerModel.zone?.cityID,
+        phidsMap: createPhidsMapFromFlyerPhids(
+          phids: flyerModel.phids,
+        ),
+      );
+
+    }
+
+    return _zonePhids;
+  }
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  static Map<String, dynamic>? createIncrementationMap({
+    required List<String> removedPhids,
+    required List<String> addedPhids,
+  }){
+    Map<String, dynamic>? _incrementationMap = {};
+
+      /// ADD REMOVED SPECS WITH DECREMENT VALUES
+    if (Lister.checkCanLoop(removedPhids) == true){
+      for (final String phidToRemove in removedPhids){
+        _incrementationMap = Mapper.insertPairInMap(
+          map: _incrementationMap,
+          key: phidToRemove,
+          value: _incrementationMap![phidToRemove] == null ? -1 : _incrementationMap[phidToRemove] -1,
+          overrideExisting: true,
+        );
+      }
+    }
+
+    /// ADD ADDED SPECS WITH INCREMENT VALUES
+    if (Lister.checkCanLoop(addedPhids) == true){
+      for (final String phidToAdd in addedPhids){
+        _incrementationMap = Mapper.insertPairInMap(
+          map: _incrementationMap,
+          key: phidToAdd,
+          value: _incrementationMap![phidToAdd] == null ? 1 : _incrementationMap[phidToAdd] +1,
+          overrideExisting: true,
+        );
+      }
+    }
+
+    /// REMOVE POTENTIAL ZERO VALUES PAIRS
+    _incrementationMap = Mapper.cleanZeroValuesPairs(map: _incrementationMap,);
+
+    /// CLEAR POTENTIAL NULL VALUES
+    _incrementationMap = Mapper.cleanNullPairs(map: _incrementationMap,);
+
+    return _incrementationMap;
+  }
+  // -----------------------------------------------------------------------------
+
+  /// BLOGGING
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  void blogZonePhidsModel({
+    String invoker = '',
+  }){
+    blog('id : $zoneID');
+    blog('theMap : $phidsMap');
+  }
+  // -----------------------------------------------------------------------------
+
+  /// GETTERS
+
+  // --------------------
+  /// AI TESTED
+  static List<String> getPhidsFromZonePhidsModel({
+    required ZonePhidsModel? zonePhidsModel,
+  }){
+    List<String> _output = <String>[];
+
+    if (zonePhidsModel != null){
+      final ZonePhidsModel? _cleanedZonePhids = cleanZeroValuesPhids(zonePhidsModel);
+      _output = _cleanedZonePhids?.phidsMap?.keys.toList() ?? [];
+      _output.removeWhere((element) => element == 'id');
+    }
+
+    return _output;
+  }
+  // -----------------------------------------------------------------------------
+
+  /// MODIFIERS
+
+  // --------------------
+  /// AI TESTED
+  static ZonePhidsModel? cleanZeroValuesPhids(ZonePhidsModel? zonePhids){
+
+    ZonePhidsModel? _output = zonePhids;
+
+    final List<String> _keys = zonePhids?.phidsMap?.keys.toList() ?? [];
+
+    if (Lister.checkCanLoop(_keys) == true){
+
+      final Map<String, int> _cleanedMap = {};
+
+      for (final String key in _keys){
+
+        final int _value = zonePhids!.phidsMap![key] ?? 0;
+
+          /// ONLY GET USAGE VALUES BIGGER THAN 0
+          if (_value > 0){
+            _cleanedMap[key] = _value;
+          }
+
+      }
+
+      _output = ZonePhidsModel(
+        zoneID: zonePhids!.zoneID,
+        phidsMap: _cleanedMap,
+      );
+
+    }
+
+
+    return _output;
+  }
+  // --------------------
+  /// AI TESTED
+  static ZonePhidsModel? combineModels({
+    required String? zoneID,
+    required ZonePhidsModel? base,
+    required ZonePhidsModel? add,
+  }){
+    ZonePhidsModel? _output;
+
+    if (zoneID != null){
+
+      _output = ZonePhidsModel(
+        zoneID: zoneID,
+        phidsMap: MapperSI.combineMaps(
+          map1: base?.phidsMap,
+          map2: add?.phidsMap,
+        ),
+      );
+
+    }
+
+    return cleanZeroValuesPhids(_output);
+  }
+  // -----------------------------------------------------------------------------
+
+  /// CHECKERS
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  static bool checkZonePhidsAreIdentical({
+    required ZonePhidsModel? model1,
+    required ZonePhidsModel? model2,
+  }){
+    bool _identical = false;
+
+    if (model1 == null && model2 == null){
+      _identical = true;
+    }
+    else if (model1 != null && model2 != null){
+
+      if (
+          model1.zoneID == model2.zoneID &&
+          MapperSI.checkMapsAreIdentical(map1: model1.phidsMap, map2: model2.phidsMap) == true
+      ){
+        _identical = true;
+      }
+
+    }
+
+    return _identical;
+  }
+  // -----------------------------------------------------------------------------
+
+  /// OVERRIDES
+
+  // --------------------
+  /*
+   @override
+   String toString() => 'MapModel(key: $key, value: ${value.toString()})';
+   */
+  // --------------------
+  @override
+  bool operator == (Object other){
+
+    if (identical(this, other)) {
+      return true;
+    }
+
+    bool _areIdentical = false;
+    if (other is ZonePhidsModel){
+      _areIdentical = checkZonePhidsAreIdentical(
+        model1: this,
+        model2: other,
+      );
+    }
+
+    return _areIdentical;
+  }
+  // --------------------
+  @override
+  int get hashCode =>
+      zoneID.hashCode^
+      phidsMap.hashCode;
+// -----------------------------------------------------------------------------
+}
---
+++ lib/b_screens/c_keyword_picker/keyword_picker_screen.dart
@@ -0,0 +1,397 @@
+import 'package:basics/helpers/checks/tracers.dart';
+import 'package:basics/helpers/maps/lister.dart';
+import 'package:basics/helpers/maps/map_pathing.dart';
+import 'package:basics/helpers/maps/mapper.dart';
+import 'package:basics/helpers/space/scale.dart';
+import 'package:basics/helpers/strings/pathing.dart';
+import 'package:basics/helpers/strings/searching.dart';
+import 'package:basics/helpers/strings/stringer.dart';
+import 'package:basics/models/phrase_model.dart';
+import 'package:basics/layouts/nav/nav.dart';
+import 'package:basics/layouts/views/floating_list.dart';
+import 'package:bldrs/b_screens/c_keyword_picker/z_keywords_tree/keywords_tree.dart';
+import 'package:bldrs/z_components/buttons/editors_buttons/editor_confirm_button.dart';
+import 'package:bldrs/z_components/layouts/main_layout/main_layout.dart';
+import 'package:bldrs/z_components/sizing/stratosphere.dart';
+import 'package:bldrs/z_components/texting/customs/no_result_found.dart';
+import 'package:bldrs/z_components/texting/super_verse/verse_model.dart';
+import 'package:bldrs/c_protocols/keywords_protocols/keywords_protocols.dart';
+import 'package:bldrs/f_helpers/localization/localizer.dart';
+import 'package:bldrs/f_helpers/router/d_bldrs_nav.dart';
+import 'package:flutter/material.dart';
+
+class KeywordsPickerScreen extends StatefulWidget {
+  // --------------------------------------------------------------------------
+  const KeywordsPickerScreen({
+    this.multipleSelection = false,
+    this.selectedPhids = const [],
+    super.key
+  });
+  // --------------------
+  final bool multipleSelection;
+  final List<String> selectedPhids;
+  // --------------------
+  @override
+  _KeywordsPickerScreenState createState() => _KeywordsPickerScreenState();
+  // --------------------------------------------------------------------------
+  static Future<String?> pickPath() async {
+
+    final String? _path = await BldrsNav.goToNewScreen(
+        screen: const KeywordsPickerScreen(
+          // multipleSelection: false,
+          // selectedPhids: [],
+        ),
+    );
+
+    return _path;
+  }
+  // --------------------
+  static Future<List<String>> pickPaths({
+    required List<String> selectedPaths,
+  }) async {
+
+    final List<String> _path = await BldrsNav.goToNewScreen(
+      screen: KeywordsPickerScreen(
+        multipleSelection: true,
+        selectedPhids: selectedPaths,
+      ),
+    );
+
+    return _path;
+
+  }
+  // --------------------
+  static Future<String?> pickPhid() async {
+    final String? _path = await pickPath();
+    return Pathing.getLastPathNode(_path);
+  }
+  // --------------------
+  static Future<List<String>> pickPhids({
+    required List<String> selectedPaths,
+  }) async {
+
+    final List<String> _paths = await pickPaths(
+        selectedPaths: selectedPaths,
+    );
+
+    return Pathing.getPathsLastNodes(_paths);
+  }
+  // --------------------------------------------------------------------------
+}
+
+class _KeywordsPickerScreenState extends State<KeywordsPickerScreen> {
+  // ----------------------------------------------------------------------------
+  final ValueNotifier<bool> _isSearching = ValueNotifier<bool>(false);
+  final TextEditingController _searchController = TextEditingController();
+  List<String> _mapPaths = [];
+  Map<String, dynamic> _foundMap = {};
+  List<Phrase> _phrases = [];
+  List<String> _allPhids = [];
+  List<String> _selectedPaths = [];
+  // -----------------------------------------------------------------------------
+  Map<String, dynamic>? _keywordsMap;
+  // -----------------------------------------------------------------------------
+  /// --- LOADING
+  final ValueNotifier<bool> _loading = ValueNotifier(false);
+  // --------------------
+  Future<void> _triggerLoading({required bool setTo}) async {
+    setNotifier(
+      notifier: _loading,
+      mounted: mounted,
+      value: setTo,
+    );
+  }
+  // -----------------------------------------------------------------------------
+  @override
+  void initState() {
+    super.initState();
+
+  }
+  // --------------------
+  bool _isInit = true;
+  @override
+  void didChangeDependencies() {
+
+    if (_isInit && mounted) {
+      _isInit = false; // good
+
+      asyncInSync(() async {
+
+        await _triggerLoading(setTo: true);
+
+        final Map<String, dynamic>? _map = await KeywordsProtocols.fetch();
+
+
+        setState(() {
+          _mapPaths = MapPathing.generatePathsFromMap(map: _map);
+          _allPhids = Mapper.getAllNestedKeys(
+            map: _map,
+            allowDuplicates: false,
+          );
+          _phrases = _generatePhrases();
+          _keywordsMap = _map;
+          _selectedPaths = Pathing.findPathsContainingSubStrings(
+            paths: _mapPaths,
+            subStrings: widget.selectedPhids,
+          );
+
+        });
+
+        await _triggerLoading(setTo: false);
+
+      });
+
+    }
+    super.didChangeDependencies();
+  }
+  // --------------------
+  /*
+  @override
+  void didUpdateWidget(TheStatefulScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.thing != widget.thing) {
+      unawaited(_doStuff());
+    }
+  }
+   */
+  // --------------------
+  @override
+  void dispose() {
+    _loading.dispose();
+    _isSearching.dispose();
+    _searchController.dispose();
+    super.dispose();
+  }
+  // --------------------------------------------------------------------------
+
+  /// GENERATE PHRASES
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  List<Phrase> _generatePhrases(){
+
+    final List<Phrase> _output = [];
+
+    for (final String phid in _allPhids){
+
+      final String _value = getWord(phid);
+      final Phrase _phrase = Phrase(
+        id: phid,
+        value: _value,
+        langCode: Localizer.getCurrentLangCode(),
+        trigram: Stringer.createTrigram(input: _value),
+      );
+
+      _output.add(_phrase);
+
+    }
+
+    return _output;
+  }
+  // --------------------------------------------------------------------------
+
+  /// MAP SEARCH
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  Future<void> _onSearchChanged(String? text) async {
+
+    if (_keywordsMap != null){
+
+      Searching.triggerIsSearchingNotifier(
+        text: text,
+        isSearching: _isSearching,
+        mounted: mounted,
+      );
+
+      if (_isSearching.value  == true){
+
+        /// SEARCH KEYS
+        final List<String> _foundPaths = Pathing.findPathsContainingSubstring(
+          paths: _mapPaths,
+          subString: text,
+        );
+
+        /// SEARCH TRANSLATIONS
+        final List<String> _foundPhrases = searchPhrasesAndGetPaths(
+          text: text,
+        );
+
+        final List<String> _allFound = Stringer.addStringsToStringsIfDoNotContainThem(
+          listToTake: _foundPaths,
+          listToAdd: _foundPhrases,
+        );
+
+        /// FOUND PATHS
+        if (Lister.checkCanLoop(_allFound) == true){
+
+          final Map<String, dynamic> _output = MapPathing.generateMapFromSomePaths(
+            somePaths: _allFound,
+            sourceMap: _keywordsMap!,
+          );
+
+          setState(() {
+            _foundMap = _output;
+          });
+
+        }
+
+        /// NOTHING FOUND
+        else {
+          setState(() {
+            _foundMap = {};
+          });
+        }
+
+      }
+
+    }
+
+  }
+// --------------------
+  /// TESTED : WORKS PERFECT
+  List<String> searchPhrasesAndGetPaths({
+    required String? text
+  }){
+    List<String> _output = [];
+
+    final List<Phrase> _foundPhrases = Phrase.searchPhrasesTrigrams(
+      sourcePhrases: _phrases,
+      inputText: text,
+    );
+
+    if (Lister.checkCanLoop(_foundPhrases) == true){
+
+      final List<String> _phids = Phrase.getPhrasesIDs(_foundPhrases);
+
+      _output = Pathing.findPathsContainingSubStrings(
+        paths: _mapPaths,
+        subStrings: _phids,
+      );
+
+    }
+
+    return _output;
+  }
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  void _onSearchCancelled(){
+    setNotifier(notifier: _isSearching, mounted: mounted, value: false);
+    _searchController.text = '';
+  }
+  // --------------------------------------------------------------------------
+
+  /// TAPS
+
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  Future<void> onLastNodeTap(String path) async {
+
+    if (widget.multipleSelection == true){
+      setState(() {
+        _selectedPaths = Stringer.addOrRemoveStringToStrings(
+            strings: _selectedPaths,
+            string: path
+        );
+      });
+    }
+
+    else {
+      await Nav.goBack(
+        context: context,
+        passedData: path,
+      );
+    }
+
+
+  }
+  // --------------------
+  /// TESTED : WORKS PERFECT
+  Future<void> onExpandableNodeTap(String path) async {
+
+    blog('expandable node path is : $path');
+
+  }
+  // --------------------------------------------------------------------------
+  @override
+  Widget build(BuildContext context) {
+    // --------------------
+    final double _treeZoneWidth = Scale.screenWidth(context) * 0.8;
+    final double _keyWidth = Scale.screenWidth(context) * 0.6;
+    // --------------------
+    return MainLayout(
+      key: const ValueKey<String>('theKeywordsPickerScreen'),
+      canSwipeBack: false,
+      loading: _loading,
+      // pyramidType: PyramidType.crystalYellow,
+
+      appBarType: AppBarType.search,
+      onSearchChanged: _onSearchChanged,
+      searchController: _searchController,
+      onSearchCancelled: _onSearchCancelled,
+      onSearchSubmit: _onSearchChanged,
+      confirmButton: widget.multipleSelection == false ? null : ConfirmButton(
+        confirmButtonModel: ConfirmButtonModel(
+          onTap: () => Nav.goBack(
+            context: context,
+            passedData: _selectedPaths,
+          ),
+          firstLine: const Verse(id: 'phid_confirm', translate: true),
+          isWide: true,
+        ),
+        enAlignment: Alignment.bottomCenter,
+      ),
+      child: ValueListenableBuilder(
+          valueListenable: _isSearching,
+          builder: (_, bool searching, Widget? child){
+
+            return FloatingList(
+              width: Scale.screenWidth(context),
+              height: Scale.screenHeight(context),
+              boxAlignment: Alignment.topCenter,
+              padding: Stratosphere.getStratosphereSandwich(context: context, appBarType: AppBarType.search),
+              columnChildren: <Widget>[
+
+                /// BROWSING : MAP TREE
+                if (searching == false)
+                  KeywordsTreeStarter(
+                    key: const ValueKey<String>('KeywordsTreeStarter_browse'),
+                    map: _keywordsMap,
+                    width: _treeZoneWidth,
+                    keyWidth: _keyWidth,
+                    onLastNodeTap: onLastNodeTap,
+                    onExpandableNodeTap: onExpandableNodeTap,
+                    selectedPaths: _selectedPaths,
+                    // initiallyExpanded: false,
+                  ),
+
+                /// SEARCHING : NO RESULT FOUND
+                if (searching == true && _foundMap.isEmpty)
+                  const Center(
+                    child: NoResultFound(),
+                  ),
+
+                /// SEARCHING : FOUND RESULTS TREE
+                if (searching == true && _foundMap.isNotEmpty)
+                  KeywordsTreeStarter(
+                    key: const ValueKey<String>('KeywordsTreeStarter_search'),
+                    map: _foundMap,
+                    width: _treeZoneWidth,
+                    keyWidth: _keyWidth,
+                    searchValue: _searchController,
+                    initiallyExpanded: true,
+                    onLastNodeTap: onLastNodeTap,
+                    onExpandableNodeTap: onExpandableNodeTap,
+                    selectedPaths: _selectedPaths,
+                  ),
+
+              ],
+            );
+
+          }
+      ),
+    );
+    // --------------------
+  }
+  // -----------------------------------------------------------------------------
+}
